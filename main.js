da57908640e1ed2630bd837641539acdcbee38fb
extr0py <extr0py@extropygames.com>
2016-12-10 15:03:11 -0800

    Update logic in main.js because mainWindow isn't stored anymore, the windows are stored in an array

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
+    if (windows.length === 0) {
         createWindow()
     }
 })
