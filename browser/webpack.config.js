4ac7308c535e9bfcbdad1b1b6ca004ad0bfeebf9
extr0py <extr0py@extropygames.com>
2016-12-17 11:49:54 -0800

    Update to use react-hot-loader and remove old cursor

--- browser/webpack.config.js
+++ browser/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
             test: /\.tsx?$/,
             exclude: [/node_modules/],
             loaders: [
+                "react-hot",
                 "ts-loader"
             ]
         }, {
