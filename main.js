bb7a171c6ec1c8a474e8add3b7000ca698ffb1ae
extr0py <extr0py@extropygames.com>
2016-12-20 04:42:47 -0800

    Fix bug with <C-PageUp>/<C-PageDown> in latest change

--- main.js
+++ main.js
@@ -69,8 +69,9 @@ function createWindow(commandLineArguments, workingDirectory) {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null
         windows = windows.filter(m => m !== mainWindow)
+        mainWindow = null
+
     })
 
     windows.push(mainWindow)
