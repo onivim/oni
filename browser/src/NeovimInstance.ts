f139caba34f0c6ef0f85e1305eaf79636ac2a0d1
extr0py <extr0py@users.noreply.github.com>
2016-12-22 12:32:55 -0800

    Fix #94 - set g:gui_oni variable before loading init.vim (#119)
    * Add g:gui_oni variable so that vimrcs can be modified based on whether or not ONI is running
    
    * Fix for #94 - Add g:gui_oni variable for init.vim differentation

--- browser/src/NeovimInstance.ts
+++ browser/src/NeovimInstance.ts
@@ -347,7 +347,7 @@ function startNeovim(runtimePaths: string[], args: any): Q.IPromise<any> {
     const vimRcArg = (shouldLoadInitVim || !useDefaultConfig) ? [] : ["-u", noopInitVimPath]
 
     const argsToPass = vimRcArg
-        .concat(["--cmd", "set rtp+=" + joinedRuntimePaths, "-N", "--embed", "--"])
+        .concat(["--cmd", "set rtp+=" + joinedRuntimePaths, "--cmd", "let g:gui_oni = 1", "-N", "--embed", "--"])
         .concat(args)
 
     const nvimProc = cp.spawn(nvimProcessPath, argsToPass, {})
