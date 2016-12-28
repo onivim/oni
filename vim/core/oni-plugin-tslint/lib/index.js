1a6c03bcbd2bdcd7878a338b34aabe3b2b9436cf
extr0py <extr0py@extropygames.com>
2016-12-27 21:37:52 -0800

    Run tslint process

--- vim/core/oni-plugin-tslint/lib/index.js
+++ vim/core/oni-plugin-tslint/lib/index.js
@@ -1,11 +1,27 @@
+const path = require("path")
+const os = require("os")
+const execSync = require("child_process").execSync
 
+const isWindows = os.platform() === "win32"
+
+const tslintExecutable = isWindows ? "tslint.cmd" : "tslint"
+
+const tslintPath = path.join(__dirname, "..", "..", "..", "..", "node_modules", ".bin", tslintExecutable)
+
+// TODO: Switch to save event
 Oni.on("buffer-update", (args) => {
-
     if (!args.eventContext.bufferFullPath) {
         return
     }
 
     console.log("edited: " + args.eventContext.bufferFullPath)
+
+    const args = ["--force", "--format json"]
+
+    // TODO: If tsconfig.json is present, use that
+    // TODO: If tslint.json is present, use that
+
+    const derp = execSync(tslintPath + " --force --format json " + args.eventContext.bufferFullPath, { cwd: path.dirname(args.eventContext.bufferFullPath) }).toString()
 
     Oni.diagnostics.setErrors("tslint", args.eventContext.bufferFullPath, [{ type: null, text: "derp", lineNumber: 1, startColumn: 0, endColumn: 1}], "yellow")
 })
