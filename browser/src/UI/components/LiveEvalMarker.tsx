c9326b61ff349a1da80c53368ec0f3968963e2bb
extr0py <extr0py@extropygames.com>
2017-01-10 06:39:15 -0800

    Add style for live eval marker

--- browser/src/UI/components/LiveEvalMarker.tsx
+++ browser/src/UI/components/LiveEvalMarker.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
+import * as classNames from "classnames"
+
 require("./LiveEvalMarker.less") // tslint:disable-line no-var-requires
 
 import { ILiveCodeBlock } from "./../../Services/LiveEvaluation"
@@ -42,6 +44,8 @@ export class LiveEvalMarker extends React.Component<ILiveEvalMarkerProps, void> 
 
     public render(): JSX.Element {
 
+        const cssClasses = classNames("live-eval-marker", { "evaluating": this._isEvaluating() }, { "error": this._hasError() })
+
         const positionDivStyles = {
             position: "absolute",
             top: this.props.y.toString() + "px",
@@ -50,7 +54,29 @@ export class LiveEvalMarker extends React.Component<ILiveEvalMarkerProps, void> 
             width: "250px",
         }
 
-        return <div className="live-eval-marker" style={positionDivStyles}>{JSON.stringify(this.props.result)}</div>
+        return <div className={cssClasses} style={positionDivStyles}>{this._getText()}</div>
+    }
+
+    private _hasError(): boolean {
+        return !!(this.props.result && this.props.result.errors && this.props.result.errors.length)
+    }
+
+    private _hasResult(): boolean {
+        return !!this.props.result
+    }
+
+    private _isEvaluating(): boolean {
+        return !this._hasResult()
+    }
+
+    private _getText(): string {
+        if (this._hasError() && this.props.result.errors) {
+            return this.props.result.errors[0]
+        } else if (this._hasResult() && this.props.result.output) {
+            return this.props.result.output[0]
+        } else {
+            return ""
+        }
     }
 }
 
