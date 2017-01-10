6a22a9b0ff79c0e9a539c2c20ab44080c3c18e9a
rafi993 <fair7993@gmail.com>
2017-01-10 16:25:53 +0000

    TSLINT error fixed

--- browser/src/Platform.ts
+++ browser/src/Platform.ts
@@ -3,5 +3,5 @@ import * as os from "os"
 export const isWindows = () => os.platform() === "win32"
 export const isLinux = () => os.platform() === "linux"
 
-export const getUserHome = () => 
+export const getUserHome = () =>
     isWindows() ? process.env["USERPROFILE"] : process.env["HOME"] // tslint:disable-line no-string-literal
