56ad3bbde4ff78c8075be41343201455d4eebbdd
extr0py <extr0py@users.noreply.github.com>
2017-01-21 10:48:54 -0800

    Extr0py/178/debugging tips (#180)
    * Fix up sourcemaps and add some documentation around debugging
    
    * Improve the hot-reload experience so that it isn't just a blank white screen while the webpack bundle is being compiled
    
    * Add gif of live-editing of cursor
    
    * Fix image link
    
    * Fix image link
    
    * Remove changes in Cursor.less

--- browser/webpack.production.config.js
+++ browser/webpack.production.config.js
@@ -1,50 +1,9 @@
-var path = require("path");
 var webpack = require("webpack");
-var lessPluginAutoPrefix = require("less-plugin-autoprefix");
 var OptimizeJsPlugin = require("optimize-js-plugin")
 
-module.exports = {
-    entry: [
-        path.join(__dirname, "src/index.tsx")
-    ],
-    target: "electron-renderer",
+const baseConfig = require("./webpack.debug.config.js")
 
-    module: {
-        preLoaders: [{
-            test: /\.tsx?%/,
-            exclude: /(node_modules)/,
-            loader: "source-map"
-        }],
-        loaders: [{
-            test: /\.tsx?$/,
-            exclude: [/node_modules/],
-            loaders: [
-                "ts-loader"
-            ]
-        }, {
-            test: /\.less$/,
-            loader: "style!css?-url!less"
-        }]
-    },
-    lessLoader: {
-        lessPlugins: [
-            new lessPluginAutoPrefix({browsers: ["last 2 versions"]})
-        ]
-    },
-    ts: {
-        configFileName: "tsconfig.src.json"
-    },
-    resolve: {
-        extensions: ['', '.webpack.js', '.js', '.jsx', '.ts', '.tsx']
-    },
-    output: {
-        path: path.join(__dirname, "..", "dist", "browser"),
-        publicPath: "/",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: "./dist",
-    },
+const productionConfig = Object.assign({}, baseConfig, {
     plugins: [
         new webpack.DefinePlugin({
             "process.env.NODE_ENV":'"production"'
@@ -52,17 +11,16 @@ module.exports = {
         // Unfortunately, the packaged version of UglifyJS doesn't support ES6
         // https://github.com/mishoo/UglifyJS2/issues/448
         //
-        // This could help the app load performance, because a non-trivial amount of time is spent loading & parsing the script
+        // This could help the app load performance, because a non-trivial amount of time 
+        // is spent loading & parsing the script
+        //
         // new webpack.optimize.UglifyJsPlugin({
         //     minimize: true,
         //     sourceMap: false
-        // }),
         new OptimizeJsPlugin({
             sourcemap: false
         })
     ],
-    node: {
-        process: false,
-        __dirname: false
-    }
-};
+})
+
+module.exports = productionConfig
