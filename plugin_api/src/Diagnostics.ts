a5e1605ede9756bcb7bbd3521b416958e9bd028c
extr0py <extr0py@extropygames.com>
2017-02-07 06:20:46 -0800

    Get tests green for DiagnosticsTests

--- plugin_api/src/Diagnostics.ts
+++ plugin_api/src/Diagnostics.ts
@@ -3,12 +3,22 @@ import * as Sender from "./Sender"
 /**
  * API instance for interacting with Oni (and vim)
  */
-export class Diagnostics implements Oni.Plugin.Diagnostics.Api { 
+export class Diagnostics implements Oni.Plugin.Diagnostics.Api {
+    private _filesThatHaveErrors: { [fileName: string]: boolean } = {}
 
     constructor(private _sender: Sender.ISender = new Sender.IpcSender) {
     }
 
     public setErrors(key: string, fileName: string, errors: Oni.Plugin.Diagnostics.Error[], color?: string): void {
+
+        if (!errors)
+            return
+
+        if (errors.length === 0 && !this._filesThatHaveErrors[fileName])
+            return
+
+        this._filesThatHaveErrors[fileName] = errors.length > 0
+
         this._sender.send("set-errors", null, {
             key: key,
             fileName: fileName,
@@ -18,6 +28,7 @@ export class Diagnostics implements Oni.Plugin.Diagnostics.Api {
     }
 
     public clearErrors(key: string): void {
+        this._filesThatHaveErrors = {}
         this._sender.send("clear-errors", null, {
             key: key
         })
--- plugin_api/test/DiagnosticsTests.ts
+++ plugin_api/test/DiagnosticsTests.ts
@@ -3,6 +3,47 @@ import * as assert from "assert"
 import { Diagnostics } from "./../src/Diagnostics"
 import { ISender } from "./../src/Sender"
 
+describe("Diagnostics", () => {
+    let diagnostics: Diagnostics
+    let mockSender: MockSender
+
+    beforeEach(() => {
+        mockSender = new MockSender()
+        diagnostics = new Diagnostics(mockSender)
+    })
+
+    it("sends errors for a file", () => {
+        diagnostics.setErrors("test-plugin", "someFile.ts", [{
+            lineNumber: 1,
+            startColumn: 0,
+            endColumn: 1,
+            type: "error",
+            text: "some error"
+        }], "red")
+
+        assert.strictEqual(mockSender.sentMessages.length, 1)
+    })
+
+    it("does not send errors for a file if there have never been any errors", () => {
+        diagnostics.setErrors("test-plugin", "someFile.ts", [], "red")
+
+        assert.strictEqual(mockSender.sentMessages.length, 0)
+    })
+
+    it("does send empty errors array, if there have been errors previously", () => {
+        diagnostics.setErrors("test-plugin", "someFile.ts", [{
+            lineNumber: 1,
+            startColumn: 0,
+            endColumn: 1,
+            type: "error",
+            text: "some error"
+        }], "red")
+
+        diagnostics.setErrors("test-plugin", "someFile.ts", [], "red")
+        assert.strictEqual(mockSender.sentMessages.length, 2)
+    })
+})
+
 class MockSender implements ISender {
     private _sentMessages: any[] = []
     private _sentErrors: any[] = []
@@ -31,26 +72,3 @@ class MockSender implements ISender {
         })
     }
 }
-
-describe("Diagnostics", () => {
-    let diagnostics: Diagnostics;
-    let mockSender: ISender;
-
-    beforeEach(() => {
-        mockSender = new MockSender();
-        diagnostics = new Diagnostics(mockSender);
-    });
-
-    it("sends errors for a file", () => {
-        assert.ok(false);
-    });
-
-    it("does not send errors for a file if there have never been any errors", () => {
-        assert.ok(false);
-    });
-
-    it("does send empty errors array, if there have been errors previously", () => {
-        assert.ok(false);
-    });
-})
-
