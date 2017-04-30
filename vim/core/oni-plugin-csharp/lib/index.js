0772817a5e7fd57d11ae343929d314781f16d91c
extr0py <extr0py@extropygames.com>
2017-04-29 21:33:36 -0700

    Use the exec path

--- vim/core/oni-plugin-csharp/lib/index.js
+++ vim/core/oni-plugin-csharp/lib/index.js
@@ -9,7 +9,7 @@ const activate = (Oni) => {
 
     const omniSharpLangServerPath = path.join(__dirname, "..", "..", "..", "..", "node_modules", "omnisharp-client", "languageserver", "server.js")
 
-    const execCommand = `${process.execPath} "${omniSharpLangServerPath}"`
+    const execCommand = `${omniSharpLangServerPath}`
 
     const client = Oni.createLanguageClient(execCommand, (filePath) => {
         return getRootProjectFileAsync(path.dirname(filePath))
