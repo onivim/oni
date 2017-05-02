df9a7e2e30c2632163359b8cdc465c55b1aff81e
extr0py <extr0py@extropygames.com>
2017-05-02 07:20:53 -0700

    Set travis display

---
+++ build/script/set-travis-display.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+fi
+
+node --version
+npm --version
+
+npm install
+npm test
