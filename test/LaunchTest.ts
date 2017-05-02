da8062f734a67106d01caa1da332bb04841abd23
extr0py <extropy@extropygames.com>
2017-05-02 07:06:15 -0700

    Update path for darwin build

--- test/LaunchTest.ts
+++ test/LaunchTest.ts
@@ -3,6 +3,18 @@ import * as path from "path"
 
 import { Application } from "spectron"
 
+const getExecutablePath = () => {
+    switch (process.platform) {
+
+        case "win32":
+            return path.join(__dirname, "..", "..", "dist", "win-ia32-unpacked", "Oni.exe")
+        case "darwin":
+            return path.join(__dirname, "..", "..", "dist", "mac", "Oni.app", "Contents", "MacOS", "Oni")
+        default:
+            throw `Unable to find Oni executable for platform ${process.platform}`
+    }
+}
+
 describe("application launch", function () {
 
     this.timeout(10000)
@@ -11,7 +23,7 @@ describe("application launch", function () {
 
     beforeEach(() => {
         app = new Application({
-            path: path.join(__dirname, "..", "..", "dist", "win-ia32-unpacked", "Oni.exe")
+            path: getExecutablePath()
         })
 
         return app.start()
