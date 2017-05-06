d148d4fe296cb7ab94a9dd2584876eb70ad1893f
extr0py <extr0py@extropygames.com>
2017-05-06 12:10:12 -0700

    Fix regression in file:// vs file:/// on windows

--- browser/src/Plugins/Api/LanguageClient/LanguageClientHelpers.ts
+++ browser/src/Plugins/Api/LanguageClient/LanguageClientHelpers.ts
@@ -25,9 +25,9 @@ export const ProtocolConstants = {
     },
 }
 
-export const wrapPathInFileUri = (path: string) => "file://" + path
+export const wrapPathInFileUri = (path: string) => getFilePrefix() + path
 
-export const unwrapFileUriPath = (uri: string) => decodeURIComponent((uri).split("file://")[1])
+export const unwrapFileUriPath = (uri: string) => decodeURIComponent((uri).split(getFilePrefix())[1])
 
 export const getTextFromContents = (contents: types.MarkedString | types.MarkedString[]): string[] => {
     if (contents instanceof Array) {
@@ -35,15 +35,6 @@ export const getTextFromContents = (contents: types.MarkedString | types.MarkedS
                 .map((markedString) => getTextFromMarkedString(markedString))
     } else {
         return [getTextFromMarkedString(contents)]
-    }
-}
-
-const getTextFromMarkedString = (markedString: types.MarkedString): string => {
-    if (typeof markedString === "string") {
-        return markedString.trim()
-    } else {
-        // TODO: Properly apply syntax highlighting based on the `language` parameter
-        return markedString.value.trim()
     }
 }
 
@@ -100,5 +91,23 @@ export const incrementalBufferUpdateToDidChangeTextDocumentParams = (args: Oni.I
             range: types.Range.create(lineNumber - 1, 0, lineNumber - 1, previousLineLength),
             text: changedLine,
         }],
+    }
+}
+
+const getTextFromMarkedString = (markedString: types.MarkedString): string => {
+    if (typeof markedString === "string") {
+        return markedString.trim()
+    } else {
+        // TODO: Properly apply syntax highlighting based on the `language` parameter
+        return markedString.value.trim()
+    }
+}
+
+
+const getFilePrefix = () => {
+    if (process.platform === "win32") {
+        return "file://"
+    } else {
+        return "file:///"
     }
 }
