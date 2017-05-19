40a5b4d9ff521a62f777061a3da1dbb3f44dfae8
extr0py <extr0py@extropygames.com>
2017-05-19 07:28:03 -0700

    Revert SignatureHelpContainer

--- browser/src/UI/RootComponent.tsx
+++ browser/src/UI/RootComponent.tsx
@@ -25,18 +25,6 @@ export class RootComponent extends React.Component<IRootComponentProps, void> {
             <div className="layer">
                 <div className="container full">
                     <EditorHost editor={this.props.editor} />
-<<<<<<< HEAD
-=======
-                    <Cursor />
-                    <CursorLine lineType={"line"} />
-                    <CursorLine lineType={"column"} />
-                    <SignatureHelpContainer />
-                    <QuickInfoContainer />
-                    <SignatureHelpContainer />
-                    <MenuContainer />
-                    <AutoCompletionContainer />
-                    <Logs />
->>>>>>> parent of 17bad69... Refactor containers to separate files
                 </div>
             </div>
             <div className="layer">
