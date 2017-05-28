1881bdca7571aeff66ee41745fb0643bb15b8d2a
extr0py <extr0py@extropygames.com>
2017-05-28 08:50:24 -0700

    Clean up python a bit

--- vim/core/oni-plugin-python/lib/index.js
+++ vim/core/oni-plugin-python/lib/index.js
@@ -4,21 +4,21 @@ const childProcess = require("child_process")
 
 const activate = (Oni) => {
 
-    // TODO: Remove log file
     const serverOptions = {
         command: "pyls",
     }
 
-    const client = Oni.createLanguageClient(serverOptions, (filePath) => {
+    const getInitializationOptionsAsync = (filePath) => {
         return Promise.resolve({
             clientName: "python",
-            // TODO: Remove hardcoded dependency
             rootPath: "file:///" + filePath,
             capabilities: {
                 highlightProvider: true
             }
         })
-    })
+    }
+
+    const client = Oni.createLanguageClient(serverOptions, getInitializationOptionsAsync)
 }
 
 module.exports = {
