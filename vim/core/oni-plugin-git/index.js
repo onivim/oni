770e4fedcad74c6852bd314ee5753c44cd7954e3
Alessio Linares <alessio.linares@skyscanner.net>
2017-07-14 16:26:40 +0200

    #486 - Initial implementation

---
+++ vim/core/oni-plugin-git/index.js
@@ -0,0 +1,39 @@
+const childProcess = require("child_process");
+const path = require("path");
+const Q = require("q");
+
+const activate = (Oni) => {
+
+    const getGitBranch = (options) => {
+        const deferred = Q.defer();
+        childProcess.exec("git rev-parse --abbrev-ref HEAD", options, (error, stdout, stderr) => {
+            if (error && error.code) {
+                deferred.reject(new Error(stderr));
+            } else {
+                deferred.resolve(stdout);
+            }
+        });
+        return deferred.promise;
+    };
+
+    const gitBranchIndicator = Oni.statusBar.createItem(0, -3);
+
+    const updateBranchIndicator = (evt) => {
+        getGitBranch({ cwd: path.dirname(evt.bufferFullPath) })
+        .then((branchName) => {
+            const React = Oni.dependencies.React;
+            const gitBranch  = React.createElement("div", null, branchName);
+            gitBranchIndicator.setContents(gitBranch);
+            gitBranchIndicator.show();
+        })
+        .fail((error) => {
+            gitBranchIndicator.hide();
+        });
+    };
+
+    Oni.on("buffer-enter", updateBranchIndicator);
+};
+
+module.exports = {
+    activate,
+};
