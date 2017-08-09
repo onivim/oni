a1e4dec2a0326344c140eb7f63512592feaa44ea
extr0py <extr0py@extropygames.com>
2017-08-08 18:06:42 -0700

    Delete unneeded files, update icns and ico

--- build/GenerateWindowsIco.js
+++ build/GenerateWindowsIco.js
@@ -5,20 +5,7 @@
 const path = require("path")
 const fs = require("fs")
 
-const toIco = require("to-ico-sync")
-
-const rootPath = path.join(__dirname, "icons")
-
-const iconSizesToInclude = ["32x32", "64x64", "128x128", "256x256"]
-
-const files = iconSizesToInclude.map((icon) => {
-    const fileFullPath = path.join(rootPath, icon + ".png")
-    console.log("Reading file: " + fileFullPath)
-    return fs.readFileSync(fileFullPath)
-})
-
-const buf = toIco(files)
+const buf = fs.readFileSync(path.join(__dirname, "..", "images", "oni.ico"))
 
 fs.writeFileSync(path.join(__dirname, "icon.ico"), buf)
 fs.writeFileSync(path.join(__dirname, "installerIcon.ico"), buf)
-fs.writeFileSync(path.join(__dirname, "..", "images", "oni.ico"), buf)
