c76fee036dbebfc097cb5e4c705597ea69417f93
extr0py <extr0py@extropygames.com>
2017-08-11 08:55:03 -0700

    Get debug build... almost working

--- browser/webpack.debug.config.js
+++ browser/webpack.debug.config.js
@@ -1,50 +1,47 @@
 var path = require("path");
 var webpack = require("webpack");
-var lessPluginAutoPrefix = require("less-plugin-autoprefix");
-var OptimizeJsPlugin = require("optimize-js-plugin")
+// var lessPluginAutoPrefix = require("less-plugin-autoprefix");
+// var OptimizeJsPlugin = require("optimize-js-plugin")
 
 module.exports = {
     entry: [
         path.join(__dirname, "src/index.tsx")
     ],
     target: "electron-renderer",
-
-    module: {
-        preLoaders: [{
-            test: /\.tsx?%/,
-            exclude: /(node_modules)/,
-            loader: "source-map"
-        }],
-        loaders: [{
-            test: /\.tsx?$/,
-            exclude: [/node_modules/],
-            loaders: [
-                "ts-loader"
-            ]
-        }, {
-            test: /\.less$/,
-            loader: "style!css?-url!less"
-        }]
-    },
-    lessLoader: {
-        lessPlugins: [
-            new lessPluginAutoPrefix({browsers: ["last 2 versions"]})
-        ]
-    },
-    devtool: "source-map",
-    ts: {
-        configFileName: "tsconfig.src.json"
-    },
     resolve: {
-        extensions: ['', '.webpack.js', '.js', '.jsx', '.ts', '.tsx']
+        extensions: [".tsx", ".ts", ".js", ".less"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
     output: {
         path: path.join(__dirname, "..", "lib", "browser"),
         publicPath: "/",
         filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: "./lib",
     },
     node: {
         process: false,
