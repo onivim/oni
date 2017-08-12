fdfe0f62018810488f41ca1b07977e1706e4ab89
extr0py <extr0py@extropygames.com>
2017-08-12 16:57:52 -0700

    Some cleanup, add to PLAN

---
+++ PLAN.md
@@ -0,0 +1,6 @@
+- Create a non-plugin based neovim editor
+- Override window navigation commands
+- Fix left pane to 250px
+- Create `deactivate`, `activate` concepts
+
+- Share common code between SimpleEditor / OniEditor
--- browser/src/Editor/SimpleNeovimEditor.tsx
+++ browser/src/Editor/SimpleNeovimEditor.tsx
@@ -6,7 +6,6 @@
 
 import * as React from "react"
 
-
 import { IncrementalDeltaRegionTracker } from "./../DeltaRegionTracker"
 import { NeovimInstance } from "./../neovim"
 import { CanvasRenderer, DOMRenderer, INeovimRenderer } from "./../Renderer"
