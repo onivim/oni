416f913ea73d7daeda12478c44e7f1902c04bf8b
extr0py <extr0py@extropygames.com>
2017-08-17 09:07:11 -0700

    Add 'EditorManager' interface

---
+++ browser/src/Services/EditorManager.ts
@@ -0,0 +1,14 @@
+/**
+ * EditorManager.ts
+ * 
+ * Responsible for managing state of the editor collection, and
+ * switching between active editors.
+ *
+ * It also provides convenience methods for hooking events
+ * to the active editor, and managing transitions between editors.
+ */
+
+export class EditorManager {
+}
+
+export const editorManager = new EditorManager()
