e13ceb120979035a13e8e14ec76b0b2eaf53ed44
extr0py <extr0py@extropygames.com>
2017-09-12 18:36:03 -0700

    Only log to console if --verbose is specified

--- main/src/Log.ts
+++ main/src/Log.ts
@@ -6,14 +6,20 @@
 
 const logs = []
 
+const isVerbose = process.argv.filter(arg => arg.indexOf("--verbose") >= 0).length > 0
+
 export const info = (msg: string): void => {
-    console.log(msg) // tslint:disable-line no-console
+    if (isVerbose) {
+        console.log(msg) // tslint:disable-line no-console
+    }
 
     logs.push(msg)
 }
 
 export const warn = (msg: string): void => {
-    console.warn(msg) // tslint:disable-line no-console
+    if (isVerbose) {
+        console.warn(msg) // tslint:disable-line no-console
+    }
 
     logs.push(msg)
 }
