87c0acfbe983ed2669b0eb9b282065d375e19fe4
extr0py <extr0py@extropygames.com>
2017-09-25 18:46:17 -0700

    Update to use 'key' instead of 'code'

--- browser/src/Input/Keyboard.ts
+++ browser/src/Input/Keyboard.ts
@@ -2,33 +2,6 @@ import { EventEmitter } from "events"
 
 import * as Log from "./../Log"
 
-// List taken from:
-// https://github.com/zeit/hyper/blob/7a08b1dc3e07ae552debfe7e62c48b0a5a028ff9/lib/utils/key-code.js
-const suppressShiftKeyCharacters = [
-  "~",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "+",
-  "{",
-  "}",
-  "|",
-  ":",
-  "'",
-  "\"",
-  "<",
-  ">",
-  "?",
-]
-
 export class Keyboard extends EventEmitter {
     constructor() {
         super()
@@ -70,7 +43,7 @@ export class Keyboard extends EventEmitter {
             evt.preventDefault()
         }
 
-        if (evt.shiftKey && suppressShiftKeyCharacters.indexOf(mappedKey) === -1) {
+        if (evt.shiftKey) {
             mappedKey = "s-" + mappedKey
         }
 
@@ -93,34 +66,25 @@ export class Keyboard extends EventEmitter {
 
     private _convertKeyEventToVimKey(evt: KeyboardEvent): null | string {
 
-        const keyCode: { [key: number]: string } = {
-            8: "bs",      // Backspace
-            9: "tab",     // Tab
-            13: "enter",   // Enter
-            27: "esc",     // Escape
-            33: "pageup",  // Page up
-            34: "pagedown", // Page down
-            35: "end",
-            36: "home",
-            37: "left",    // ArrowLeft
-            38: "up",      // ArrowUp
-            39: "right",   // ArrowRight
-            40: "down",    // ArrowDown
-            45: "insert",
-            114: "f3",
-            116: "f5",
-            123: "f12",
-            16: null,     // Shift left
-            17: null,     // Ctrl left
-            18: null,     // Alt left
-            19: null,     // Pause
-            20: null,     // Caps lock
-            145: null,     // Scroll lock
-            174: null,     // Volume up
-            175: null,     // Volume down
+        const keyCode: { [key: string]: string } = {
+            "Backspace": "bs",
+            "Tab": "tab",     // Tab
+            "ArrowLeft": "left",    // ArrowLeft
+            "ArrowUp": "up",      // ArrowUp
+            "ArrowRight": "right",   // ArrowRight
+            "ArrowDown": "down",    // ArrowDown
+            "Insert": "insert",
+            "Shift": null,     // Shift left
+            "Control": null,     // Ctrl left
+            "Alt": null,     // Alt left
+            "CapsLock": null,
+            "Pause": null,
+            "ScrollLock": null,     // Scroll lock
+            "AudioVolumeUp": null,     // Volume up
+            "AudioVolumeDown": null,     // Volume down
         }
 
-        return keyCode[evt.keyCode] ? keyCode[evt.keyCode] : evt.key
+        return keyCode[evt.key] ? keyCode[evt.key] : evt.key.toLowerCase()
 
     }
 }
