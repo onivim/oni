dadc69309a24c1dd153004085dfc7ee3dd4a170e
Bryan Phelps <bryphe@outlook.com>
2017-10-12 14:41:40 -0700

    Fix lint issue

--- browser/src/Services/QuickOpen/RipGrep.ts
+++ browser/src/Services/QuickOpen/RipGrep.ts
@@ -10,18 +10,14 @@ import * as Platform from "./../../Platform"
 
 export const getCommand = () => {
     const rootPath = path.join(__dirname, "node_modules", "vscode-ripgrep", "bin")
-
     const executableName = Platform.isWindows() ? "rg.exe" : "rg"
-
     return path.join(rootPath, executableName)
 }
 
 export const getArguments = (excludePaths: string[]) => {
-
     const ignoreArguments = excludePaths.reduce((prev, cur) => {
         return prev.concat(["-g", "!" + cur])
     }, [])
 
     return ["--files", "--hidden", "--case-sensitive"].concat(ignoreArguments)
 }
-
