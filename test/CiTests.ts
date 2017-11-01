0ab9502bd26bf552df04440d91966a1796e50d01
Bryan Phelps <bryphe@outlook.com>
2017-11-01 16:42:32 -0700

    Add automation API and factor tests out

---
+++ test/CiTests.ts
@@ -0,0 +1,46 @@
+import * as assert from "assert"
+import * as path from "path"
+
+import { Oni } from "./common"
+
+const LongTimeout = 5000
+
+const CiTests = [
+    "BasicEditingTest",
+    "QuickOpenTest",
+]
+
+describe("ci tests", function() { // tslint:disable-line only-arrow-functions
+    // Retry up to two times
+    this.retries(2)
+
+    let oni: Oni
+
+    beforeEach(async () => {
+        oni = new Oni()
+        return oni.start()
+    })
+
+    afterEach(async () => {
+        return oni.close()
+    })
+
+    CiTests.forEach((test) => {
+
+        const testPath = path.join(__dirname, "ci", test, ".js")
+
+        it("launches oni", async () => {
+            await oni.client.waitForExist(".editor", LongTimeout)
+            const text = await oni.client.getText(".editor")
+            assert(text && text.length > 0, "Validate editor element is present")
+
+            oni.client.execute("Oni.automation.runTest('" + testPath + "')")
+
+            await oni.client.waitForExist(".automated-test-result")
+            const resultText = await oni.client.getText(".automated-test-result")
+
+            const result = JSON.parse(resultText)
+            assert.ok(result.passed)
+        })
+    })
+})
