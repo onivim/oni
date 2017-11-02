d45f1f52d4881e2f1b88a13bab967cefea39a30b
Bryan Phelps <bryphe@outlook.com>
2017-11-02 11:40:48 -0700

    Automation: Add in-process test cases (#850)
    * Add automation API surface
    
    * Add QuickOpenTest
    
    * Add automation API and factor tests out
    
    * Add a basic editing test
    
    * Add some additional tests
    
    * Fix lint issues
    
    * Replace input binding with command, so that it is not impacted by platform-specific settings

---
+++ test/CiTests.ts
@@ -0,0 +1,51 @@
+import * as assert from "assert"
+import * as path from "path"
+
+import { Oni } from "./common"
+
+const LongTimeout = 5000
+
+const CiTests = [
+    "BasicEditingTest",
+    "QuickOpenTest",
+]
+
+describe("ci tests", function() { // tslint:disable-line only-arrow-functions
+    // Retry up to two times
+    this.retries(2)
+
+    let oni: Oni
+
+    beforeEach(async () => {
+        oni = new Oni()
+        return oni.start()
+    })
+
+    afterEach(async () => {
+        return oni.close()
+    })
+
+    CiTests.forEach((test) => {
+
+        const testPath = path.join(__dirname, "ci", test + ".js")
+        const normalizedTestPath = testPath.split("\\").join("/")
+
+        it("ci test: " + test, async () => {
+            await oni.client.waitForExist(".editor", LongTimeout)
+            const text = await oni.client.getText(".editor")
+            assert(text && text.length > 0, "Validate editor element is present")
+
+            console.log("Test path: " + normalizedTestPath) // tslint:disable-line
+
+            oni.client.execute("Oni.automation.runTest('" + normalizedTestPath + "')")
+
+            console.log("Waiting for result...") // tslint:disable-line
+            await oni.client.waitForExist(".automated-test-result")
+            const resultText = await oni.client.getText(".automated-test-result")
+            console.log("Got result: " + resultText) // tslint:disable-line
+
+            const result = JSON.parse(resultText)
+            assert.ok(result.passed)
+        })
+    })
+})
