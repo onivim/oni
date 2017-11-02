f766b0ea6f484cf7201faaa6ab1baba64a3cde68
Bryan Phelps <bryphe@outlook.com>
2017-11-02 15:30:13 -0700

    Fix completion utility tests

--- browser/test/Services/Language/Completion/CompletionUtilityTests.ts
+++ browser/test/Services/Language/Completion/CompletionUtilityTests.ts
@@ -1,37 +1,8 @@
 import * as assert from "assert"
 
-import * as AutoCompletionUtility from "./../../src/Services/AutoCompletionUtility"
+import * as CompletionUtility from "./../../../../src/Services/Language/Completion/CompletionUtility"
 
-describe("AutoCompletionUtility", () => {
-
-    // describe("replacePrefixWithCompletion", () => {
-    //     it("replaces with completion", () => {
-    //         const line = "window.setTim"
-    //         const cursorColumn = 12
-    //         const completion = "setTimeout"
-
-    //         const newLine = AutoCompletionUtility.replacePrefixWithCompletion(line, cursorColumn, completion)
-    //         assert.strictEqual(newLine, "window.setTimeout")
-    //     })
-
-    //     it("replaces with completion at beginning of line", () => {
-    //         const line = "wi"
-    //         const cursorColumn = 2
-    //         const completion = "window"
-
-    //         const newLine = AutoCompletionUtility.replacePrefixWithCompletion(line, cursorColumn, completion)
-    //         assert.strictEqual(newLine, "window")
-    //     })
-
-    //     it("replaces with completion where none of the items match", () => {
-    //         const line = "window."
-    //         const cursorColumn = 7
-    //         const completion = "Blob"
-
-    //         const newLine = AutoCompletionUtility.replacePrefixWithCompletion(line, cursorColumn, completion)
-    //         assert.strictEqual(newLine, "window.Blob")
-    //     })
-    // })
+describe("CompletionUtility", () => {
 
     describe("getCompletionStart", () => {
         it("rewinds back to first character", () => {
@@ -39,7 +10,7 @@ describe("AutoCompletionUtility", () => {
             const cursorColumn = 5
             const completion = "c"
 
-            const completionStart = AutoCompletionUtility.getCompletionStart(bufferLine, cursorColumn, completion)
+            const completionStart = CompletionUtility.getCompletionStart(bufferLine, cursorColumn, completion)
             assert.strictEqual(completionStart, 2)
         })
     })
