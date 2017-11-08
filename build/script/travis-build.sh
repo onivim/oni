aa7ca049ed9831374fb633cca8b0a3c3d43bf4c2
Bryan Phelps <bryphe@outlook.com>
2017-11-08 11:08:10 -0800

    Revert travis-build

--- build/script/travis-build.sh
+++ build/script/travis-build.sh
@@ -2,20 +2,6 @@
 
 echo Travis build - detected OS is: $TRAVIS_OS_NAME
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  export DISPLAY=:99.0
-  sh -e /etc/init.d/xvfb start
-  sleep 3
-
-  sudo apt-get --assume-yes install software-properties-common
-  sudo apt-get --assume-yes install python-software-properties
-  sudo add-apt-repository ppa:neovim-ppa/stable
-  sudo apt-get update
-  sudo apt-get --assume-yes install neovim
-
-  nvim --version
-fi
-
 node --version
 npm --version
 
@@ -24,4 +10,6 @@ npm run test:unit
 npm run lint
 npm run pack
 
-npm run test:integration
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    npm run test:integration
+fi
