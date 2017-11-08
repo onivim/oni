753d4af2a0220a4afebb5f7209e58e327b2ce254
Bryan Phelps <bryphe@outlook.com>
2017-11-08 08:57:56 -0800

    Add linux path

--- browser/src/neovim/NeovimProcessSpawner.ts
+++ browser/src/neovim/NeovimProcessSpawner.ts
@@ -19,9 +19,7 @@ export const startNeovim = (runtimePaths: string[], args: string[]): Session => 
 
     const nvimWindowsProcessPath = path.join(__dirname, "node_modules", "oni-neovim-binaries", "bin", "Neovim", "bin", "nvim.exe")
     const nvimMacProcessPath = path.join(__dirname, "node_modules", "oni-neovim-binaries", "bin", "nvim-osx64", "bin", "nvim")
-
-    // For Linux, assume there is a locally installed neovim
-    const nvimLinuxPath = "nvim"
+    const nvimLinuxPath = path.join(__dirname, "node_modules", "oni-neovim-binaries", "bin", "nvim.appimage")
 
     let nvimProcessPath = Platform.isWindows() ? nvimWindowsProcessPath : Platform.isMac() ? nvimMacProcessPath : nvimLinuxPath
 
