67160ea9800ed5fb18380480c6687a19fae12c32
Bryan Phelps <bryphe@outlook.com>
2017-11-13 16:20:34 -0800

    Add failing test to exercise #911

--- test/CiTests.ts
+++ test/CiTests.ts
@@ -1,4 +1,5 @@
 import * as assert from "assert"
+import * as fs from "fs"
 import * as path from "path"
 
 import { Oni } from "./common"
@@ -9,44 +10,88 @@ const CiTests = [
     "AutoCompletionTest",
     "BasicEditingTest",
     "QuickOpenTest",
+    "NoInstalledNeovim",
 ]
+
+import * as Platform from "./../browser/src/Platform"
+
+export interface ITestCase {
+    name: string
+    testPath: string
+    configPath: string
+}
+
+const normalizePath = (p) => p.split("\\").join("/")
+
+const loadTest = (testName: string): ITestCase => {
+    const ciPath = path.join(__dirname, "ci")
+    const testPath = path.join(ciPath, testName + ".js")
+
+    const testMeta = require(testPath)
+    const testDescription = testMeta.settings || {}
+
+    const normalizedMeta: ITestCase = {
+        name: testDescription.name || testName,
+        testPath: normalizePath(testPath),
+        configPath: testDescription.configPath ? normalizePath(path.join(ciPath, testDescription.configPath)) : "",
+    }
+
+    return normalizedMeta
+}
 
 describe("ci tests", function() { // tslint:disable-line only-arrow-functions
     // Retry up to two times
     this.retries(2)
 
-    let oni: Oni
-
-    beforeEach(async () => {
-        oni = new Oni()
-        return oni.start()
-    })
-
-    afterEach(async () => {
-        return oni.close()
-    })
-
     CiTests.forEach((test) => {
 
-        const testPath = path.join(__dirname, "ci", test + ".js")
-        const normalizedTestPath = testPath.split("\\").join("/")
+        describe(test, () => {
 
-        it("ci test: " + test, async () => {
-            await oni.client.waitForExist(".editor", LongTimeout)
-            const text = await oni.client.getText(".editor")
-            assert(text && text.length > 0, "Validate editor element is present")
+            const testCase = loadTest(test)
 
-            console.log("Test path: " + normalizedTestPath) // tslint:disable-line
+            let oni: Oni
 
-            oni.client.execute("Oni.automation.runTest('" + normalizedTestPath + "')")
+            beforeEach(async () => {
 
-            console.log("Waiting for result...") // tslint:disable-line
-            await oni.client.waitForExist(".automated-test-result", 30000)
-            const resultText = await oni.client.getText(".automated-test-result")
-            console.log("Got result: " + resultText) // tslint:disable-line
+                const configPath = path.join(Platform.getUserHome(), ".oni", "config.js")
 
-            const result = JSON.parse(resultText)
-            assert.ok(result.passed)
+                if (fs.existsSync(configPath)) {
+                    // console.log("--Removing existing config..")
+                    // fs.unlinkSync(configPath)
+                }
+
+                if (testCase.configPath) {
+                    console.log("Writing config from: " + testCase.configPath)
+                    const configContents = fs.readFileSync(testCase.configPath)
+                    console.log("Writing config to: " + configPath)
+                    fs.writeFileSync(configPath, configContents)
+                }
+
+                oni = new Oni()
+                return oni.start()
+            })
+
+            afterEach(async () => {
+                return oni.close()
+            })
+
+            it("ci test: " + test, async () => {
+                await oni.client.waitForExist(".editor", LongTimeout)
+                const text = await oni.client.getText(".editor")
+                assert(text && text.length > 0, "Validate editor element is present")
+
+                console.log("Test path: " + testCase.testPath) // tslint:disable-line
+
+                oni.client.execute("Oni.automation.runTest('" + testCase.testPath + "')")
+
+                console.log("Waiting for result...") // tslint:disable-line
+                await oni.client.waitForExist(".automated-test-result", 30000)
+                const resultText = await oni.client.getText(".automated-test-result")
+                console.log("Got result: " + resultText) // tslint:disable-line
+
+                const result = JSON.parse(resultText)
+                assert.ok(result.passed)
+            })
         })
     })
 })
