39ba9c94ce8dd61a6a2662b46de3688198670302
Bryan Phelps <bryphe@outlook.com>
2017-11-15 12:07:05 -0800

    Add HeroDemo + DemoTest file

---
+++ test/Demo.ts
@@ -0,0 +1,19 @@
+import * as assert from "assert"
+import * as path from "path"
+
+import { runInProcTest } from "./common"
+
+const DemoTests = [
+    "HeroDemo"
+]
+
+// tslint:disable:no-console
+
+describe("demo tests", function() { // tslint:disable-line only-arrow-functions
+    // Retry up to two times
+    this.retries(2)
+
+    DemoTests.forEach((test) => {
+        runInProcTest(path.join(__dirname, "demo"), test)
+    })
+})
