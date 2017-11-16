c28f64c1e4c75f636ba94347a6c77b7f8dce806f
Bryan Phelps <bryphe@outlook.com>
2017-11-15 17:56:00 -0800

    Add demo files

---
+++ test/demo/HeroScreenshot.ts
@@ -0,0 +1,63 @@
+/**
+ * Script for hero screenshot on Oni's website and github
+ */
+
+import * as assert from "assert"
+import * as os from "os"
+import * as path from "path"
+
+import { remote } from "electron"
+
+import { getDistPath, getRootPath } from "./DemoCommon"
+
+const getCompletionElement = () => {
+
+    const elements = document.body.getElementsByClassName("autocompletion")
+
+    if (!elements || !elements.length) {
+        return null
+    } else {
+        return elements[0]
+    }
+}
+
+export const test = async (oni: any) => {
+
+    // Use the `Completion.ts` file as the screenshot source
+    remote.getCurrentWindow().setSize(800, 600)
+
+    const outputPath = getDistPath()
+
+    oni.configuration.setValues({"recorder.outputPath": outputPath})
+
+    const filePath = path.join(getRootPath(), "browser", "src", "Services", "Language", "Completion", "Completion.ts")
+
+    oni.automation.sendKeys(":e test.py<CR>")
+
+    await oni.automation.sleep(500)
+
+    oni.automation.sendKeys(":e " + filePath + "<CR>")
+
+    await oni.automation.sleep(500)
+
+    oni.automation.sendKeys("/distinctUntil<CR>")
+
+    await oni.automation.sleep(500)
+
+    oni.automation.sendKeys("<ESC>zz")
+
+    await oni.automation.sleep(500)
+
+    oni.automation.sendKeys("o")
+    oni.automation.sendKeys(".m")
+
+    await oni.automation.sleep(500)
+
+    oni.automation.sendKeys("er")
+
+    await oni.automation.waitFor(() => getCompletionElement() !== null)
+
+    await oni.automation.sleep(500)
+
+    oni.recorder.takeScreenshot("demo-screenshot.png")
+}
