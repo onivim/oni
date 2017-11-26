a3da2c9876a5596c86f641df99c945d0a6539d56
Akin <akin.sowemimo@gmail.com>
2017-11-26 15:14:38 +0000

    Feature/incorporate shell env package (#934)
    * add shell-env module to source shell path
    
    * add typings for shell env
    add dependencies for shell env `thread sleep`
    and `spawn-sync`
    
    * Add async version of shellEnv call
    change call sites to await fn and typings to return Promise<T>
    Fix package version for shell env
    
    * fix version numbers for remaining shell env deps
    
    * Init tss server host with a pending promise
    
    Await promise prior to calling make tss
    server request
    
    * Remove try-thread-sleep
    Merge master
    
    * add submodules to shell-env branch
    
    * remove deleted (in master) oni.d.ts
    which seems to have survived the merge
    
    * update oni-api package
    
    * upgrade electron-builder with yarn
    add await to before all in ci test
    
    * refactor to use child process to find path
    
    * Remove shell env module
    
    * merge remote changes
    await `oni.start`
    [WIP] fix failing CI tests

--- browser/src/Plugins/Api/Process.ts
+++ browser/src/Plugins/Api/Process.ts
@@ -1,13 +1,17 @@
 import * as ChildProcess from "child_process"
+import * as util from "util"
 
 import * as Platform from "./../../Platform"
 import { configuration } from "./../../Services/Configuration"
+
+const exec = util.promisify(ChildProcess.exec)
+type ExecReturn = string | { stdout: string, stderr: string }
 
 const getPathSeparator = () => {
     return Platform.isWindows() ? ";" : ":"
 }
 
-const mergePathEnvironmentVariable = (currentPath: string, pathsToAdd: string[]): string => {
+const mergePathEnvironmentVariable = (currentPath: ExecReturn, pathsToAdd: string[]): ExecReturn => {
     if (!pathsToAdd || !pathsToAdd.length) {
         return currentPath
     }
@@ -19,7 +23,7 @@ const mergePathEnvironmentVariable = (currentPath: string, pathsToAdd: string[])
     return currentPath + separator + joinedPathsToAdd + separator
 }
 
-const mergeSpawnOptions = (originalSpawnOptions: ChildProcess.ExecOptions | ChildProcess.SpawnOptions): any => {
+const mergeSpawnOptions = async (originalSpawnOptions: ChildProcess.ExecOptions | ChildProcess.SpawnOptions): Promise<any> => {
     const requiredOptions = {
         env: {
             ...process.env,
@@ -27,7 +31,16 @@ const mergeSpawnOptions = (originalSpawnOptions: ChildProcess.ExecOptions | Chil
         },
     }
 
-    const existingPath = process.env.Path || process.env.PATH
+    let existingPath: ExecReturn
+
+    try {
+        const pathCommand = Platform.isWindows() ? "echo %PATH%" : "echo $PATH"
+        const path = await exec(pathCommand)
+
+        existingPath = path || process.env.Path || process.env.PATH
+    } catch (e) {
+        existingPath = process.env.Path || process.env.PATH
+    }
 
     requiredOptions.env.PATH = mergePathEnvironmentVariable(existingPath, configuration.getValue("environment.additionalPaths"))
 
@@ -41,8 +54,8 @@ const mergeSpawnOptions = (originalSpawnOptions: ChildProcess.ExecOptions | Chil
  * API surface area responsible for handling process-related tasks
  * (spawning processes, managing running process, etc)
  */
-export const execNodeScript = (scriptPath: string, args: string[] = [], options: ChildProcess.ExecOptions = {}, callback: (err: any, stdout: string, stderr: string) => void): ChildProcess.ChildProcess => {
-    const spawnOptions = mergeSpawnOptions(options)
+export const execNodeScript = async (scriptPath: string, args: string[] = [], options: ChildProcess.ExecOptions = {}, callback: (err: any, stdout: string, stderr: string) => void): Promise<ChildProcess.ChildProcess> => {
+    const spawnOptions = await mergeSpawnOptions(options)
     spawnOptions.env.ELECTRON_RUN_AS_NODE = 1
 
     const execOptions = [process.execPath, scriptPath].concat(args)
@@ -54,8 +67,8 @@ export const execNodeScript = (scriptPath: string, args: string[] = [], options:
 /**
  * Wrapper around `child_process.exec` to run using electron as opposed to node
  */
-export const spawnNodeScript = (scriptPath: string, args: string[] = [], options: ChildProcess.SpawnOptions = {}): ChildProcess.ChildProcess => {
-    const spawnOptions = mergeSpawnOptions(options)
+export const spawnNodeScript = async (scriptPath: string, args: string[] = [], options: ChildProcess.SpawnOptions = {}): Promise<ChildProcess.ChildProcess> => {
+    const spawnOptions = await mergeSpawnOptions(options)
     spawnOptions.env.ELECTRON_RUN_AS_NODE = 1
 
     const allArgs = [scriptPath].concat(args)
@@ -66,8 +79,8 @@ export const spawnNodeScript = (scriptPath: string, args: string[] = [], options
 /**
  * Spawn process - wrapper around `child_process.spawn`
  */
-export const spawnProcess = (startCommand: string, args: string[] = [], options: ChildProcess.SpawnOptions = {}): ChildProcess.ChildProcess => {
-    const spawnOptions = mergeSpawnOptions(options)
+export const spawnProcess = async (startCommand: string, args: string[] = [], options: ChildProcess.SpawnOptions = {}): Promise<ChildProcess.ChildProcess> => {
+    const spawnOptions = await mergeSpawnOptions(options)
 
     return ChildProcess.spawn(startCommand, args, spawnOptions)
     }
