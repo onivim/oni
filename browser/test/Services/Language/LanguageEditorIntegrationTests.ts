5a1ce7e46885a0595cf656b8c2345582a8f1745d
Bryan Phelps <bryphe@outlook.com>
2017-11-30 10:02:15 -0800

    Start creating unit test mocks

--- browser/test/Services/Language/LanguageEditorIntegrationTests.ts
+++ browser/test/Services/Language/LanguageEditorIntegrationTests.ts
@@ -7,7 +7,8 @@ import * as assert from "assert"
 // import * as types from "vscode-languageserver-types"
 
 import { Editor } from "./../../../src/Editor/Editor"
-import { LanguageEditorIntegration } from "./../../../src/Services/Language/LanguageEditorIntegration"
+import * as Language from "./../../../src/Services/Language"
+import { ICompletablePromise, createCompletablePromise } from "./../../../src/Utility"
 
 export class MockConfiguration {
 
@@ -23,13 +24,73 @@ export class MockConfiguration {
 }
 
 export class MockEditor extends Editor {
-    
+    public simulateModeChange(newMode: string): void {
+        this.setMode(newMode as any)
+    }
+
+    public simulateCursorMoved(line: number, column: number): void {
+    }
+
+    public simulateBufferEnter(buffer: MockBuffer): void {
+        this.notifyBufferEnter(buffer as any)
+    }
+}
+
+export class MockBuffer {
+}
+
+export class MockRequestor<T> {
+
+    private _completablePromises: ICompletablePromise<T>[] = []
+
+    public get(language: string, filePath: string, line: number, column: number): Promise<T> {
+
+        const newPromise = createCompletablePromise<T>()
+
+        this._completablePromises.push(newPromise)
+
+        return newPromise.promise
+    }
+
+    public resolve(val: T): void {
+        const firstPromise = this._completablePromises.shift()
+        firstPromise.resolve(val)
+    }
+
+    public reject(err?: Error): void {
+        const firstPromise = this._completablePromises.shift()
+        firstPromise.reject(err)
+    }
+}
+
+export class MockDefinitionRequestor extends MockRequestor<Language.IDefinitionResult> implements Language.IDefinitionRequestor {
+    public getDefinition(language: string, filePath: string, line: number, column: number): Promise<Language.IDefinitionResult> {
+        return this.get(language, filePath, line, column)
+    }
+}
+
+export class MockHoverRequestor extends MockRequestor<Language.IHoverResult> implements Language.IHoverRequestor {
+    public getHover(language: string, filePath: string, line: number, column: number): Promise<Language.IHoverResult> {
+        return this.get(language, filePath, line, column)
+    }
 }
 
 describe("LanguageEditorIntegration", () => {
 
     it("shows hover", () => {
-        assert.ok(false, "TODO")
+        const mockConfiguration = new MockConfiguration()
+        const mockEditor = new MockEditor()
+
+        const mockDefinitionRequestor = new MockDefinitionRequestor()
+        const mockHoverRequestor = new MockHoverRequestor()
+
+        const lei = new Language.LanguageEditorIntegration(mockEditor, mockConfiguration as any, null, mockDefinitionRequestor, mockHoverRequestor)
+
+        mockEditor.simulateModeChange("normal")
+        mockEditor.simulateBufferEnter(new MockBuffer())
+
+        lei.dispose()
+        assert.ok(false, "fails")
     })
 
     it("respects editor.quickInfo.delay setting for hover", () => {
@@ -48,7 +109,7 @@ describe("LanguageEditorIntegration", () => {
         const mockConfiguration = new MockConfiguration()
         const mockEditor = new MockEditor()
 
-        const lei = new LanguageEditorIntegration(mockEditor, mockConfiguration as any, null, null, null)
+        const lei = new Language.LanguageEditorIntegration(mockEditor, mockConfiguration as any, null, null, null)
 
         lei.dispose()
         assert.ok(false, "fails")
