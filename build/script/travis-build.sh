f9e462a9a9d990992960d1798118eb0d2fb64e99
Bryan Phelps <bryphe@outlook.com>
2017-11-30 16:44:46 -0800

    Move xvfb initialization to script, only run for linux

--- build/script/travis-build.sh
+++ build/script/travis-build.sh
@@ -5,6 +5,12 @@ echo Travis build - detected OS is: $TRAVIS_OS_NAME
 node --version
 npm --version
 
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  export DISPLAY=:99.0
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+fi
+
 npm run build
 npm run test:unit
 npm run lint
