7c39f00edd62a5a428ad96758c78b838266ac84c
Bryan Phelps <bryphe@outlook.com>
2017-12-01 18:19:29 -0800

    Add auto closing pairs tests

---
+++ test/ci/AutoClosingPairsTest.ts
@@ -0,0 +1,35 @@
+/**
+ * Test script to verify the scenario where no neovim is installed
+ *
+ * We should be showing a descriptive error message...
+ */
+
+import * as assert from "assert"
+import * as os from "os"
+import * as path from "path"
+
+export const test = async (oni: any) => {
+    const dir = os.tmpdir()
+    const testFileName = `testFile-${new Date().getTime()}.js`
+    const tempFilePath = path.join(dir, testFileName)
+    oni.automation.sendKeys(":e " + tempFilePath)
+    oni.automation.sendKeys("window.setTimeout(() => {<CR>")
+
+    await oni.automation.sleep(50)
+
+    const lines: string[]  = await oni.editors.activeEditor.activeBuffer.getLines(0, 2)
+
+    const expectedResult = [
+        "window.setTimeout(() => {",
+        "   ",
+        "})",
+    ]
+
+    assert.deepEqual(lines, expectedResult, "Verify lines are as expected")
+])
+
+}
+
+export const settings = {
+    configPath: "AutoClosingPairs.config.js",
+}
