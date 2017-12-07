703c607fbfe7d34cd735f2c65b29356efcfd490c
Bryan Phelps <bryphe@outlook.com>
2017-12-06 18:55:13 -0800

    Make progress on applying IconThemes

---
+++ browser/src/Services/FileIcon.tsx
@@ -0,0 +1,20 @@
+/**
+ * Icons
+ *
+ * - Data source for icons present in Oni
+ * - Loads icons based on the `ui.iconTheme` configuration setting
+ */
+
+import * as React from "react"
+
+import { IIconInfo } from "./Icons"
+
+export interface IFileIconProps {
+    icon: IIconInfo
+}
+
+export class FileIcon extends React.PureComponent<IFileIconProps, {}> {
+    public render(): JSX.Element {
+        return <div>{this.props.icon.fontCharacter}</div>
+    }
+}
