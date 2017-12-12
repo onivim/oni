966935069043a91ae43963c11f47dfef5fb4b6e1
Bryan Phelps <bryphe@outlook.com>
2017-12-11 18:09:39 -0800

    Add failing unit test that covers #1039

--- browser/test/Services/TypingPredictionManagerTests.ts
+++ browser/test/Services/TypingPredictionManagerTests.ts
@@ -4,6 +4,8 @@
 
 import * as assert from "assert"
 
+import * as Neovim from "./../../src/neovim"
+
 import { ITypingPrediction, TypingPredictionManager } from "./../../src/Services/TypingPredictionManager"
 import { MockScreen } from "./../Mocks/neovim"
 
@@ -41,5 +43,36 @@ describe("TypingPredictionManager", () => {
         assert.strictEqual(lastResult.predictedCursorColumn, 2, "Verify predictedCursorColumn is correct")
         assert.strictEqual(lastResult.predictedCharacters.length, 1, "Verify there is exactly one prediction in lastResult")
         assert.deepEqual(lastResult.predictedCharacters[0].character, "a", "Verify predicted character is correct")
+    })
+
+    it("#1039 - Doesn't add a prediction if it overlaps with an existing character on the screen", () => {
+        mockScreen.cursorRow = 1
+        mockScreen.cursorColumn = 1
+        typingPredictionManager.setCursorPosition(mockScreen)
+
+        // Set a character at the third position, so that we can test the case where there
+        // is a single prediction, and a rejected prediction (because there is a character already on the cell)
+
+        const cell: Neovim.ICell = {
+            character: "T",
+            characterWidth: 1,
+        }
+
+        mockScreen.setCell(3 /* x: column*/, 1 /* y: row */, cell)
+
+        let callCount = 0
+        let lastResult: ITypingPrediction = null
+
+        typingPredictionManager.onPredictionsChanged.subscribe((pd) => {
+            callCount++
+            lastResult = pd
+        })
+
+        typingPredictionManager.addPrediction("a") // First prediction should succeed
+        typingPredictionManager.addPrediction("b") // Second prediction doesn't succeed
+
+        assert.strictEqual(callCount, 1, "Verify only a single call to the prediction handler was made")
+        assert.strictEqual(lastResult.predictedCharacters.length, 1, "Verify there is exactly one prediction in last result")
+        assert.strictEqual(lastResult.predictedCharacters[0].character, "a", "Verify it was the first character entered that got predicted")
     })
 })
