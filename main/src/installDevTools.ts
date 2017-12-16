7f03ce97eb059bf23d72392b58c18cb3f29b1f4a
Akin909 <akin.sowemimo@gmail.com>
2017-12-16 22:41:21 +0000

    less asyncification in a misguided.. attempt to debug if that causing the lack of cool tools

--- main/src/installDevTools.ts
+++ main/src/installDevTools.ts
@@ -5,19 +5,20 @@
  */
 
 import * as Log from "./Log"
-(async () => {
+try {
+    // tslint:disable-next-line no-var-requires
+    const { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, default: installExtension } = require("electron-devtools-installer")
+    console.log('RREACT_DEVELOPER_TOOLS: ', REACT_DEVELOPER_TOOLS)
     try {
-        // const electronDevtoolsInstaller = require("electron-devtools-installer") // tslint:disable-line no-var-requires
-        const { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, installExtension } = require("electron-devtools-installer")
-        try {
-            const reduxExt = await installExtension(REDUX_DEVTOOLS)
-            Log.info(`Added extension: ${reduxExt}`)
-            const reactExt = await installExtension(REACT_DEVELOPER_TOOLS)
-            Log.info(`Added extension: ${reactExt}`)
-        } catch (e) {
-            Log.info(`An error occurred: ${e}`)
-        }
-    } catch (ex) {
-        Log.warn("Unable to install developer tools. `electron-devtools-installer` may not be available in this environment")
+        Promise.all([
+            installExtension(REDUX_DEVTOOLS),
+            installExtension(REACT_DEVELOPER_TOOLS),
+        ]).then((extensions) =>
+            extensions.forEach(ext => Log.info(`Added extension: ${ext}`)),
+        )
+    } catch (e) {
+        Log.info(`An error occurred: ${e}`)
+    }
+} catch (ex) {
+    Log.warn("Unable to install developer tools. `electron-devtools-installer` may not be available in this environment")
 }
-})()
--- main/src/main.ts
+++ main/src/main.ts
@@ -51,8 +51,8 @@ if (!isDevelopment && !isDebug) {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
-    app.on("ready", () => {
-        require("./installDevTools")
+    app.on("ready", async () => {
+        await import("./installDevTools")
         loadFileFromArguments(process.platform, process.argv, process.cwd())
     })
 }
