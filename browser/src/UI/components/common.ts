846670e6f712b0824779840ba3f03e957e0db2f3
Akin909 <akin.sowemimo@gmail.com>
2017-12-17 15:06:05 +0000

    actually add common file

---
+++ browser/src/UI/components/common.ts
@@ -0,0 +1,7 @@
+import { StyledFunction } from "styled-components"
+
+export function withProps<T, U extends HTMLElement = HTMLElement>(
+    styledFunction: StyledFunction<React.HTMLProps<U>>,
+): StyledFunction<T & React.HTMLProps<U>> {
+    return styledFunction
+}
