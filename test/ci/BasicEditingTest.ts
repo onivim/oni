c618f15b564ed83b0e6a75715dca7b5b7fdb1b5d
Bryan Phelps <bryphe@outlook.com>
2017-12-19 19:46:54 -0800

    Add fix for BasicEditingTest

--- test/ci/BasicEditingTest.ts
+++ test/ci/BasicEditingTest.ts
@@ -3,12 +3,13 @@
  */
 
 export const test = async (oni: any) => {
-    oni.automation.sendKeys(":new<CR>")
-    await oni.automation.sleep()
+    oni.automation.sendKeys(":new test.txt<CR>")
 
-    oni.automation.sendKeys("ihelloworld")
+    await oni.automation.sleep(3000)
 
-    oni.automation.waitFor(() => {
+    oni.automation.sendKeys("ihelloworld2")
+
+    await oni.automation.waitFor(() => {
         const lines = oni.editors.activeEditor.activeBuffer.getLines()
         return lines && lines.length && lines[0] === "helloworld2"
     })
