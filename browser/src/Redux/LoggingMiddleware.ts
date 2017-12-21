67df6f2408bd9a36dddebb3b98f523aff02df99f
Bryan Phelps <bryphe@outlook.com>
2017-12-20 16:33:21 -0800

    Remove unnecessary logging, now that we have Redux devtools

--- browser/src/Redux/LoggingMiddleware.ts
+++ browser/src/Redux/LoggingMiddleware.ts
@@ -11,16 +11,7 @@ import * as Log from "./../Log"
 export const createLoggingMiddleware = (storeName: string) => (store: Store<any>) => (next: any) => (action: any): any => {
     Log.verbose("[REDUX - " + storeName + "][ACTION] " + action.type)
 
-    if (Log.isDebugLoggingEnabled()) {
-        console.dir(action) // tslint:disable-line
-    }
-
     const result = next(action)
-
-    if (Log.isDebugLoggingEnabled()) {
-        Log.debug("[REDUX - " + storeName + "] New State: ")
-        console.dir(store.getState()) // tslint:disable-line
-    }
 
     return result
 }
