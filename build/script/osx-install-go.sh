91a81b9fe31a12e2389c21e50ab6f3ac58989371
Bryan Phelps <bryphe@outlook.com>
2017-12-22 19:27:35 -0800

    Add path checks

--- build/script/osx-install-go.sh
+++ build/script/osx-install-go.sh
@@ -8,4 +8,10 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
     go version
     go get -u github.com/sourcegraph/go-langserver
+
+    echo "Checking go path..."
+    which go
+
+    echo "Checking go-langserver path..."
+    which go-langserver
 fi
