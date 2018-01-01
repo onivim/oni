8531e850658ef250ebfc55ac3c9ecc70f0978c9f
Bryan Phelps <bryphe@outlook.com>
2018-01-01 12:08:17 -0800

    Hook up open url command

--- extensions/oni-layer-browser/src/index.ts
+++ extensions/oni-layer-browser/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World")
+
+
+// import * as Oni from "oni-api"
+
+import { shell } from "electron"
+
+export const activate = (oni: any) => {
+
+    const openUrl = (url: string) => {
+        shell.openExternal(url)
+    }
+
+    oni.commands.registerCommand({
+        command: "browser.openUrl",
+        execute: openUrl,
+    })
+}
