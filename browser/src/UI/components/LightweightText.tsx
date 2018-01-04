3915e4965530849e3417830ac6e78c760d589070
Bryan Phelps <bryphe@outlook.com>
2018-01-03 17:42:22 -0800

    Add 'ctrl-u', 'ctrl-h', and 'ctrl-w' emulation in TextInputView

--- browser/src/UI/components/LightweightText.tsx
+++ browser/src/UI/components/LightweightText.tsx
@@ -7,6 +7,7 @@ import { focusManager } from "./../../Services/FocusManager"
 
 export interface IToolTipsViewProps {
     onComplete?: (result: string) => void
+    onChange?: (evt: React.ChangeEvent<HTMLInputElement>) => void
 
     defaultValue?: string
 
@@ -14,6 +15,9 @@ export interface IToolTipsViewProps {
     foregroundColor: string
 
 }
+
+// TODO: Is there a better value for this?
+const WordRegex = /[$_a-zA-Z0-9]/i
 
 export class TextInputView extends React.PureComponent<IToolTipsViewProps, {}> {
 
@@ -43,10 +47,60 @@ export class TextInputView extends React.PureComponent<IToolTipsViewProps, {}> {
         const defaultValue = this.props.defaultValue || ""
 
         return <div style={containerStyle}><input type="text"
-                    style={inputStyle}
-                    placeholder={defaultValue}
-                    onFocus={(evt) => evt.currentTarget.select()}
-                    ref={(elem) => this._element = elem} /></div>
+            style={inputStyle}
+            placeholder={defaultValue}
+            onKeyDown={(evt) => this._onKeyDown(evt)}
+            onChange={(evt) => this._onChange(evt)}
+            onFocus={(evt) => evt.currentTarget.select()}
+            ref={(elem) => this._element = elem} /></div>
+    }
+
+    private _onChange(changeEvent: React.ChangeEvent<HTMLInputElement>): void {
+        if (this.props.onChange) {
+            this.props.onChange(changeEvent)
+        }
+    }
+
+    private _onKeyDown(keyboardEvent: React.KeyboardEvent<HTMLInputElement>): void {
+
+        if (this._element && keyboardEvent.ctrlKey) {
+
+            switch (keyboardEvent.key) {
+                case "u":
+                    {
+                        this._element.value = ""
+                        break
+                    }
+                case "h":
+                    {
+                        const previousValue = this._element.value
+
+                        if (previousValue.length > 0) {
+                            this._element.value = previousValue.substring(0, previousValue.length - 1)
+                        }
+                        break
+                    }
+                case "w":
+                    {
+                        const previousValue = this._element.value
+                        let idx = previousValue.length - 1
+
+                        while(idx > 0) {
+                            if (!previousValue[idx].match(WordRegex)) {
+                                break
+                            }
+
+                            idx--
+                        }
+
+                        this._element.value = previousValue.substring(0, idx)
+                    }
+                    break
+                default:
+                    return
+
+            }
+        }
     }
 
     public componentWillUnmount(): void {
