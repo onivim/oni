97f7a83121e92a28d180ca3ab90388306d716e41
Bryan Phelps <bryphe@outlook.com>
2018-01-04 14:01:28 -0800

    Factor sidebar items out

--- browser/src/Services/Sidebar/index.ts
+++ browser/src/Services/Sidebar/index.ts
@@ -4,10 +4,10 @@ import { editorManager } from "./../../Services/EditorManager"
 import { windowManager } from "./../../Services/WindowManager"
 import { workspace } from "./../../Services/Workspace"
 
+import { ExplorerSplit } from "./../Explorer/ExplorerSplit"
 import { SidebarContentSplit } from "./SidebarContentSplit"
 import { SidebarSplit } from "./SidebarSplit"
 import { SidebarManager } from "./SidebarStore"
-import { ExplorerSplit } from "./../Explorer/ExplorerSplit"
 
 let _sidebarManager: SidebarManager = null
 
@@ -19,21 +19,23 @@ export const activate = (configuration: Configuration) => {
         const leftDock = windowManager.getDock(2)
         leftDock.addSplit(new SidebarSplit(_sidebarManager))
         leftDock.addSplit(new SidebarContentSplit(_sidebarManager))
+
+        // Sidebar items
+        // TODO: Move to extensions
+        _sidebarManager.add("files-o", new ExplorerSplit(configuration, workspace, commandManager, editorManager))
+
+        _sidebarManager.add("search", {
+            id: "search",
+            title: "Search",
+            render: () => null,
+        })
+
+        _sidebarManager.add("th", {
+            id: "plugins",
+            title: "Plugins",
+            render: () => null,
+        })
     }
-
-    _sidebarManager.add("files-o", new ExplorerSplit(configuration, workspace, commandManager, editorManager))
-
-    _sidebarManager.add("search", {
-        id: "search",
-        title: "Search",
-        render: () => null
-    })
-
-    _sidebarManager.add("th", {
-        id: "plugins",
-        title: "Plugins",
-        render: () => null
-    })
 }
 
 export const getInstance = (): SidebarManager => _sidebarManager
