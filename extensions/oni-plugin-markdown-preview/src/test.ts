443c374d629e29852b20b5fe74adbd315de019e4
Tal Amuyal <TalAmuyal@gmail.com>
2018-01-06 15:02:59 +0200

    Add test examples for the Markdown-preview plugin

--- extensions/oni-plugin-markdown-preview/src/test.ts
+++ extensions/oni-plugin-markdown-preview/src/test.ts
@@ -1,233 +1,42 @@
-//import * as assert from "assert"
+import * as assert from "assert"
 
 import * as MarkdownPreview from "./index"
 
 import * as OniApi from "oni-api"
 
 export const test = (oni: OniApi.Plugin.Api): void => {
-    it("no error", () => {
-        //
-    })
-    //throw Error("This is my test")
-    //MarkdownPreview.activate(oni)
-}
+    //const splitRoot = oni.windows.splitRoot;
 
-/*
-describe("MarkdownPreview", () => {
+    describe("initially", () => {
+        it("has only one split", () => {
+            //assert.strictEqual(splitRoot.leaves.length, 1)
+        })
+    })
+
     describe("activate", () => {
-        it("returns empty if the string doesn't have whitespace", () => {
-            const result = AutoClosingPairs.getWhiteSpacePrefix("test")
-            assert.strictEqual(result, "")
-        })
+        MarkdownPreview.activate(oni)
 
-        it("returns tab", () => {
-            const result = AutoClosingPairs.getWhiteSpacePrefix("\ttest")
-            assert.strictEqual(result, "\t")
-        })
+        describe("open an empty markdown file", () => {
+            oni.automation.sendKeys(":e markdown-preview-test.md")
 
-        it("returns spaces", () => {
-            const result = AutoClosingPairs.getWhiteSpacePrefix("  test")
-            assert.strictEqual(result, "  ")
-        })
-    })
-})
-*/
+            it("opens a new preview split", () => {
+                //assert.strictEqual(splitRoot.leaves.length, 2)
+            })
 
+            describe("the markdown-preview split", () => {
+                it("has no text", () => {
+                    // assert.strictEqual(splitRoot.leaves[1].content.text.trim(), "")
+                })
+            })
 
-
-/*
-describe(`PLUGIN ${plugin._oniPluginMetadata.name}`, () => {
-    it("getCell returns correct value", () => {
-        const grid = new Grid<string>()
-
-        grid.setCell(1, 1, "a")
-
-        assert.strictEqual(grid.getCell(0, 0), null)
-        assert.strictEqual(grid.getCell(1, 1), "a")
-    })
-
-    it("width and height are 0 by default", () => {
-        const grid = new Grid<void>()
-
-        assert.strictEqual(grid.width, 0)
-        assert.strictEqual(grid.height, 0)
-    })
-
-    describe("shift", () => {
-        it("shift(0) does not shift", () => {
-            const val = [
-                [1, 2],
-                [3, 4],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-            startGrid.shiftRows(0)
-            assertGridValues(startGrid, val)
-        })
-
-        it("shift(1) shifts upwards", () => {
-            const val = [
-                [1, 2],
-                [3, 4],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-            startGrid.shiftRows(1)
-
-            const expectedOutput = [
-                [3, 4],
-                [null, null],
-            ]
-            assertGridValues(startGrid, expectedOutput)
-        })
-
-        it("shift(-1) shifts downwards", () => {
-            const val = [
-                [1, 2],
-                [3, 4],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-            startGrid.shiftRows(-1)
-
-            const expectedOutput = [
-                [null, null],
-                [1, 2],
-            ]
-            assertGridValues(startGrid, expectedOutput)
+            describe("with Markdown title", () => {
+                oni.automation.sendKeys("i# Title 1")
+                it("has a Header element", () => {
+                    // const headers = splitRoot.leaves[1].content.getElements("h1")
+                    // assert.strictEqual(headers.length, 1)
+                    // assert.strictEqual(headers[0].text.trim(), "Title 1")
+                })
+            })
         })
     })
-
-    describe("setRegion", () => {
-        it("sets value", () => {
-
-            const val = [
-                [1, 2],
-                [3, 4],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-
-            startGrid.setRegion(0, 0, 2, 2, 5)
-
-            const expectedVal = [
-                [5, 5],
-                [5, 5],
-            ]
-
-            assertGridValues(startGrid, expectedVal)
-        })
-
-        it("sets null correctly", () => {
-            const val = [
-                [1, 2, 3],
-                [3, 4, 5],
-                [6, 7, 8],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-
-            startGrid.setRegion(0, 0, 2, 2, null)
-
-            const expectedOuput = [
-                [null, null, 3],
-                [null, null, 5],
-                [6, 7, 8],
-            ]
-
-            assertGridValues(startGrid, expectedOuput)
-        })
-    })
-
-    describe("cloneRegion", () => {
-        it("returns new grid for single item", () => {
-            const val = [
-                [1, 2],
-                [3, 4],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-
-            const topLeftGrid = startGrid.cloneRegion(0, 0, 1, 1)
-            assert.strictEqual(topLeftGrid.width, 1)
-            assert.strictEqual(topLeftGrid.height, 1)
-            assert.strictEqual(topLeftGrid.getCell(0, 0), 1)
-
-            const bottomRightGrid = startGrid.cloneRegion(1, 1, 1, 1)
-            assert.strictEqual(bottomRightGrid.width, 1)
-            assert.strictEqual(bottomRightGrid.height, 1)
-            assert.strictEqual(bottomRightGrid.getCell(0, 0), 4)
-        })
-
-        it("returns square subsection", () => {
-            const val = [
-                [1, 2, 3],
-                [3, 4, 5],
-                [6, 7, 8],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-
-            const topLeftGrid = startGrid.cloneRegion(0, 0, 2, 2)
-            assert.strictEqual(topLeftGrid.width, 2)
-            assert.strictEqual(topLeftGrid.height, 2)
-
-            const expectedOutput = [
-                [1, 2],
-                [3, 4],
-            ]
-
-            assertGridValues(topLeftGrid, expectedOutput)
-        })
-
-        it("handles null & undefined correctly", () => {
-            const val = [
-                [null, undefined, 3],
-                [0, 1, 5],
-                [6, 7, 8],
-            ]
-
-            const startGrid = createGridFromNestedArray(val)
-            const outGrid = startGrid.cloneRegion(0, 0, 2, 2)
-
-            const expectedOutput = [
-                [null, null],
-                [0, 1],
-            ]
-
-            assertGridValues(outGrid, expectedOutput)
-        })
-    })
-})
-
-function createGridFromNestedArray<T>(array: T[][]): Grid<T> {
-
-    const grid = new Grid<T>()
-
-    for (let row = 0; row < array.length; row++) {
-        const rowItems = array[row]
-
-        for (let col = 0; col < rowItems.length; col++) {
-            const colItem = rowItems[col]
-
-            grid.setCell(col, row, colItem)
-        }
-    }
-
-    return grid
 }
-
-function assertGridValues<T>(grid: Grid<T>, array: T[][]): void {
-
-    for (let row = 0; row < array.length; row++) {
-        const rowItems = array[row]
-
-        for (let col = 0; col < rowItems.length; col++) {
-            // var colItem = rowItems[col]
-
-            const item = grid.getCell(col, row)
-            assert.strictEqual(item, array[row][col], `Validate item at row: ${row} and ${col}`)
-        }
-    }
-}
-*/
