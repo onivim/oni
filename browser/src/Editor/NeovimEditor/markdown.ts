40b135717af3bb70560d362950de38332f4961da
Akin909 <akin.sowemimo@gmail.com>
2018-01-07 22:30:16 +0000

    add markdown to editor - temp

---
+++ browser/src/Editor/NeovimEditor/markdown.ts
@@ -0,0 +1,15 @@
+import * as Highlight from "highlight.js"
+import * as marked from "marked"
+
+export const convertMarkdown = (markdown: string): { __html: string } => {
+    marked.setOptions({
+        sanitize: true,
+        gfm: true,
+        highlight: (code) => {
+            return Highlight.highlightAuto(code).value
+        },
+    })
+
+    const html = marked(markdown)
+    return { __html: html }
+}
