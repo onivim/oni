47263fec7ec8dfd1a6457ca20f3a19962e28ea71
Akin909 <akin.sowemimo@gmail.com>
2018-01-07 23:20:11 +0000

    remove highlight.js

--- browser/src/Editor/NeovimEditor/markdown.ts
+++ browser/src/Editor/NeovimEditor/markdown.ts
@@ -1,13 +1,13 @@
-import * as Highlight from "highlight.js"
+// import * as Highlight from "highlight.js"
 import * as marked from "marked"
 
 export const convertMarkdown = (markdown: string): { __html: string } => {
     marked.setOptions({
         sanitize: true,
         gfm: true,
-        highlight: (code) => {
-            return Highlight.highlightAuto(code).value
-        },
+        // highlight: (code) => {
+        //     return Highlight.highlightAuto(code).value
+        // },
     })
 
     const html = marked(markdown)
