f74355a3e725550386d522c126acdbb92a8312a4
Bryan Phelps <bryphe@outlook.com>
2018-01-09 13:53:47 -0800

    Extract out filesystem dependency for testability

---
+++ browser/src/Services/Explorer/ExplorerFileSystem.ts
@@ -0,0 +1,46 @@
+/**
+ * ExplorerFileSystem.ts
+ *
+ * State management for the explorer split
+ */
+
+import * as fs from "fs"
+import * as path from "path"
+
+import { FolderOrFile } from "./ExplorerStore"
+
+/**
+ * An abstraction of the node filesystem APIs to enable testing
+ */
+export interface IFileSystem {
+    readdir(fullPath: string): Promise<FolderOrFile[]>
+}
+
+export class FileSystem implements IFileSystem {
+
+    constructor(
+        private _fs: typeof fs
+    ) {}
+
+    public readdir(directoryPath: string): Promise<FolderOrFile[]> {
+        const files = this._fs.readdirSync(directoryPath)
+
+        const filesAndFolders = files.map((f) => {
+            const fullPath = path.join(directoryPath, f)
+            const stat = this._fs.statSync(directoryPath)
+            if (stat.isDirectory()) {
+                return {
+                    type: "folder",
+                    fullPath,
+                } as FolderOrFile
+            } else {
+                return {
+                    type: "file",
+                    fullPath,
+                } as FolderOrFile
+            }
+        })
+
+        return Promise.resolve(filesAndFolders)
+    }
+}
---
+++ browser/test/Services/Explorer/ExplorerStoreTests.ts
@@ -0,0 +1,86 @@
+/**
+ * ExplorerStoreTests.ts
+ */
+
+import * as assert from "assert"
+import * as ExplorerSelectors from "./../../../src/Services/Explorer/ExplorerSelectors"
+import * as ExplorerState from "./../../../src/Services/Explorer/ExplorerStore"
+
+const createTestFile = (filePath: string): ExplorerState.IFileState => ({
+    type: "file",
+    fullPath: filePath,
+})
+
+describe("ExplorerSelectors", () => {
+    describe("flattenFolderTree", () => {
+
+        it("flattens a single file", () => {
+
+            const file: ExplorerState.IFileState = createTestFile("/test/files/testFile.txt")
+
+            const result = ExplorerSelectors.flattenFolderTree(file, [], {}, 1)
+
+            const expectedResult = [{
+                id: "explorer:/test/files/testFile.txt",
+                type: "file",
+                filePath: "/test/files/testFile.txt",
+                name: "testFile.txt",
+                modified: false,
+                indentationLevel: 1,
+            }]
+
+            assert.deepEqual(result, expectedResult)
+        })
+
+        it("flattens a single folder", () => {
+            const folder: ExplorerState.IFolderState = {
+                type: "folder",
+                fullPath: "/test/folder/folderPath",
+            }
+
+            const result = ExplorerSelectors.flattenFolderTree(folder, [], {}, 1)
+
+            const expectedResult = [{
+                type: "folder",
+                id: "explorer:/test/folder/folderPath",
+                folderPath: "/test/folder/folderPath",
+                name: "folderPath",
+                expanded: false,
+                indentationLevel: 1,
+            }]
+
+            assert.deepEqual(result, expectedResult)
+        })
+
+        it("flattens a folder with subfiles", () => {
+            const folder: ExplorerState.IFolderState = {
+                type: "folder",
+                fullPath: "/test/folder1/folderPath",
+            }
+
+            const expandedFolders = {
+                "/test/folder1/folderPath": [ createTestFile("/test/files/file1")],
+            }
+
+            const result = ExplorerSelectors.flattenFolderTree(folder, [], expandedFolders, 1)
+
+            const expectedResult = [{
+                type: "folder",
+                id: "explorer:/test/folder1/folderPath",
+                folderPath: "/test/folder1/folderPath",
+                name: "folderPath",
+                expanded: true,
+                indentationLevel: 1,
+            }, {
+                type: "file",
+                id: "explorer:/test/files/file1",
+                filePath: "/test/files/file1",
+                name: "file1",
+                modified: false,
+                indentationLevel: 2,
+            }]
+
+            assert.deepEqual(result, expectedResult)
+        })
+    })
+})
