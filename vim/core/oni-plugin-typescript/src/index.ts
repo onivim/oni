4c1e42402b5f92ec8897edee0fdaed2a8402eae9
Bryan Phelps <bryphe@outlook.com>
2018-01-10 08:40:58 -0800

    Fix typescript server starting immediately on activate, defer initialization until first request

--- vim/core/oni-plugin-typescript/src/index.ts
+++ vim/core/oni-plugin-typescript/src/index.ts
@@ -28,7 +28,66 @@ import * as Utility from "./Utility"
 
 export const activate = (oni: Oni.Plugin.Api) => {
 
-    const host = new TypeScriptServerHost(oni)
+    let _host: TypeScriptServerHost = null
+
+    const initializeHost = (host: TypeScriptServerHost) => {
+        host.on("semanticDiag", (diagnostics) => {
+            const fileName = diagnostics.file
+
+            const diags = diagnostics.diagnostics || []
+
+            const errors = diags.map((d) => {
+                // Convert lines to zero-based to accomodate protocol
+                const startPosition = types.Position.create(d.start.line - 1, d.start.offset - 1)
+                const endPosition = types.Position.create(d.end.line - 1, d.end.offset - 1)
+                const range = types.Range.create(startPosition, endPosition)
+
+                const ret: types.Diagnostic =  {
+                    // type: null,
+                    code: d.code,
+                    message: d.text,
+                    range,
+                    severity: types.DiagnosticSeverity.Error,
+                }
+                return ret
+            })
+
+            const language = Utility.getLanguageFromFileName(fileName)
+
+            connection.notify("textDocument/publishDiagnostics", language, {
+                uri: oni.language.wrapPathInFileUri(fileName),
+                diagnostics: errors,
+            })
+        })
+
+        connection.subscribeToRequest("completionItem/resolve", getCompletionDetails(host))
+
+        connection.subscribeToNotification("textDocument/didOpen", protocolOpenFile(host))
+        connection.subscribeToNotification("textDocument/didChange", protocolChangeFile(host))
+        connection.subscribeToNotification("textDocument/didSave", onSaved(host))
+
+        connection.subscribeToRequest("textDocument/completion", getCompletions(oni, host))
+        connection.subscribeToRequest("textDocument/codeAction", getCodeActions(oni, host))
+        connection.subscribeToRequest("textDocument/definition", getDefinition(oni, host))
+        connection.subscribeToRequest("textDocument/hover",  getQuickInfo(oni, host))
+        connection.subscribeToRequest("textDocument/rangeFormatting", formatRange(oni, host))
+        connection.subscribeToRequest("textDocument/references",  findAllReferences(oni, host))
+        connection.subscribeToRequest("textDocument/rename",  doRename(oni, host))
+        connection.subscribeToRequest("textDocument/signatureHelp",  getSignatureHelp(oni, host))
+        connection.subscribeToRequest("textDocument/documentSymbol", getDocumentSymbols(oni, host))
+
+        connection.subscribeToRequest("workspace/executeCommand", executeCommand(connection, oni, host))
+        connection.subscribeToRequest("workspace/symbol", getWorkspaceSymbols(oni, host))
+    }
+
+    const host = () => {
+        if (!_host) {
+            _host = new TypeScriptServerHost(oni)
+            initializeHost(_host)
+        }
+
+        return _host
+    }
 
     const _lightweightLanguageClient = new LightweightLanguageClient()
     const jsLspCommand = oni.configuration.getValue("language.javascript.languageServer.command")
@@ -47,36 +106,7 @@ export const activate = (oni: Oni.Plugin.Api) => {
 
     const connection = new LanguageConnection(_lightweightLanguageClient)
 
-    host.on("semanticDiag", (diagnostics) => {
-        const fileName = diagnostics.file
-
-        const diags = diagnostics.diagnostics || []
-
-        const errors = diags.map((d) => {
-            // Convert lines to zero-based to accomodate protocol
-            const startPosition = types.Position.create(d.start.line - 1, d.start.offset - 1)
-            const endPosition = types.Position.create(d.end.line - 1, d.end.offset - 1)
-            const range = types.Range.create(startPosition, endPosition)
-
-            const ret: types.Diagnostic =  {
-                // type: null,
-                code: d.code,
-                message: d.text,
-                range,
-                severity: types.DiagnosticSeverity.Error,
-            }
-            return ret
-        })
-
-        const language = Utility.getLanguageFromFileName(fileName)
-
-        connection.notify("textDocument/publishDiagnostics", language, {
-            uri: oni.language.wrapPathInFileUri(fileName),
-            diagnostics: errors,
-        })
-    })
-
-    const protocolOpenFile = (message: string, payload: any) => {
+    const protocolOpenFile = (host:TypeScriptServerHost) => (message: string, payload: any) => {
         const textDocument: any = payload.textDocument
         const filePath = oni.language.unwrapFileUriPath(textDocument.uri)
 
@@ -100,7 +130,7 @@ export const activate = (oni: Oni.Plugin.Api) => {
         return str.replace(/(\r\n|\n|\r)/gm, "")
     }
 
-    const protocolChangeFile = async (message: string, payload: any) => {
+    const protocolChangeFile = (host: TypeScriptServerHost) => async (message: string, payload: any) => {
 
         const textDocument: types.TextDocumentIdentifier = payload.textDocument
         const contentChanges: types.TextDocumentContentChangeEvent[] = payload.contentChanges
@@ -117,9 +147,9 @@ export const activate = (oni: Oni.Plugin.Api) => {
 
         const change = contentChanges[0]
         if (!change.range) {
-            host.updateFile(filePath, change.text)
+            host().updateFile(filePath, change.text)
         } else if (isSingleLineChange(change.range) && change.text) {
-            host.changeLineInFile(filePath, change.range.start.line + 1, removeNewLines(change.text))
+            host().changeLineInFile(filePath, change.range.start.line + 1, removeNewLines(change.text))
         } else {
             oni.log.warn("Unhandled change request!")
         }
@@ -133,28 +163,10 @@ export const activate = (oni: Oni.Plugin.Api) => {
         host.getErrors(filePath)
     }
 
-    const onSaved = (protocolName: string, payload: any) => {
+    const onSaved = (host: TypeScriptServerHost) => (protocolName: string, payload: any) => {
         const textDocument = payload.textDocument
         const filePath = oni.language.unwrapFileUriPath(textDocument.uri)
         host.getErrorsAcrossProject(filePath)
     }
 
-    connection.subscribeToRequest("completionItem/resolve", getCompletionDetails(host))
-
-    connection.subscribeToNotification("textDocument/didOpen", protocolOpenFile)
-    connection.subscribeToNotification("textDocument/didChange", protocolChangeFile)
-    connection.subscribeToNotification("textDocument/didSave", onSaved)
-
-    connection.subscribeToRequest("textDocument/completion", getCompletions(oni, host))
-    connection.subscribeToRequest("textDocument/codeAction", getCodeActions(oni, host))
-    connection.subscribeToRequest("textDocument/definition", getDefinition(oni, host))
-    connection.subscribeToRequest("textDocument/hover",  getQuickInfo(oni, host))
-    connection.subscribeToRequest("textDocument/rangeFormatting", formatRange(oni, host))
-    connection.subscribeToRequest("textDocument/references",  findAllReferences(oni, host))
-    connection.subscribeToRequest("textDocument/rename",  doRename(oni, host))
-    connection.subscribeToRequest("textDocument/signatureHelp",  getSignatureHelp(oni, host))
-    connection.subscribeToRequest("textDocument/documentSymbol", getDocumentSymbols(oni, host))
-
-    connection.subscribeToRequest("workspace/executeCommand", executeCommand(connection, oni, host))
-    connection.subscribeToRequest("workspace/symbol", getWorkspaceSymbols(oni, host))
 }
