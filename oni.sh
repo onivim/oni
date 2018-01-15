b929b75509ae74210a1e7316e5bc43331bb69e8f
Akin909 <akin.sowemimo@gmail.com>
2018-01-15 20:40:32 +0000

    revert changes to oni.sh

--- oni.sh
+++ oni.sh
@@ -16,13 +16,13 @@ OPEN_DIRECTORY="$PWD"
 # test if $self is a symlink
 if [ -L "$self" ] ; then
    # readlink returns the path to the file the link points to:
-   target=$(readlink "$self")
+   target=`readlink $self`
 else
    target=$self
 fi
 
-ONI_PATH=$(dirname "$target")
+ONI_PATH=`dirname $target`
 
 FULL_ONI_PATH="$ONI_PATH/../../MacOS/Oni"
 
-ONI_CWD="$OPEN_DIRECTORY" open --new -a "$FULL_ONI_PATH" --args "$*"
+ONI_CWD="$OPEN_DIRECTORY" open --new -a "$FULL_ONI_PATH" --args $*
