8a821b121c3aa9039e1d52f74f09c9672eadf0da
Bryan Phelps <bryphe@outlook.com>
2018-01-22 15:11:38 -0800

    Factor chooseTheme to separate method

--- browser/src/Services/Themes/ThemePicker.ts
+++ browser/src/Services/Themes/ThemePicker.ts
@@ -9,32 +9,49 @@ import { Configuration }  from "./../Configuration"
 import { menuManager } from "./../Menu"
 import { ThemeManager } from "./ThemeManager"
 
+const chooseTheme = async (configuration: Configuration, themeManager: ThemeManager) => {
+        const themes = await themeManager.getAllThemes()
+
+        const items = themes.map((t) => ({
+            icon: "paint",
+            label: t.name,
+            detail: t.path,
+        }))
+
+        const currentTheme = themeManager.activeTheme.name
+
+        const themeMenu = menuManager.create()
+        themeMenu.show()
+        themeMenu.setItems(items)
+
+        let wasSelected = false
+
+        themeMenu.onItemSelected.subscribe(() => wasSelected = true)
+
+        themeMenu.onHide.subscribe(() => {
+            if (!wasSelected) {
+                themeManager.setTheme(currentTheme)
+            }
+        })
+
+        themeMenu.onSelectedItemChanged.subscribe((newOption) => {
+            if (newOption) {
+                configuration.setValues({"ui.colorscheme": newOption.label})
+                themeManager.setTheme(newOption.label)
+            } else {
+                themeManager.setTheme(currentTheme)
+            }
+        })
+}
+
 export const activate = (configuration: Configuration, themeManager: ThemeManager) => {
 
     commandManager.registerCommand(
         new CallbackCommand(
-            "oni.themes.pick",
-            "Themes: Choose theme",
-            null,
-            async () => {
-
-                const themes = await themeManager.getAllThemes()
-
-                const items = themes.map((t) => ({
-                    icon: null,
-                    label: t.name,
-                    detail: t.path,
-                }))
-
-                const themeMenu = menuManager.create()
-                themeMenu.show()
-                themeMenu.setItems(items)
-
-                themeMenu.onSelectedItemChanged.subscribe((newOption) => {
-                    configuration.setValues({"ui.colorscheme": newOption.label})
-                    themeManager.setTheme(newOption.label)
-                })
-            }
+            "oni.themes.choose",
+            "Themes: Choose Theme",
+            "Choose your theme from the available bundled themes.",
+            () => chooseTheme(configuration, themeManager),
         ))
 }
 
