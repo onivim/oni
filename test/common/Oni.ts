15d6ec4ff07588bad0774170cfcecaead4902843
Bryan Phelps <bryphe@outlook.com>
2018-01-22 17:07:58 -0800

    Add logging for environment variables for test too

--- test/common/Oni.ts
+++ test/common/Oni.ts
@@ -38,6 +38,8 @@ export class Oni {
 
         log("Start options: " + JSON.stringify(options))
 
+        log("automation - env: " + JSON.stringify(process.env))
+
         this._app = new Application({
             path: executablePath,
             env: options.configurationPath ? { "ONI_CONFIG_FILE": options.configurationPath } : {},
