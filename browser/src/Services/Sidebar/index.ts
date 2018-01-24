f8a62999d45b6accf292453030b738dd5871bd93
Bryan Phelps <bryphe@outlook.com>
2018-01-24 11:19:00 -0800

    Fix selection / hover behavior

--- browser/src/Services/Sidebar/index.ts
+++ browser/src/Services/Sidebar/index.ts
@@ -9,7 +9,7 @@ import { SidebarContentSplit } from "./SidebarContentSplit"
 import { SidebarSplit } from "./SidebarSplit"
 import { SidebarManager } from "./SidebarStore"
 
-import { ItemWidget, LabelWidget, SidebarPane } from "./SidebarPane"
+import { SidebarPane } from "./SidebarPane"
 
 let _sidebarManager: SidebarManager = null
 
@@ -24,10 +24,14 @@ export const activate = (configuration: Configuration, workspace: Workspace) => 
 
         _sidebarManager.add("files-o", new ExplorerSplit(workspace, commandManager, editorManager))
 
+        const tasksPane = new SidebarPane("oni.sidebar.tasks", "Tasks")
+        _sidebarManager.add("cogs", tasksPane)
+
+        const bookmarksPane = new SidebarPane("oni.sidebar.bookmarks", "Bookmarks")
+        _sidebarManager.add("bookmark", bookmarksPane)
+
         const searchPane = new SidebarPane("oni.sidebar.search", "Search")
         _sidebarManager.add("search", searchPane)
-
-        searchPane.set([new LabelWidget(), new LabelWidget(), new ItemWidget("oni.test"), new ItemWidget("oni.test2")])
     }
 }
 
