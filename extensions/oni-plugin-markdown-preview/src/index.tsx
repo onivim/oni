cf8a2307b87e402262aa62d6287fe3fb5d502b08
Bryan Phelps <bryphe@outlook.com>
2018-01-25 19:21:36 -0800

    Use common tslint.json for typescript

--- extensions/oni-plugin-markdown-preview/src/index.tsx
+++ extensions/oni-plugin-markdown-preview/src/index.tsx
@@ -116,7 +116,7 @@ class MarkdownPreview extends React.PureComponent<IMarkdownPreviewProps, IMarkdo
 
         let isBlock: boolean = false
         const originalLinesCount: number = markdownLines.length - 1
-        for (var i = originalLinesCount; i > 0; i--) {
+        for (let i = originalLinesCount; i > 0; i--) {
             // tslint:disable-line
             if (markdownLines[i].includes("```")) {
                 isBlock = !isBlock
