32d96dcf8f83ae1f02f9f9fb36f6e3705362c982
Akin909 <akin.sowemimo@gmail.com>
2018-01-26 08:17:43 +0000

    add filter to bufferlist commands

--- vim/core/oni-plugin-buffers/index.js
+++ vim/core/oni-plugin-buffers/index.js
@@ -1,109 +1,107 @@
 // @ts-check
-const path = require('path');
+const path = require("path")
 
 const activate = Oni => {
-  const menu = Oni.menu.create();
+    const menu = Oni.menu.create()
 
-  const truncateFilePath = filepath => {
-    const sections = filepath.split(path.sep);
-    const folderAndFiles = sections.slice(-2);
-    return folderAndFiles.join(path.sep);
-  };
-
-  const updateBufferList = (Oni, menu) => {
-    const buffers = Oni.editors.activeEditor.getBuffers();
-    const active = Oni.editors.activeEditor.activeBuffer.filePath;
-
-    const bufferMenuItems = buffers.map(b => ({
-      label: `${active === b.filePath ? b.id + ' %' : b.id}`,
-      detail: truncateFilePath(b.filePath),
-      icon: Oni.ui.getIconClassForFile(b.filePath),
-      pinned: active === b.filePath,
-      metadata: { filePath: b.filePath, id: b.id },
-    }));
-
-    return bufferMenuItems;
-  };
-
-  const createBufferList = () => {
-    const buffers = updateBufferList(Oni, menu);
-    menu.show();
-    menu.setItems(buffers);
-  };
-
-  const toggleBufferList = () => {
-    !menu.isOpen() ? createBufferList() : menu.hide();
-  };
-
-  const deleteBuffer = menu => {
-    if (menu.selectedItem) {
-      Oni.editors.activeEditor.bufferDelete(menu.selectedItem.metadata.id);
-      menu.hide();
+    const truncateFilePath = filepath => {
+        const sections = filepath.split(path.sep)
+        const folderAndFiles = sections.slice(-2)
+        return folderAndFiles.join(path.sep)
     }
-  };
 
-  const openBuffer = (menu, orientation) => {
-    if (menu.selectedItem && menu.isOpen()) {
-      const buffers = Oni.editors.activeEditor.getBuffers();
-      try {
-        const { filePath = '' } = menu.selectedItem.metadata;
-        Oni.editors.activeEditor.openFile(filePath, orientation);
-      } catch(e) {
-        console.warn('[Oni Buffer Plugin Error]: ', e);
-      } finally {
-        menu.hide();
-      }
+    const updateBufferList = (Oni, menu) => {
+        const buffers = Oni.editors.activeEditor.getBuffers()
+        const active = Oni.editors.activeEditor.activeBuffer.filePath
+
+        const bufferMenuItems = buffers.map(b => ({
+            label: `${active === b.filePath ? b.id + " %" : b.id}`,
+            detail: truncateFilePath(b.filePath),
+            icon: Oni.ui.getIconClassForFile(b.filePath),
+            pinned: active === b.filePath,
+            metadata: { filePath: b.filePath, id: b.id },
+        }))
+
+        return bufferMenuItems
     }
-    return;
-  };
 
-  Oni.commands.registerCommand({
-    command: 'bufferlist.delete',
-    name: 'Delete Selected Buffer',
-    execute: () => deleteBuffer(menu),
-  });
-
-  Oni.commands.registerCommand({
-    command: 'bufferlist.split',
-    name: 'Split Selected Buffer',
-    execute: () => openBuffer(menu, 'horizontal'),
-  });
-
-  Oni.commands.registerCommand({
-    command: 'bufferlist.vsplit',
-    name: 'Vertical Split Selected Buffer',
-    execute: () => openBuffer(menu, 'vertical'),
-  });
-
-  Oni.commands.registerCommand({
-    command: 'bufferlist.tabedit',
-    name: 'Open Selected Buffer in a Tab',
-    execute: () => openBuffer(menu, 'tab'),
-  });
-
-  Oni.commands.registerCommand({
-    command: 'bufferlist.open',
-    name: 'Open Bufferlist ',
-    detail: 'Open A List of All Available Buffers',
-    execute: createBufferList,
-  });
-
-  Oni.commands.registerCommand({
-    command: 'bufferlist.toggle',
-    name: 'Toggle Bufferlist ',
-    detail: 'Toggle A List of All Available Buffers',
-    execute: toggleBufferList,
-  });
-
-  menu.onItemSelected.subscribe(menuItem => {
-    if (menuItem && menuItem.detail) {
-      openBuffer(menu, 'edit');
+    const createBufferList = () => {
+        const buffers = updateBufferList(Oni, menu)
+        menu.show()
+        menu.setItems(buffers)
     }
-  });
 
-  Oni.editors.activeEditor.onBufferEnter.subscribe(() =>
-    updateBufferList(Oni, menu)
-  );
-};
+    const toggleBufferList = () => {
+        !menu.isOpen() ? createBufferList() : menu.hide()
+    }
 
-module.exports = { activate };
+    const deleteBuffer = menu => {
+        if (menu.selectedItem) {
+            Oni.editors.activeEditor.bufferDelete(menu.selectedItem.metadata.id)
+            menu.hide()
+        }
+    }
+
+    const openBuffer = (menu, orientation) => {
+        if (menu.selectedItem && menu.isOpen()) {
+            const buffers = Oni.editors.activeEditor.getBuffers()
+            try {
+                const { filePath = "" } = menu.selectedItem.metadata
+                Oni.editors.activeEditor.openFile(filePath, orientation)
+            } catch (e) {
+                console.warn("[Oni Buffer Plugin Error]: ", e)
+            } finally {
+                menu.hide()
+            }
+        }
+        return
+    }
+
+    Oni.commands.registerCommand({
+        command: "bufferlist.delete",
+        name: "Delete Selected Buffer",
+        execute: () => menu.isMenuOpen() && deleteBuffer(menu),
+    })
+
+    Oni.commands.registerCommand({
+        command: "bufferlist.split",
+        name: "Split Selected Buffer",
+        execute: () => menu.isMenuOpen() && openBuffer(menu, "horizontal"),
+    })
+
+    Oni.commands.registerCommand({
+        command: "bufferlist.vsplit",
+        name: "Vertical Split Selected Buffer",
+        execute: () => menu.isMenuOpen() && openBuffer(menu, "vertical"),
+    })
+
+    Oni.commands.registerCommand({
+        command: "bufferlist.tabedit",
+        name: "Open Selected Buffer in a Tab",
+        execute: () => menu.isMenuOpen() && openBuffer(menu, "tab"),
+    })
+
+    Oni.commands.registerCommand({
+        command: "bufferlist.open",
+        name: "Open Bufferlist ",
+        detail: "Open A List of All Available Buffers",
+        execute: createBufferList,
+    })
+
+    Oni.commands.registerCommand({
+        command: "bufferlist.toggle",
+        name: "Toggle Bufferlist ",
+        detail: "Toggle A List of All Available Buffers",
+        execute: toggleBufferList,
+    })
+
+    menu.onItemSelected.subscribe(menuItem => {
+        if (menuItem && menuItem.detail) {
+            openBuffer(menu, "edit")
+        }
+    })
+
+    Oni.editors.activeEditor.onBufferEnter.subscribe(() => updateBufferList(Oni, menu))
+}
+
+module.exports = { activate }
