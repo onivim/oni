4b531a86a5952d9179c9238dc1dc543a2737ca03
CrossR <sc13rjc@leeds.ac.uk>
2018-01-27 20:15:12 +0000

    Added (broken) unit tests.
    Need to fix the last one and actually validate they are doing what I think they are.

--- browser/test/Services/AutoClosingPairsTests.ts
+++ browser/test/Services/AutoClosingPairsTests.ts
@@ -1,6 +1,11 @@
 import * as assert from "assert"
 
+import * as types from "vscode-languageserver-types"
+
+import * as Mocks from "./../Mocks"
+
 import * as AutoClosingPairs from "./../../src/Services/AutoClosingPairs"
+import { IAutoClosingPair } from "./../../src/Services/AutoClosingPairs"
 
 describe("AutoClosingPairs", () => {
     describe("getWhitespacePrefix", () => {
@@ -17,6 +22,73 @@ describe("AutoClosingPairs", () => {
         it("returns spaces", () => {
             const result = AutoClosingPairs.getWhiteSpacePrefix("  test")
             assert.strictEqual(result, "  ")
+        })
+    })
+
+    describe("handleOpenCharacter", () => {
+        let mockEditor: Mocks.MockEditor
+        let mockBuffer: Mocks.MockBuffer
+
+        beforeEach(() => {
+            mockEditor = new Mocks.MockEditor()
+            mockBuffer = new Mocks.MockBuffer("typescript", "test.ts", [""])
+            mockEditor.simulateBufferEnter(mockBuffer)
+        })
+
+        it("inserts both characters of the pair", async () => {
+            const pair = { open: "(", close: ")" } as IAutoClosingPair
+
+            mockBuffer.setLinesSync(["("])
+            AutoClosingPairs.checkOpenCharacter(
+                (str: string) => {
+                    mockBuffer.setLinesSync([str])
+                },
+                pair,
+                mockEditor as any,
+                false,
+            )
+
+            const [firstLine] = await mockBuffer.getLines(0, 1)
+
+            assert.strictEqual(firstLine, "()")
+        })
+
+        it("doesn't duplicate quote on close", async () => {
+            const pair = { open: '"', close: '"' } as IAutoClosingPair
+
+            mockBuffer.setLinesSync(['"Oni"'])
+            mockBuffer.setCursorPosition(types.Position.create(0, 4))
+            AutoClosingPairs.checkOpenCharacter(
+                (str: string) => {
+                    mockBuffer.setLinesSync([str])
+                },
+                pair,
+                mockEditor as any,
+                true,
+            )
+
+            const [firstLine] = await mockBuffer.getLines(0, 1)
+
+            assert.strictEqual(firstLine, '"Oni"')
+        })
+
+        it("moves the cursor correctly onto the closing quote", async () => {
+            const pair = { open: '"', close: '"' } as IAutoClosingPair
+
+            mockBuffer.setLinesSync(['"Oni"'])
+            mockBuffer.setCursorPosition(types.Position.create(0, 4))
+            AutoClosingPairs.checkOpenCharacter(
+                (str: string) => {
+                    mockBuffer.setLinesSync([str])
+                },
+                pair,
+                mockEditor as any,
+                true,
+            )
+
+            const endCursorPos = await mockBuffer.getCursorPosition()
+
+            assert.strictEqual(endCursorPos, types.Position.create(0, 5))
         })
     })
 })
