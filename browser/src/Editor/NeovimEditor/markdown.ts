1b3623b0b8f5b5ea9df4e9e047b817e972a3badf
Akin909 <akin.sowemimo@gmail.com>
2018-01-28 01:43:08 +0000

    create syntax hl fn WIP

--- browser/src/Editor/NeovimEditor/markdown.ts
+++ browser/src/Editor/NeovimEditor/markdown.ts
@@ -1,4 +1,18 @@
 import * as marked from "marked"
+import { StackElement } from "vscode-textmate"
+import { GrammarLoader } from "../../Services/SyntaxHighlighting/GrammarLoader"
+
+interface IGetTokens {
+    line: string
+    prevState: StackElement
+    language: string
+    ext: string
+}
+async function getTokens({ language, ext, line, prevState }: IGetTokens) {
+    const Grammer = new GrammarLoader()
+    const grammer = await Grammer.getGrammarForLanguage(language, ext)
+    grammer.tokenizeLine(line, prevState)
+}
 
 export const convertMarkdown = (markdown: string): { __html: string } => {
     marked.setOptions({
