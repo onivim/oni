39e2a38785750b96a5d3eaf873b5bee8693ee99a
Akin909 <akin.sowemimo@gmail.com>
2018-01-31 23:54:19 +0000

    [WIP] Toggleability of window split

--- browser/src/UI/components/WindowSplitHost.tsx
+++ browser/src/UI/components/WindowSplitHost.tsx
@@ -5,8 +5,11 @@
  */
 
 import * as React from "react"
+import { connect } from "react-redux"
 
 import * as Oni from "oni-api"
+
+import { Configuration } from "./../../Services/Configuration"
 
 export interface IWindowSplitHostProps {
     split: Oni.IWindowSplit
@@ -14,18 +17,27 @@ export interface IWindowSplitHostProps {
     isFocused: boolean
 }
 
+function mapStateToProps(state: IState) {
+    return {
+        showSplit: state,
+    }
+}
+
 /**
  * Component responsible for rendering an individual window split
  */
+@connect<IWindowSplitHostProps>(mapStateToProps, { toggleSplit })
 export class WindowSplitHost extends React.PureComponent<IWindowSplitHostProps, {}> {
     public render(): JSX.Element {
         const className =
             this.props.containerClassName + (this.props.isFocused ? " focus" : " not-focused")
 
         return (
-            <div className="container vertical full">
-                <div className={className}>{this.props.split.render()}</div>
-            </div>
+            className.includes("editor") && (
+                <div className="container vertical full">
+                    <div className={className}>{this.props.split.render()}</div>
+                </div>
+            )
         )
     }
 }
