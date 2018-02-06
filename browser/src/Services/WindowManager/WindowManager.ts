2ea33ced8276ca926014b5972dcf5d5aa4b074e8
Bryan Phelps <bryphe@outlook.com>
2018-02-06 12:07:06 -0800

    Start sketching out window store

--- browser/src/Services/WindowManager/WindowManager.ts
+++ browser/src/Services/WindowManager/WindowManager.ts
@@ -17,6 +17,75 @@ import { RelationalSplitNavigator } from "./RelationalSplitNavigator"
 import { WindowDock } from "./WindowDock"
 import { ISplitInfo } from "./WindowState"
 
+import { Store, Reducer } from "redux"
+import { createStore as createReduxStore } from "./../../Redux"
+
+export interface IAugmentedSplitInfo extends Oni.IWindowSplit {
+    // Potential API methods
+    enter?(): void
+    leave?(): void
+
+    // Internal bookkeeping
+    id: string
+}
+
+type WindowActions =
+    | {
+          type: "SET_DOCK_SPLITS"
+          dock: Direction
+          splits: IAugmentedSplitInfo[]
+      }
+    | {
+          type: "SET_PRIMARY_SPLITS"
+          splits: ISplitInfo<IAugmentedSplitInfo>
+      }
+    | {
+          type: "SET_FOCUSED_SPLIT"
+          splitId: string
+      }
+    | {
+          type: "SHOW_SPLIT"
+          splitId: string
+      }
+    | {
+          type: "HIDE_SPLIT"
+          splitId: string
+      }
+
+export type DockWindows = { [key: string]: IAugmentedSplitInfo[] }
+
+export interface WindowState {
+    docks: DockWindows
+
+    primarySplit: ISplitInfo<IAugmentedSplitInfo>
+
+    focusedSplitId: string
+    hiddenSplits: string[]
+}
+
+export const DefaultWindowState: WindowState = {
+    docks: {
+        left: [],
+        right: [],
+        up: [],
+        down: [],
+    },
+    primarySplit: null,
+    focusedSplitId: null,
+    hiddenSplits: [],
+}
+
+export const reducer: Reducer<WindowState> = (
+    state: WindowState = DefaultWindowState,
+    action: WindowActions,
+) => {
+    return state
+}
+
+export const createStore = (): Store<WindowState> => {
+    return createReduxStore("WindowManager", reducer, DefaultWindowState, [])
+}
+
 export class WindowManager {
     private _activeSplit: any
 
