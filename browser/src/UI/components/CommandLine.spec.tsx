019d546ee64f9b9679005fd9f03ecbf2cae72996
Akin909 <akin.sowemimo@gmail.com>
2018-02-10 14:26:35 +0000

    add jest codemod

---
+++ browser/src/UI/components/CommandLine.spec.tsx
@@ -0,0 +1,14 @@
+import { mount } from "enzyme"
+import * as React from "react"
+import renderer from "react-test-renderer"
+import { spy } from "sinon"
+import CommandLine from "./Commandline"
+
+spy(CommandLine.prototype, "componentDidMount")
+
+test("<Commandline />: It should correctly mount the commandline component", () => {
+    it("calls componentDidMount", () => {
+        const wrapper = mount(<CommandLine />)
+        expect(CommandLine.prototype.componentDidMount.calledOnce).to.equal(true)
+    })
+})
