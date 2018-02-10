f2afc7463f11856c9e2681a69eeb878bbecbf378
Akin909 <akin.sowemimo@gmail.com>
2018-02-10 15:26:18 +0000

    configure tests for ui tweak command so tests are found

--- browser/src/UI/components/tests/Commandline.test.tsx
+++ browser/src/UI/components/tests/Commandline.test.tsx
@@ -1,15 +1,21 @@
 import { expect } from "chai"
-import { mount } from "enzyme"
+import { /* mount, */ shallow } from "enzyme"
 import * as React from "react"
-import renderer from "react-test-renderer"
+// import * as renderer from "react-test-renderer"
 import { spy } from "sinon"
-import CommandLine from "./Commandline"
+import CommandLine from "./../CommandLine"
 
 spy(CommandLine.prototype, "componentDidMount")
 
-test("<Commandline />: It should correctly mount the commandline component", () => {
+describe("<Commandline />: It should correctly mount the commandline component", () => {
     it("calls componentDidMount", () => {
-        const wrapper = mount(<CommandLine />)
         expect(CommandLine.prototype.componentDidMount.calledOnce).to.equal(true)
+        // expect(wrapper).to.exist()
+        // const wrapper = mount(<CommandLine />)
+    })
+
+    it("Renders Without Exploding", () => {
+        const wrapper = shallow(<CommandLine />)
+        expect(wrapper.find(<CommandLine />).length).to.equal(1)
     })
 })
