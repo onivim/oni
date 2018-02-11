2f52619fce5c22aabb488f679c30a7bae8db5c86
Bryan Phelps <bryphe@outlook.com>
2018-02-10 16:50:09 -0800

    Add simple test cases for NeovimTokenColorSynchronizer

--- browser/test/Mocks/neovim/MockNeovimInstance.ts
+++ browser/test/Mocks/neovim/MockNeovimInstance.ts
@@ -1,80 +1,36 @@
 /**
- * Mocks/neovim.ts
+ * Mocks/neovim/MockNeovimInstance.ts
  *
  * Implementations of test mocks and doubles,
  * for Neovim facing classes / interfaces.
  */
 
-import * as Neovim from "./../../src/neovim"
+import * as Utility from "./../../../src/Utility"
 
-export class MockScreen implements Neovim.IScreen {
-    public backgroundColor: string
-    public foregroundColor: string
-    public cursorColumn: number = 0
-    public cursorRow: number = 0
+export interface NeovimRequest {
+    requestName: string
+    args: any[]
+}
 
-    private _cells: { [row: number]: { [col: number]: Neovim.ICell } } = {}
+export class MockNeovimInstance {
+    private _requests: NeovimRequest[] = []
+    private _pendingPromises: Utility.ICompletablePromise<any>[] = []
 
-    public get currentBackgroundColor(): string {
-        return null
+    public request(requestName: string, args: any[]) {
+        this._requests.push({ requestName, args })
+        const promise = Utility.createCompletablePromise()
+        this._pendingPromises.push(promise)
+
+        return promise.promise
     }
 
-    public get currentForegroundColor(): string {
-        return null
+    public getPendingRequests(): NeovimRequest[] {
+        return this._requests
     }
 
-    public get fontFamily(): null | string {
-        return null
-    }
-    public get fontHeightInPixels(): number {
-        return null
-    }
-    public get fontSize(): null | string {
-        return null
-    }
-    public get fontWidthInPixels(): number {
-        return null
-    }
-    public get height(): number {
-        return null
-    }
-    public get linePaddingInPixels(): number {
-        return null
-    }
-    public get mode(): string {
-        return "normal"
-    }
-    public get width(): number {
-        return null
-    }
-
-    public dispatch(action: Neovim.IAction): void {
-        // tslint:disable-line
-    }
-
-    public getCell(x: number, y: number): Neovim.ICell {
-        const row = this._cells[y]
-
-        if (!row) {
-            return null
-        }
-
-        const cell = row[x]
-        return cell || null
-    }
-
-    public setCell(x: number, y: number, cell: Neovim.ICell): void {
-        const row = this._cells[y] || {}
-
-        const updatedRow = {
-            ...row,
-            [x]: cell,
-        }
-
-        this._cells[y] = updatedRow
-    }
-
-    public getScrollRegion(): Neovim.IScrollRegion {
-        return null
+    public flushPendingRequests(): void {
+        this._pendingPromises.forEach(p => p.resolve())
+        this._requests = []
+        this._pendingPromises = []
     }
 }
--- browser/test/neovim/NeovimTokenColorSynchronizerTests.ts
+++ browser/test/neovim/NeovimTokenColorSynchronizerTests.ts
@@ -4,28 +4,67 @@
 
 import * as assert from "assert"
 
+import { NeovimTokenColorSynchronizer } from "./../../src/neovim/NeovimTokenColorSynchronizer"
+import { TokenColor } from "./../../src/Services/TokenColors"
+
+import { MockNeovimInstance } from "./../Mocks/neovim"
+
+const createTokenColor = (scope: string): TokenColor => ({
+    scope,
+    settings: {
+        foregroundColor: "#FFFFFF",
+        backgroundColor: "#FF0000",
+        bold: false,
+        italic: false,
+    },
+})
+
 describe("NeovimTokenColorSynchronizer", () => {
-    describe("synchronizeTokenColors", () => {
-        it("adds token color if it doesn't exist", () => {
-            assert.ok(false, "fails")
-        })
+    let neovimInstance: MockNeovimInstance
+    let tokenColorSynchronizer: NeovimTokenColorSynchronizer
 
-        it("doesn't add token color if it already exists", () => {
-            assert.ok(false, "fails")
-        })
-
-        it("updates token color if it changed", () => {
-            assert.ok(false, "fails")
-        })
+    beforeEach(() => {
+        neovimInstance = new MockNeovimInstance()
+        tokenColorSynchronizer = new NeovimTokenColorSynchronizer(neovimInstance as any)
     })
 
-    describe("getHighlightGroupForTokenColor", () => {
-        it("returns highlight group even if not synchronized yet", () => {
-            assert.ok(false, "fails")
+    describe("synchronizeTokenColors", () => {
+        it("adds token color if it doesn't exist", async () => {
+            const color = createTokenColor("test.scope")
+            const promise = tokenColorSynchronizer.synchronizeTokenColors([color])
+            const pendingRequests = neovimInstance.getPendingRequests()
+
+            const firstRequest = pendingRequests[0]
+            assert.strictEqual(firstRequest.requestName, "nvim_call_atomic")
+
+            const subRequests = pendingRequests[0].args
+            assert.strictEqual(subRequests.length, 1)
+
+            const [subRequestName, subRequestArgs] = subRequests[0][0]
+
+            assert.strictEqual(subRequestName, "nvim_command")
+            assert.ok(subRequestArgs[0].indexOf("guibg=#FF0000"))
+            assert.ok(subRequestArgs[0].indexOf("guifg=#FFFFFF"))
+
+            neovimInstance.flushPendingRequests()
+
+            await promise
         })
 
-        it("returns same highlight group as from synchronize call", () => {
-            assert.ok(false, "fails")
+        it("doesn't add token color if it already exists", async () => {
+            // Add a token color...
+            const color = createTokenColor("test.scope")
+            const promise = tokenColorSynchronizer.synchronizeTokenColors([color])
+
+            neovimInstance.flushPendingRequests()
+            await promise
+
+            // ...now add it again
+            const promise2 = tokenColorSynchronizer.synchronizeTokenColors([color])
+
+            // Validate there were no pending requests
+            assert.strictEqual(neovimInstance.getPendingRequests().length, 0)
+            await promise2
         })
     })
 })
