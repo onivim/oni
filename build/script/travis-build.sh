2466810602d396a1890b13278ea2bec6e83f8fd3
Bryan Phelps <bryphe@outlook.com>
2018-02-11 09:14:20 -0800

    Fix typo in build script

--- build/script/travis-build.sh
+++ build/script/travis-build.sh
@@ -40,7 +40,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     ls -a .nyc_output
     ls -a .nyc_output/coverage
 
-    node_modules/.bin/coveralls < .nyc_output/coverage/lcov.inf
+    node_modules/.bin/coveralls < .nyc_output/coverage/lcov.info
 fi
 
 npm run copy-dist-to-s3
