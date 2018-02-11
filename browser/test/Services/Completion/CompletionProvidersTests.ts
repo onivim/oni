5586b6d4d85cd20bdec49df982c284ed9d466836
Bryan Phelps <bryphe@outlook.com>
2018-02-11 06:43:33 -0800

    Add red test case exercising crash in production

--- browser/test/Services/Completion/CompletionProvidersTests.ts
+++ browser/test/Services/Completion/CompletionProvidersTests.ts
@@ -56,4 +56,23 @@ describe("CompletionProvider", () => {
         assert.strictEqual(result.length, 4)
         assert.deepEqual(resultItems, ["a", "b", "c", "d"])
     })
+
+    it("handles case where one completion provider returns null", async () => {
+        const provider1 = new MockCompletionsRequestor(null)
+        const provider2 = new MockCompletionsRequestor([
+            createCompletionItem("c"),
+            createCompletionItem("d"),
+        ])
+
+        const completionProviders = new CompletionProviders()
+        completionProviders.registerCompletionProvider("provider1", provider1)
+        completionProviders.registerCompletionProvider("provider2", provider2)
+
+        const result = await completionProviders.getCompletions("lang", "file", 0, 0)
+
+        const resultItems = result.map(i => i.label)
+
+        assert.strictEqual(result.length, 2)
+        assert.deepEqual(resultItems, ["c", "d"])
+    })
 })
