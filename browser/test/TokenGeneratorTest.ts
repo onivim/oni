aec4f14fc585ceb11f0a4feed70a4e6d9e290335
Akin909 <akin.sowemimo@gmail.com>
2018-02-12 00:14:00 +0000

    add new test [WIP]

---
+++ browser/test/TokenGeneratorTest.ts
@@ -0,0 +1,25 @@
+// import * as assert from "assert"
+// import getTokens, { IGrammarPerLine, IGrammarTokens } from "./../src/Services/SyntaxHighlighting/TokenGenerator"
+//
+// describe("Token Generator function test", async () => {
+//     const testString = "function() { a + b }"
+//     let tokens: IGrammarPerLine
+//     let firstLine: IGrammarTokens
+//
+//     before(async () => {
+//         tokens = await getTokens({ language: "typescript", ext: ".ts", line: testString })
+//         ({ 0: firstLine } = tokens)
+//     })
+//
+//     it("Should create a map with the line as the key and should have a tokens array", () => {
+//         assert.ok(Array.isArray(firstLine.tokens))
+//     })
+//
+//     it(`Should have a line property which should equal the original string`, () => {
+//         assert.ok(firstLine.line === testString)
+//     })
+//
+//     it("Should have a ruleStack property", () => {
+//         assert.ok(firstLine.ruleStack)
+//     })
+// })
