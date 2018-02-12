999edc0325deb48c360864c9fe1b11e6346b3608
Bryan Phelps <bryphe@outlook.com>
2018-02-12 13:30:59 -0800

    Temporarily remove tests to get quicker feedback on coverage

--- test/CiTests.ts
+++ test/CiTests.ts
@@ -10,28 +10,27 @@ import { IFailedTest, Oni, runInProcTest } from "./common"
 const LongTimeout = 5000
 
 const CiTests = [
-    // Core functionality tests
-    "Api.Buffer.AddLayer",
-    "Api.Overlays.AddRemoveTest",
-    "AutoClosingPairsTest",
-    "AutoCompletionTest-CSS",
-    "AutoCompletionTest-HTML",
-    "AutoCompletionTest-TypeScript",
-    "Editor.ExternalCommandLineTest",
-    "LargeFileTest",
-    "MarkdownPreviewTest",
-    "PaintPerformanceTest",
-    "QuickOpenTest",
-    "StatusBar-Mode",
-    "NoInstalledNeovim",
-    "Sidebar.ToggleSplitTest",
-    "WindowManager.ErrorBoundary",
-    "Workspace.ConfigurationTest",
-
-    // Regression Tests
-    "Regression.1251.NoAdditionalProcessesOnStartup",
-    "Regression.1296.SettingColorsTest",
-    "Regression.1295.UnfocusedWindowTest",
+    // // Core functionality tests
+    // "Api.Buffer.AddLayer",
+    // "Api.Overlays.AddRemoveTest",
+    // "AutoClosingPairsTest",
+    // "AutoCompletionTest-CSS",
+    // "AutoCompletionTest-HTML",
+    // "AutoCompletionTest-TypeScript",
+    // "Editor.ExternalCommandLineTest",
+    // "LargeFileTest",
+    // "MarkdownPreviewTest",
+    // "PaintPerformanceTest",
+    // "QuickOpenTest",
+    // "StatusBar-Mode",
+    // "NoInstalledNeovim",
+    // "Sidebar.ToggleSplitTest",
+    // "WindowManager.ErrorBoundary",
+    // "Workspace.ConfigurationTest",
+    // // Regression Tests
+    // "Regression.1251.NoAdditionalProcessesOnStartup",
+    // "Regression.1296.SettingColorsTest",
+    // "Regression.1295.UnfocusedWindowTest",
 ]
 
 const WindowsOnlyTests = [
