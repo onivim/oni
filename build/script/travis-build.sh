1fd42ee2047e6f57bb780b614603d95d89a60f31
Bryan Phelps <bryphe@outlook.com>
2018-02-11 22:00:58 -0800

    Fix erroneous ls since we aren't writing lcov

--- build/script/travis-build.sh
+++ build/script/travis-build.sh
@@ -35,10 +35,8 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     npm run ccov:instrument
     npm run ccov:test:browser
-    npm run ccov:report
-
     ls -a .nyc_output
-    ls -a .nyc_output/coverage
+    npm run ccov:report
 fi
 
 npm run copy-dist-to-s3
