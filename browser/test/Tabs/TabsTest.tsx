61f2f1e818d55e605064367a014a2317816de166
Akin909 <akin.sowemimo@gmail.com>
2018-02-16 14:49:58 +0000

    change separators to be system agnostic

--- browser/test/Tabs/TabsTest.tsx
+++ browser/test/Tabs/TabsTest.tsx
@@ -1,4 +1,5 @@
 import * as assert from "assert"
+import * as path from "path"
 import { checkDuplicate, getTabName } from "./../../src/UI/components/Tabs"
 
 describe("Tab bar utility function tests", () => {
@@ -15,17 +16,18 @@ describe("Tab bar utility function tests", () => {
         })
     })
     describe("Get tab name function should return a longer name if duplicates present", () => {
+        const sep = path.sep
         it("Should return a only a filename if there are no duplicates", () => {
-            const testArray = ["/folder/index.js", "/folderTwo/apple.js"]
+            const testArray = [`${sep}folder${sep}index.js`, `${sep}folderTwo${sep}apple.js`]
             const isDuplicate = checkDuplicate("index.js", testArray)
-            const name = getTabName("/folder/index.js", isDuplicate)
+            const name = getTabName(`${sep}folder${sep}index.js`, isDuplicate)
             assert.ok(name === "index.js")
         })
         it("Should return a folder and a filename if there are duplicates", () => {
-            const testArray = ["/folder/index.js", "/folderTwo/index.js"]
-            const isDuplicate = checkDuplicate("index.js", testArray)
-            const name = getTabName("/folder/index.js", isDuplicate)
-            assert.ok(name === "folder/index.js")
+            const testArray = [`${sep}folder${sep}index.js`, `${sep}folderTwo${sep}index.js`]
+            const isDuplicate = checkDuplicate(`index.js`, testArray)
+            const name = getTabName(`${sep}folder${sep}index.js`, isDuplicate)
+            assert.ok(name === `folder${sep}index.js`)
         })
     })
 })
