a1d902846b550f30e6869408deb38cb73b1b3114
Akin909 <akin.sowemimo@gmail.com>
2018-02-16 14:42:41 +0000

    rename test file and make test every so slightly more robust

--- browser/test/Tabs/TabsTest.tsx
+++ browser/test/Tabs/TabsTest.tsx
@@ -18,7 +18,7 @@ describe("Tab bar utility function tests", () => {
         it("Should return a only a filename if there are no duplicates", () => {
             const testArray = ["/folder/index.js", "/folderTwo/apple.js"]
             const isDuplicate = checkDuplicate("index.js", testArray)
-            const name = getTabName("index.js", isDuplicate)
+            const name = getTabName("/folder/index.js", isDuplicate)
             assert.ok(name === "index.js")
         })
         it("Should return a folder and a filename if there are duplicates", () => {
