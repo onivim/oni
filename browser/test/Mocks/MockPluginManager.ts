bb14c2dfad7d3fface173f6f5065777d485ea9c0
Bryan Phelps <bryphe@outlook.com>
2018-02-27 10:07:27 -0800

    Add MockPluginManager

---
+++ browser/test/Mocks/MockPluginManager.ts
@@ -0,0 +1,27 @@
+/**
+ * Mocks/MockPluginManager.ts
+ */
+
+import * as Oni from "oni-api"
+
+import { IContributions } from "./../../src/Plugins/Api/Capabilities"
+
+export class MockPluginManager implements Oni.IPluginManager {
+    public get loaded(): boolean {
+        return false
+    }
+
+    public discoverPlugins(): void {}
+
+    public getAllContributionsOfType<T>(selector: (capabilities: IContributions) => T[]): T[] {
+        return []
+    }
+
+    public startApi(): Oni.Plugin.Api {
+        return null
+    }
+
+    public getPlugin(name: string): any {
+        return null
+    }
+}
