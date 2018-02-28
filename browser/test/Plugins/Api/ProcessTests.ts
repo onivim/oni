be3bf923bfbdc5a616a05b36f146d27c4d13d623
Bryan Phelps <bryphe@outlook.com>
2018-02-28 10:41:47 -0800

    Add test for mergeSpawnOptions

---
+++ browser/test/Plugins/Api/ProcessTests.ts
@@ -0,0 +1,31 @@
+/**
+ * ProcessTests.ts
+ */
+
+import * as assert from "assert"
+
+import { Process, IShellEnvironmentFetcher } from "./../../../src/Plugins/Api/Process"
+
+class MockShellEnvironmentFetcher implements IShellEnvironmentFetcher {
+    constructor(private _env: any) {}
+
+    public async getEnvironmentVariables(): Promise<any> {
+        return this._env
+    }
+}
+
+describe("ProcessTests", () => {
+    it("mergeSpawnOptions takes into account shell environment", async () => {
+        const mockShellEnv = new MockShellEnvironmentFetcher({ test2: true })
+        const process = new Process(mockShellEnv)
+
+        const newOptions = await process.mergeSpawnOptions({
+            env: {
+                test1: true,
+            },
+        })
+
+        assert.strictEqual(newOptions.env["test1"], true)
+        assert.strictEqual(newOptions.env["test2"], true)
+    })
+})
