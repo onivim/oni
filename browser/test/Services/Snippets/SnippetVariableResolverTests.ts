a83df95c99f7b58075d5f0d2afd3477c7de4fcbc
Bryan Phelps <bryphe@outlook.com>
2018-03-01 18:49:36 -0800

    Stub out test case for SnippetVariableResolver

---
+++ browser/test/Services/Snippets/SnippetVariableResolverTests.ts
@@ -0,0 +1,11 @@
+/**
+ * SnippetVariableResolverTests.ts
+ */
+
+import * as assert from "assert"
+
+describe("SnippetVariableResolverTests", () => {
+    it("tests", async () => {
+        assert.ok(false, "fail")
+    })
+})
