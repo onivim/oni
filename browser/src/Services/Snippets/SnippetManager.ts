1eb44f305a28ad8d30c6235e3a72c20092fd350d
Bryan Phelps <bryphe@outlook.com>
2018-03-06 15:30:52 -0800

    Force leaving insert mode

--- browser/src/Services/Snippets/SnippetManager.ts
+++ browser/src/Services/Snippets/SnippetManager.ts
@@ -103,9 +103,11 @@ export class SnippetManager {
         return !!this._activeSession
     }
 
-    public cancel(): void {
+    public async cancel(): Promise<void> {
         if (this._activeSession) {
             this._cleanupAfterSession()
+            await (this._editorManager.activeEditor as any).clearSelection()
+            await this._editorManager.activeEditor.neovim.command("stopinsert")
         }
 
         if (this._currentLayer) {
