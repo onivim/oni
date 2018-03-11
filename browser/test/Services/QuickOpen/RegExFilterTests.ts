f79d550a5252d12f66758d5fd4b5a405c885789f
CrossR <sc13rjc@leeds.ac.uk>
2018-03-11 13:12:05 +0000

    Fix lint issue.

--- browser/test/Services/QuickOpen/RegExFilterTests.ts
+++ browser/test/Services/QuickOpen/RegExFilterTests.ts
@@ -3,36 +3,32 @@
  */
 
 import * as assert from "assert"
-import { regexFilter, processSearchTerm } from "./../../../src/Services/QuickOpen/RegExFilter"
+import { processSearchTerm, regexFilter } from "./../../../src/Services/QuickOpen/RegExFilter"
 
 describe("processSearchTerm", () => {
     it("Correctly matches word.", async () => {
         const testString = "src"
         const testList = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-            {label: "index.ts", detail: "browser/test/index.ts"},
+            { label: "index.ts", detail: "browser/src/index.ts" },
+            { label: "index.ts", detail: "browser/test/index.ts" },
         ]
 
         const result = processSearchTerm(testString, testList, false)
 
-        const expectedResult = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-        ]
+        const expectedResult = [{ label: "index.ts", detail: "browser/src/index.ts" }]
 
         assert.deepEqual(result, expectedResult)
     })
     it("Correctly matches case-sensitive word.", async () => {
         const testString = "SRC"
         const testList = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-            {label: "index.ts", detail: "browser/SRC/index.ts"},
+            { label: "index.ts", detail: "browser/src/index.ts" },
+            { label: "index.ts", detail: "browser/SRC/index.ts" },
         ]
 
         const result = processSearchTerm(testString, testList, true)
 
-        const expectedResult = [
-            {label: "index.ts", detail: "browser/SRC/index.ts"},
-        ]
+        const expectedResult = [{ label: "index.ts", detail: "browser/SRC/index.ts" }]
 
         assert.deepEqual(result, expectedResult)
     })
@@ -42,23 +38,23 @@ describe("regexFilter", () => {
     it("Correctly matches string.", async () => {
         const testString = "index"
         const testList = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-            {label: "main.ts", detail: "browser/src/main.ts"},
-            {label: "index.ts", detail: "browser/test/index.ts"},
+            { label: "index.ts", detail: "browser/src/index.ts" },
+            { label: "main.ts", detail: "browser/src/main.ts" },
+            { label: "index.ts", detail: "browser/test/index.ts" },
         ]
 
         const result = regexFilter(testList, testString)
 
         // Remove the added highlights since they can be tested
         // elsewhere.
-        result.forEach((r) => {
+        result.forEach(r => {
             delete r.detailHighlights
             delete r.labelHighlights
         })
 
         const expectedResult = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-            {label: "index.ts", detail: "browser/test/index.ts"},
+            { label: "index.ts", detail: "browser/src/index.ts" },
+            { label: "index.ts", detail: "browser/test/index.ts" },
         ]
 
         assert.deepEqual(result, expectedResult)
@@ -66,8 +62,8 @@ describe("regexFilter", () => {
     it("Correctly splits and matches string.", async () => {
         const testString = "index src"
         const testList = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-            {label: "index.ts", detail: "browser/test/index.ts"},
+            { label: "index.ts", detail: "browser/src/index.ts" },
+            { label: "index.ts", detail: "browser/test/index.ts" },
         ]
 
         const result = regexFilter(testList, testString)
@@ -77,17 +73,15 @@ describe("regexFilter", () => {
         delete result[0].detailHighlights
         delete result[0].labelHighlights
 
-        const expectedResult = [
-            {label: "index.ts", detail: "browser/src/index.ts"},
-        ]
+        const expectedResult = [{ label: "index.ts", detail: "browser/src/index.ts" }]
 
         assert.deepEqual(result, expectedResult)
     })
     it("Correctly matches long split string.", async () => {
         const testString = "index src service quickopen"
         const testList = [
-            {label: "index.ts", detail: "browser/src/services/menu/index.ts"},
-            {label: "index.ts", detail: "browser/src/services/quickopen/index.ts"},
+            { label: "index.ts", detail: "browser/src/services/menu/index.ts" },
+            { label: "index.ts", detail: "browser/src/services/quickopen/index.ts" },
         ]
 
         const result = regexFilter(testList, testString)
@@ -98,7 +92,7 @@ describe("regexFilter", () => {
         delete result[0].labelHighlights
 
         const expectedResult = [
-            {label: "index.ts", detail: "browser/src/services/quickopen/index.ts"},
+            { label: "index.ts", detail: "browser/src/services/quickopen/index.ts" },
         ]
 
         assert.deepEqual(result, expectedResult)
