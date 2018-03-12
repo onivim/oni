ee9cf7831d466291ba2876f641ee79450f1afc9b
Bryan Phelps <bryphe@outlook.com>
2018-03-12 16:22:27 -0700

    Fix default configuration value

--- test/demo/HeroDemo.ts
+++ test/demo/HeroDemo.ts
@@ -33,12 +33,6 @@ export const test = async (oni: any) => {
         await shortDelay()
     }
 
-    const disableKeyDisplayer = async (func: () => Promise<void>) => {
-        // oni.commands.executeCommand("keyDisplayer.hide")
-        await func()
-        // oni.commands.executeCommand("keyDisplayer.show")
-    }
-
     const pressEscape = async () => {
         await shortDelay()
         oni.automation.sendKeysV2("<esc>")
@@ -182,19 +176,18 @@ export const test = async (oni: any) => {
         await pressEnter()
         await shortDelay()
         await simulateTyping("i")
-        await disableKeyDisplayer(async () => {
-            await simulateTyping("This is just the beginning - lots more to come:")
-            await pressEnter()
-            await simulateTyping("Live Preview")
-            await pressEnter()
-            await simulateTyping("Integrated Browser")
-            await pressEnter()
-            await simulateTyping("Interactive Tutorial")
-            await longDelay()
-            await pressEnter()
-            await pressEnter()
-            await simulateTyping("Thanks for watching! Download Oni today.")
-        })
+
+        await simulateTyping("This is just the beginning - lots more to come:")
+        await pressEnter()
+        await simulateTyping("Live Preview")
+        await pressEnter()
+        await simulateTyping("Integrated Browser")
+        await pressEnter()
+        await simulateTyping("Interactive Tutorial")
+        await longDelay()
+        await pressEnter()
+        await pressEnter()
+        await simulateTyping("Thanks for watching! Download Oni today.")
 
         await pressEscape()
     }
@@ -204,51 +197,49 @@ export const test = async (oni: any) => {
         oni.automation.sendKeysV2("<cr>")
 
         await simulateTyping("i")
-        await disableKeyDisplayer(async () => {
-            await simulateTyping("// Oni integrates with language servers, and includes several...")
-            oni.automation.sendKeysV2("<cr>")
-            await simulateTyping("...but you can hook up your own, too!")
-            await shortDelay()
+        await simulateTyping("// Oni integrates with language servers, and includes several...")
+        oni.automation.sendKeysV2("<cr>")
+        await simulateTyping("...but you can hook up your own, too!")
+        await shortDelay()
 
-            oni.automation.sendKeysV2("<cr>")
-            oni.automation.sendKeysV2("<c-w>")
-            await shortDelay()
-            await simulateTyping("const myArray = [1, 2, 3]")
+        oni.automation.sendKeysV2("<cr>")
+        oni.automation.sendKeysV2("<c-w>")
+        await shortDelay()
+        await simulateTyping("const myArray = [1, 2, 3]")
 
-            await pressEnter()
-            await pressEnter()
-            await pressEscape()
+        await pressEnter()
+        await pressEnter()
+        await pressEscape()
 
-            await simulateTyping("O")
-            await simulateTyping("myArray.")
+        await simulateTyping("O")
+        await simulateTyping("myArray.")
 
-            await simulateTyping("m")
-            await longDelay()
-            await simulateTyping("ap(")
-            await longDelay()
-            await simulateTyping("(val => val + 1")
+        await simulateTyping("m")
+        await longDelay()
+        await simulateTyping("ap(")
+        await longDelay()
+        await simulateTyping("(val => val + 1")
 
-            await pressEscape()
-            await simulateTyping("o")
-            await pressEnter()
-            await simulateTyping("// Oni also has snippet support:")
-            await pressEnter()
-            oni.automation.sendKeysV2("<c-w>")
-            await pressEnter()
+        await pressEscape()
+        await simulateTyping("o")
+        await pressEnter()
+        await simulateTyping("// Oni also has snippet support:")
+        await pressEnter()
+        oni.automation.sendKeysV2("<c-w>")
+        await pressEnter()
 
-            await simulateTyping("forsnip")
-            await pressEnter()
+        await simulateTyping("forsnip")
+        await pressEnter()
 
-            await pressTab()
-            await pressShiftTab()
+        await pressTab()
+        await pressShiftTab()
 
-            await simulateTyping("idx")
-            await pressTab()
-            await simulateTyping("myArray")
-            await pressTab()
-            await pressTab()
-            await pressEscape()
-        })
+        await simulateTyping("idx")
+        await pressTab()
+        await simulateTyping("myArray")
+        await pressTab()
+        await pressTab()
+        await pressEscape()
     }
 
     // Set window size
@@ -285,10 +276,8 @@ export const test = async (oni: any) => {
     await simulateTyping(":sp VIM.md")
     await pressEnter()
 
-    await disableKeyDisplayer(async () => {
-        await simulateTyping("i")
-        await simulateTyping("Use your Vim muscle memory to be productive without a mouse...")
-    })
+    await simulateTyping("i")
+    await simulateTyping("Use your Vim muscle memory to be productive without a mouse...")
 
     await pressEscape()
 
@@ -313,11 +302,9 @@ export const test = async (oni: any) => {
     oni.automation.sendKeysV2("<c-l>")
     await shortDelay()
 
-    await disableKeyDisplayer(async () => {
-        await simulateTyping("o")
-        await simulateTyping("..but enjoy the conveniences of a modern UI editor.")
-        await pressEscape()
-    })
+    await simulateTyping("o")
+    await simulateTyping("..but enjoy the conveniences of a modern UI editor.")
+    await pressEscape()
 
     await shortDelay()
     oni.automation.sendKeysV2("<c-g>")
@@ -345,9 +332,7 @@ export const test = async (oni: any) => {
     await simulateTyping("gT")
 
     await simulateTyping("o")
-    await disableKeyDisplayer(async () => {
-        await simulateTyping("Enjoy built in search with ripgrep..")
-    })
+    await simulateTyping("Enjoy built in search with ripgrep..")
 
     await pressEscape()
 
@@ -361,10 +346,8 @@ export const test = async (oni: any) => {
     oni.automation.sendKeysV2("<c-k>")
 
     await simulateTyping("o")
-    await disableKeyDisplayer(async () => {
-        await simulateTyping("...or the embedded file finder.")
-        await shortDelay()
-    })
+    await simulateTyping("...or the embedded file finder.")
+    await shortDelay()
 
     await pressEscape()
     await shortDelay()
@@ -378,9 +361,7 @@ export const test = async (oni: any) => {
 
     await simulateTyping("G")
     await simulateTyping("o")
-    await disableKeyDisplayer(async () => {
-        await simulateTyping("...use the built in command palette to discover functionality.")
-    })
+    await simulateTyping("...use the built in command palette to discover functionality.")
     await pressEscape()
 
     await showConfig()
