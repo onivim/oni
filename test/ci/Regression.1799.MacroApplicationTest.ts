3b1ab18b322a0bdacb729b064bb320e1abcea98a
Bryan Phelps <bryphe@outlook.com>
2018-03-14 12:35:23 -0700

    Get test to exercise failure

--- test/ci/Regression.1799.MacroApplicationTest.ts
+++ test/ci/Regression.1799.MacroApplicationTest.ts
@@ -16,4 +16,46 @@ export const test = async (oni: Oni.Plugin.Api) => {
     await oni.automation.waitForEditors()
 
     await navigateToFile(testCsvFilePath, oni)
+
+    const activeEditor = oni.editors.activeEditor
+
+    // Record macro
+    await activeEditor.neovim.input("qa")
+
+    // Delete comma from end of line
+    await activeEditor.neovim.input("$x")
+    await activeEditor.neovim.input("j0")
+
+    // End recording
+    await activeEditor.neovim.input("q")
+
+    // Run macro 999 times
+    await activeEditor.neovim.input("999@a")
+
+    // Wait for last line to not end with a comma
+
+    let attempts = 0
+    let editAccomplished = false
+    while (attempts < 5) {
+        const [lastLine] = await activeEditor.activeBuffer.getLines(998, 999)
+
+        if (!lastLine.endsWith(",")) {
+            editAccomplished = true
+            break
+        }
+
+        await oni.automation.sleep(1000)
+        attempts++
+    }
+
+    assert.ok(editAccomplished, "Verify edit applied to last line")
+
+    await activeEditor.neovim.input("G")
+    await activeEditor.neovim.input("gg")
+
+    await oni.automation.waitFor(() => activeEditor.activeBuffer.cursor.line === 0)
+
+    await activeEditor.neovim.input("G")
+
+    await oni.automation.waitFor(() => activeEditor.activeBuffer.cursor.line === 999)
 }
