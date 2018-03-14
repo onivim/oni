a7ad2f3dca0a18fc86f7c98a5109678147afe772
Bryan Phelps <bryphe@outlook.com>
2018-03-14 13:20:40 -0700

    Add test file

---
+++ browser/test/neovim/NeovimBufferUpdateManagerTests.ts
@@ -0,0 +1,13 @@
+/**
+ * NeovimBufferUpdateManagerTests.ts
+ */
+
+import * as assert from "assert"
+
+import { NeovimBufferUpdateManager } from "./../../src/neovim/NeovimBufferUpdateManager"
+
+describe("NeovimBufferUpdateManagerTests", () => {
+    it("tests", async () => {
+        assert.ok(false, "fail")
+    })
+})
