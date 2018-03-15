d5ae7c9555492e1aa72604d4c59bbce58d715ec7
Bryan Phelps <bryphe@outlook.com>
2018-03-15 13:58:25 -0700

    Initial tutorials sidebar

--- browser/src/Services/Learning/LearningPane.tsx
+++ browser/src/Services/Learning/LearningPane.tsx
@@ -10,7 +10,7 @@ import * as React from "react"
 
 // import * as path from "path"
 
-import { Event } from "oni-types"
+import { Event, IEvent, IDisposable } from "oni-types"
 
 import { TutorialManager } from "./Tutorial/TutorialManager"
 
@@ -18,14 +18,16 @@ import { SidebarPane } from "./../Sidebar"
 // import { IBookmark, IBookmarksProvider } from "./index"
 
 // import { SidebarEmptyPaneView } from "./../../UI/components/SidebarEmptyPaneView"
-// import { SidebarContainerView, SidebarItemView } from "./../../UI/components/SidebarItemView"
+import { SidebarContainerView, SidebarItemView } from "./../../UI/components/SidebarItemView"
 import { VimNavigator } from "./../../UI/components/VimNavigator"
 
 export class LearningPane implements SidebarPane {
     private _onEnter = new Event<void>()
     private _onLeave = new Event<void>()
 
-    constructor(private _tutorialManager: TutorialManager) {}
+    constructor(private _tutorialManager: TutorialManager) {
+        console.log(this._tutorialManager)
+    }
 
     public get id(): string {
         return "oni.sidebar.learning"
@@ -36,7 +38,7 @@ export class LearningPane implements SidebarPane {
     }
 
     public enter(): void {
-        this._tutorialManager.startTutorial(null)
+        // this._tutorialManager.startTutorial(null)
         this._onEnter.dispatch()
     }
 
@@ -45,12 +47,89 @@ export class LearningPane implements SidebarPane {
     }
 
     public render(): JSX.Element {
+        return <LearningPaneView onEnter={this._onEnter} onLeave={this._onLeave} />
+    }
+}
+
+export interface ILearningPaneViewProps {
+    onEnter: IEvent<void>
+    onLeave: IEvent<void>
+}
+
+export interface ILearningPaneViewState {
+    isActive: boolean
+}
+
+export class PureComponentWithDisposeTracking<TProps, TState> extends React.PureComponent<
+    TProps,
+    TState
+> {
+    private _subscriptions: IDisposable[] = []
+
+    public componentDidMount(): void {
+        this._cleanExistingSubscriptions()
+    }
+
+    public componentWillUnmount(): void {
+        this._cleanExistingSubscriptions()
+    }
+
+    protected trackDisposable(disposable: IDisposable): void {
+        this._subscriptions.push(disposable)
+    }
+
+    private _cleanExistingSubscriptions(): void {
+        this._subscriptions.forEach(s => s.dispose())
+        this._subscriptions = []
+    }
+}
+
+export class LearningPaneView extends PureComponentWithDisposeTracking<
+    ILearningPaneViewProps,
+    ILearningPaneViewState
+> {
+    constructor(props: ILearningPaneViewProps) {
+        super(props)
+
+        this.state = {
+            isActive: false,
+        }
+    }
+
+    public componentDidMount(): void {
+        super.componentDidMount()
+
+        this.trackDisposable(this.props.onEnter.subscribe(() => this.setState({ isActive: true })))
+        this.trackDisposable(this.props.onLeave.subscribe(() => this.setState({ isActive: false })))
+    }
+
+    public render(): JSX.Element {
+        const ids = ["tutorial_container", "a", "b", "c"]
+
         return (
             <VimNavigator
-                ids={[]}
-                active={false}
-                render={() => {
-                    return <div>{JSON.stringify(this._tutorialManager.getTutorialInfo())}</div>
+                ids={ids}
+                active={this.state.isActive}
+                render={selectedId => {
+                    const items = ["a", "b", "c"].map(item => (
+                        <SidebarItemView
+                            indentationLevel={1}
+                            isFocused={selectedId === item}
+                            text={item}
+                        />
+                    ))
+
+                    return (
+                        <SidebarContainerView
+                            indentationLevel={0}
+                            isFocused={selectedId === "tutorial_container"}
+                            text={"Tutorials"}
+                            isContainer={true}
+                            isExpanded={true}
+                        >
+                            {items}
+                        </SidebarContainerView>
+                    )
                 }}
             />
         )
