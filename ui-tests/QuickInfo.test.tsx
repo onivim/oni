67037360ce923861dcb075b3025bed0a1473f1b3
Akin909 <akin.sowemimo@gmail.com>
2018-03-16 11:12:57 +0000

    add a few extra tests

--- ui-tests/QuickInfo.test.tsx
+++ ui-tests/QuickInfo.test.tsx
@@ -2,11 +2,26 @@ import { mount, shallow } from "enzyme"
 import { shallowToJson } from "enzyme-to-json"
 import * as React from "react"
 
-import { QuickInfoTitle } from "./../browser/src/UI/components/QuickInfo"
+import {
+    QuickInfoDocumentation,
+    QuickInfoTitle,
+    Title,
+} from "./../browser/src/UI/components/QuickInfo"
 
 describe("<QuickInfo />", () => {
-    it("renders a shallow instance of the unconnected component", () => {
+    it("renders a shallow instance of component", () => {
         const wrapper = shallow(<QuickInfoTitle />)
         expect(wrapper.length).toEqual(1)
+    })
+
+    it("renders the correct text", () => {
+        const wrapper = mount(<QuickInfoTitle text="test text" />)
+        expect(wrapper.text()).toContain("test text")
+    })
+
+    it("renders the documentation correctly", () => {
+        const test = "line One\nline Two\nline Three"
+        const wrapper = shallow(<QuickInfoDocumentation text={test} />)
+        expect(wrapper.children()).toHaveLength(3)
     })
 })
