642bad61f39515223936f58286ee964d9115f23e
Bryan Phelps <bryphe@outlook.com>
2018-03-20 10:58:31 -0700

    Revert changes to travis-build

--- build/script/travis-build.sh
+++ build/script/travis-build.sh
@@ -20,13 +20,6 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export ONI_NEOVIM_PATH
 fi
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    # Set debug flag for electron-builder, 
-    # to troubleshoot intermittent failures with building the dmg
-    DEBUG=electron-builder,electron-builder:*
-    export DEBUG
-fi
-
 npm run build
 npm run test:unit
 npm run lint
