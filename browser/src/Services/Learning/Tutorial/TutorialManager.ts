03d07fa8a4748f493b1c924b3afae6ec992e76ed
Bryan Phelps <bryphe@outlook.com>
2018-03-20 19:48:51 -0700

    FIxes

--- browser/src/Services/Learning/Tutorial/TutorialManager.ts
+++ browser/src/Services/Learning/Tutorial/TutorialManager.ts
@@ -4,10 +4,11 @@
 
 import { EditorManager } from "./../../EditorManager"
 
-import { ITutorial, ITutorialMetadata } from "./ITutorial"
-import * as Tutorials from "./Tutorials"
+import { IPersistentStore } from "./../../../PersistentStore"
 
+import { ITutorial, ITutorialMetadata } from "./ITutorial"
 import { TutorialBufferLayer } from "./TutorialBufferLayer"
+import * as Tutorials from "./Tutorials"
 
 export interface ITutorialPersistedState {
     completedTutorialIds: string[]
@@ -15,23 +16,70 @@ export interface ITutorialPersistedState {
 
 export interface ITutorialMetadataWithProgress {
     tutorialInfo: ITutorialMetadata
-    completed: boolean
+    completionInfo: ITutorialCompletionInfo
+}
+
+export interface ITutorialCompletionInfo {
+    keyPresses: number
+    time: number /* milliseconds */
+}
+
+export type IdToCompletionInfo = { [tutorialId: string]: ITutorialCompletionInfo }
+
+export interface IPersistedTutorialState {
+    completionInfo: IdToCompletionInfo
 }
 
 export class TutorialManager {
     private _tutorials: ITutorial[] = []
 
-    constructor(private _editorManager: EditorManager) {}
+    private _persistedState: IPersistedTutorialState = { completionInfo: {} }
+
+    constructor(
+        private _editorManager: EditorManager,
+        private _persistentStore: IPersistentStore<IPersistedTutorialState>,
+    ) {}
+
+    public async start(): Promise<void> {
+        this._persistedState = await this._persistentStore.get()
+    }
 
     public getTutorialInfo(): ITutorialMetadataWithProgress[] {
-        return this._tutorials.map(tut => ({
+        return this._getSortedTutorials().map(tut => ({
             tutorialInfo: tut.metadata,
-            completed: false,
+            completionInfo: this._getCompletionState(tut.metadata.id),
         }))
     }
 
     public registerTutorial(tutorial: ITutorial): void {
         this._tutorials.push(tutorial)
+    }
+
+    public async notifyTutorialCompleted(
+        id: string,
+        completionInfo: ITutorialCompletionInfo,
+    ): Promise<void> {
+        this._persistedState[id] = completionInfo
+        await this._persistentStore.set(this._persistedState)
+    }
+
+    public getNextTutorialId(currentTutorialId?: string): string {
+        const sortedTutorials = this._getSortedTutorials()
+
+        if (!currentTutorialId) {
+            return sortedTutorials[0].metadata.id
+        }
+
+        const currentTuturial = sortedTutorials.findIndex(
+            tut => tut.metadata.id === currentTutorialId,
+        )
+        const nextTutorial = currentTuturial + 1
+
+        if (nextTutorial >= sortedTutorials.length) {
+            return null
+        }
+
+        return sortedTutorials[nextTutorial].metadata.id
     }
 
     public async startTutorial(id: string): Promise<void> {
@@ -40,5 +88,13 @@ export class TutorialManager {
         const layer = new TutorialBufferLayer()
         layer.startTutorial(new Tutorials.BasicMovementTutorial())
         buf.addLayer(layer)
+    }
+
+    private _getSortedTutorials(): ITutorial[] {
+        return this._tutorials
+    }
+
+    private _getCompletionState(id: string): ITutorialCompletionInfo {
+        return this._persistedState[id] || null
     }
 }
