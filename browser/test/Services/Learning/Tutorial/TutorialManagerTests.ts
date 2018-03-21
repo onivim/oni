285d2c7ff599c60266d970064b96dda6b53383c7
Bryan Phelps <bryphe@outlook.com>
2018-03-21 10:45:33 -0700

    Additional files

---
+++ browser/test/Services/Learning/Tutorial/TutorialManagerTests.ts
@@ -0,0 +1,13 @@
+/**
+ * TutorialManagerTests.ts
+ */
+
+import * as assert from "assert"
+
+import { TutorialManager } from "./../../../../src/Services/Learning/Tutorial/TutorialManager"
+
+describe("TutorialManagerTests", () => {
+    it("tests", async () => {
+        assert.ok(false, "fail")
+    })
+})
