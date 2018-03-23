1e00d9223bf07267519440a51701cfc65b3738d7
Bryan Phelps <bryphe@outlook.com>
2018-03-22 17:35:01 -0700

    Add display driver back to integration test too

--- build/script/travis-test.sh
+++ build/script/travis-test.sh
@@ -4,6 +4,11 @@ echo Travis build - detected OS is: "$TRAVIS_OS_NAME"
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  DISPLAY=:99.0
+  export DISPLAY
+  sh -e /etc/init.d/xvfb start
+  sleep 3
+
   # Install Neovim
   curl -LO https://github.com/neovim/neovim/releases/download/v0.2.2/nvim.appimage
   chmod u+x nvim.appimage
