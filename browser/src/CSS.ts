860e84c8098957278dd2d0c3cdcefbbc25541ac8
cryza <manuel@hornung.in>
2018-03-24 17:02:22 +0100

    Remove require of deleted file

--- browser/src/CSS.ts
+++ browser/src/CSS.ts
@@ -8,8 +8,6 @@ export const activate = () => {
     require("./UI/components/common.less") // tslint:disable-line no-var-requires
     require("./overlay.less") // tslint:disable-line
 
-    require("./Services/ContextMenu/ContextMenu.less") // tslint:disable-line
-
     require("./UI/components/Error.less")
     require("./UI/components/InstallHelp.less")
     require("./UI/components/QuickInfo.less")
