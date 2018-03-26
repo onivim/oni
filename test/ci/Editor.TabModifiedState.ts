f4fb25bcb50936276a8d875381188f8a08ac9f70
cryza <manuel@hornung.in>
2018-03-26 22:49:11 +0200

    Fix tab modified ui test

--- test/ci/Editor.TabModifiedState.ts
+++ test/ci/Editor.TabModifiedState.ts
@@ -5,7 +5,7 @@
 import * as assert from "assert"
 import * as Oni from "oni-api"
 
-import { createNewFile, getElementByClassName } from "./Common"
+import { createNewFile, getElementByTestAttribute, testAttributeSelector } from "./Common"
 
 export const test = async (oni: Oni.Plugin.Api) => {
     await oni.automation.waitForEditors()
@@ -13,9 +13,11 @@ export const test = async (oni: Oni.Plugin.Api) => {
     await createNewFile("js", oni)
 
     // Check the buffer did not have a modified state by default
-    let tabState = getElementByClassName("tab selected not-dirty")
+    const tab = getElementByTestAttribute("tab")
+    const modifiedIcon = tab.querySelector(testAttributeSelector("modified-icon"))
+    const modifiedIconStyle = getComputedStyle(modifiedIcon)
 
-    assert.ok(tabState, "Check tab has no modified icon")
+    assert.strictEqual(modifiedIconStyle.opacity, "0", "Check tab has no modified icon")
 
     // Next, edit the buffer and check that shows up
     oni.automation.sendKeys("i")
@@ -26,9 +28,7 @@ export const test = async (oni: Oni.Plugin.Api) => {
 
     await oni.automation.waitFor(() => oni.editors.activeEditor.mode === "normal")
 
-    tabState = getElementByClassName("tab selected is-dirty")
-
-    assert.ok(tabState, "Check tab now has a modified icon")
+    assert.strictEqual(modifiedIconStyle.opacity, "1", "Check tab now has a modified icon")
 
     // Next, open a split in the current tab, and check the tab still remains dirty
     oni.automation.sendKeys(":")
@@ -37,9 +37,11 @@ export const test = async (oni: Oni.Plugin.Api) => {
 
     await oni.automation.waitFor(() => oni.editors.activeEditor.activeBuffer.id === "2")
 
-    tabState = getElementByClassName("tab selected is-dirty")
-
-    assert.ok(tabState, "Check tab has modified icon after opening split")
+    assert.strictEqual(
+        modifiedIconStyle.opacity,
+        "1",
+        "Check tab has modified icon after opening split",
+    )
 
     // Finally, swap tab and swap back to ensure the modified status remains.
     oni.automation.sendKeys(":")
@@ -56,9 +58,11 @@ export const test = async (oni: Oni.Plugin.Api) => {
     // the second tab.
     await oni.automation.waitFor(() => oni.editors.activeEditor.activeBuffer.id === "2")
 
-    tabState = getElementByClassName("tab selected is-dirty")
-
-    assert.ok(tabState, "Check tab still has modified icon after swapping")
+    assert.strictEqual(
+        modifiedIconStyle.opacity,
+        "1",
+        "Check tab still has modified icon after swapping",
+    )
 }
 
 // Bring in custom config.
