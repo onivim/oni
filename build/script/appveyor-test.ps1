913a532b8ef809aba3397359a2c5b34b73019c2e
CrossR <sc13rjc@leeds.ac.uk>
2018-03-29 11:37:56 +0100

    Check error code of each step.
    Taken fom Neovim AppVeyor build script.

--- build/script/appveyor-test.ps1
+++ build/script/appveyor-test.ps1
@@ -1,14 +1,20 @@
+function exitIfFailed() {
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+}
+
 # Output useful info for debugging.
 node --version
 npm --version
 
 # Build
-npm run build
-npm run lint
-npm run test:unit
+npm run build ; exitIfFailed
+npm run lint ; exitIfFailed
+npm run test:unit ; exitIfFailed
 
 # Create setup package
-npm run copy-icons
+npm run copy-icons ; exitIfFailed
 
 $platform = (Get-Item env:PLATFORM).value
 
@@ -18,11 +24,11 @@ if ($platform -eq "x86") {
     npm run dist:win:x64
 }
 
-npm run pack:win
+npm run pack:win ; exitIfFailed
 
 # Run integration tests
-npm run test:integration
-npm run demo:screenshot
+npm run test:integration ; exitIfFailed
+npm run demo:screenshot ; exitIfFailed
 
 # Upload bits to azure
-npm run upload:dist
+npm run upload:dist ; exitIfFailed
