c4705621a7fd1a19ceab0d0ff5d45f745148ebad
CrossR <sc13rjc@leeds.ac.uk>
2018-03-30 00:46:46 +0100

    Fix test.

--- test/ci/Neovim.CallOniCommands.ts
+++ test/ci/Neovim.CallOniCommands.ts
@@ -14,30 +14,37 @@ export const test = async (oni: Oni.Plugin.Api) => {
     oni.automation.sendKeys(":")
     await oni.automation.waitFor(() => !!getElementByClassName("command-line"))
     oni.automation.sendKeys('call OniCommand("quickOpen.show")')
+    oni.automation.sendKeys("<enter>")
 
     await oni.automation.waitFor(() => oni.menu.isMenuOpen())
     assert(oni.menu.isMenuOpen(), "Check menu opened correctly.")
 
+    oni.commands.executeCommand("menu.close")
+
     // Now test passing optional arguments
     // Create a test command to call, but could be swapped to use the browser
     // when more easily accessible.
-
     oni.commands.registerCommand({
         command: "ciTest.test",
         name: "Test passing over args.",
         detail: "Opens a file.",
-        execute: (file?: string) => oni.editors.activeEditor.openFile(file),
+        execute: (file?: string) => oni.editors.activeEditor.neovim.command(`:e ${file}`),
     })
-
     await oni.automation.sleep(500)
+
     oni.automation.sendKeys(":")
     await oni.automation.waitFor(() => !!getElementByClassName("command-line"))
     oni.automation.sendKeys('call OniCommand("ciTest.test", "testFile.tsx")')
+    oni.automation.sendKeys("<enter>")
 
     await oni.automation.waitFor(() =>
         oni.editors.activeEditor.activeBuffer.filePath.endsWith("testFile.tsx"),
     )
-    assert(oni.menu.isMenuOpen(), "Check file opened correctly after being passed over.")
 
-    await oni.automation.sleep(500)
+    const currentBuffer = oni.editors.activeEditor.activeBuffer.filePath
+
+    assert(
+        currentBuffer.endsWith("testFile.tsx"),
+        "Check file opened correctly after being passed over.",
+    )
 }
