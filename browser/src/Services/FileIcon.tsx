9b8aa116289a0cfd2b749575a43690264cfcae38
Manuel Hornung <manuel@hornung.in>
2018-03-30 10:35:02 +0200

    Refactor FileIcon to styled-components (#1980)

--- browser/src/Services/FileIcon.tsx
+++ browser/src/Services/FileIcon.tsx
@@ -7,32 +7,58 @@
 
 import * as React from "react"
 
+import { css, keyframes, styled, withProps } from "../UI/components/common"
 import { getInstance } from "./IconThemes"
 
-export interface IFileIconProps {
+const appearAnimationKeyframes = keyframes`
+    0% {
+        opacity: 0;
+        transform: scale(0.8);
+    }
+    100% {
+        opacity: 1;
+        transform: scale(1);
+    }
+`
+
+const appearAnimation = css`
+    animation-name: ${appearAnimationKeyframes};
+    animation-duration: 0.25s;
+    animation-timing-function: ease-in;
+    animation-fill-mode: forwards;
+    opacity: 1;
+`
+
+const Icon = withProps<{ playAppearAnimation: boolean }>(styled.i)`
+    ${props => (props.playAppearAnimation ? appearAnimation : "")}
+`
+
+interface IFileIconProps {
     fileName: string
     language?: string
 
     isLarge?: boolean
 
-    additionalClassNames?: string
+    playAppearAnimation?: boolean
 }
 
-export class FileIcon extends React.PureComponent<IFileIconProps, {}> {
-    public render(): JSX.Element {
-        if (!this.props.fileName) {
-            return null
-        }
-
-        const icons = getInstance()
-
-        const className =
-            icons.getIconClassForFile(this.props.fileName, this.props.language) +
-            (this.props.isLarge ? " fa-lg" : "")
-        const additionalClasses = this.props.additionalClassNames || ""
-
-        return <i className={className + " " + additionalClasses} aria-hidden={true} />
+export const FileIcon = (props: IFileIconProps) => {
+    if (!props.fileName) {
+        return null
     }
+
+    const icons = getInstance()
+
+    const className =
+        icons.getIconClassForFile(props.fileName, props.language) + (props.isLarge ? " fa-lg" : "")
+
+    return (
+        <Icon
+            playAppearAnimation={props.playAppearAnimation}
+            className={className}
+            aria-hidden={true}
+        />
+    )
 }
 
 export const getFileIcon = (fileName: string) => <FileIcon fileName={fileName} isLarge={true} />
