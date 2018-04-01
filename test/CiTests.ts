ba24d3e3b2f381d91e0083b6af1210322d3773a0
Akin909 <akin.sowemimo@gmail.com>
2018-04-01 16:57:19 +0100

    readd ci tests

--- test/CiTests.ts
+++ test/CiTests.ts
@@ -10,48 +10,48 @@ import { IFailedTest, Oni, runInProcTest } from "./common"
 const LongTimeout = 5000
 
 const CiTests = [
+    // Core functionality tests
+    "Api.Buffer.AddLayer",
+    "Api.Overlays.AddRemoveTest",
+    "AutoClosingPairsTest",
+    "AutoCompletionTest-CSS",
+    "AutoCompletionTest-HTML",
+    "AutoCompletionTest-TypeScript",
+
+    "Configuration.JavaScriptEditorTest",
+    "Configuration.TypeScriptEditor.NewConfigurationTest",
+    "Configuration.TypeScriptEditor.CompletionTest",
+
+    "Editor.ExternalCommandLineTest",
+    "Editor.BufferModifiedState",
+    "Editor.OpenFile.PathWithSpacesTest",
+    "Editor.TabModifiedState",
+    "Editor.CloseTabWithTabModesTabsTest",
+    "MarkdownPreviewTest",
+    "PaintPerformanceTest",
+    "QuickOpenTest",
+    "StatusBar-Mode",
+    "Neovim.InvalidInitVimHandlingTest",
     "initVimPromptNotificationTest",
-    // // Core functionality tests
-    // "Api.Buffer.AddLayer",
-    // "Api.Overlays.AddRemoveTest",
-    // "AutoClosingPairsTest",
-    // "AutoCompletionTest-CSS",
-    // "AutoCompletionTest-HTML",
-    // "AutoCompletionTest-TypeScript",
-    //
-    // "Configuration.JavaScriptEditorTest",
-    // "Configuration.TypeScriptEditor.NewConfigurationTest",
-    // "Configuration.TypeScriptEditor.CompletionTest",
-    //
-    // "Editor.ExternalCommandLineTest",
-    // "Editor.BufferModifiedState",
-    // "Editor.OpenFile.PathWithSpacesTest",
-    // "Editor.TabModifiedState",
-    // "Editor.CloseTabWithTabModesTabsTest",
-    // "MarkdownPreviewTest",
-    // "PaintPerformanceTest",
-    // "QuickOpenTest",
-    // "StatusBar-Mode",
-    // "Neovim.InvalidInitVimHandlingTest",
-    // "NoInstalledNeovim",
-    // "Sidebar.ToggleSplitTest",
-    //
-    // "Snippets.BasicInsertTest",
-    //
-    // "WindowManager.ErrorBoundary",
-    // "Workspace.ConfigurationTest",
-    // // Regression Tests
-    // "Regression.1251.NoAdditionalProcessesOnStartup",
-    // "Regression.1296.SettingColorsTest",
-    // "Regression.1295.UnfocusedWindowTest",
-    // "Regression.1799.MacroApplicationTest",
-    //
-    // "TextmateHighlighting.DebugScopesTest",
-    // "TextmateHighlighting.ScopesOnEnterTest",
-    // "TextmateHighlighting.TokenColorOverrideTest",
-    //
-    // // This test occasionally hangs and breaks tests after - trying to move it later...
-    // "LargeFileTest",
+    "NoInstalledNeovim",
+    "Sidebar.ToggleSplitTest",
+
+    "Snippets.BasicInsertTest",
+
+    "WindowManager.ErrorBoundary",
+    "Workspace.ConfigurationTest",
+    // Regression Tests
+    "Regression.1251.NoAdditionalProcessesOnStartup",
+    "Regression.1296.SettingColorsTest",
+    "Regression.1295.UnfocusedWindowTest",
+    "Regression.1799.MacroApplicationTest",
+
+    "TextmateHighlighting.DebugScopesTest",
+    "TextmateHighlighting.ScopesOnEnterTest",
+    "TextmateHighlighting.TokenColorOverrideTest",
+
+    // This test occasionally hangs and breaks tests after - trying to move it later...
+    "LargeFileTest",
 ]
 
 const WindowsOnlyTests = [
