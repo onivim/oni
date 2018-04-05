527874156fbaa850a1d3143a7f2d2ceafe6bf6f3
samdoiron <sam.doiron96@gmail.com>
2018-04-05 20:13:43 -0300

    Account for WheelEvent.deltaMode during scrolling
    The delta values given on `WheelEvent`s can sometimes be
    measured in units other than pixels.

--- browser/src/Input/Mouse.ts
+++ browser/src/Input/Mouse.ts
@@ -2,12 +2,13 @@ import { EventEmitter } from "events"
 
 import { IScreen } from "./../neovim"
 
+const SCROLL_THRESHOLD_IN_PIXELS = 100
+
 // TODO
 // Handle modifier keys
 export class Mouse extends EventEmitter {
     private _isDragging = false
     private _scrollDelta = 0
-    private SCROLL_THRESHOLD = 100
 
     constructor(private _editorElement: HTMLDivElement, private _screen: IScreen) {
         super()
@@ -42,24 +43,39 @@ export class Mouse extends EventEmitter {
                 scrollcmdY += `C-` // The S- and C- prefixes have the same effect
             }
 
-            // This is 'less than' because I made this on a mac to behave just like
-            // the other applications I use. However, because OSX is super weird, it
-            // might be backwards
-            if (evt.deltaY) {
-                this._scrollDelta += evt.deltaY
-                if (Math.abs(this._scrollDelta) >= this.SCROLL_THRESHOLD) {
-                    if (this._scrollDelta < 0) {
-                        scrollcmdY += `ScrollWheelUp>`
-                    } else {
-                        scrollcmdY += `ScrollWheelDown>`
-                    }
-                    this._scrollDelta = 0
-                    this.emit("mouse", scrollcmdY + `<${line},${column}>`)
-                }
+            const normalizedDelta = this._normalizeScrollDeltaToPixels(evt.deltaY, evt.deltaMode)
+            if (!normalizedDelta) {
+                return
             }
 
             this._isDragging = false
+            this._scrollDelta += normalizedDelta
+            if (this._scrollDeltaIsSignificant()) {
+                // This is 'less than' because I made this on a mac to behave just like
+                // the other applications I use. However, because OSX is super weird, it
+                // might be backwards.
+                if (this._scrollDelta < 0) {
+                    scrollcmdY += `ScrollWheelUp>`
+                } else {
+                    scrollcmdY += `ScrollWheelDown>`
+                }
+                this._scrollDelta = 0
+                this.emit("mouse", scrollcmdY + `<${line},${column}>`)
+            }
         })
+    }
+
+    private _normalizeScrollDeltaToPixels(delta: number, deltaMode: number): number {
+        switch (deltaMode) {
+            case WheelEvent.DOM_DELTA_PIXEL:
+                return delta
+            case WheelEvent.DOM_DELTA_LINE:
+                return delta * this._screen.fontHeightInPixels
+            case WheelEvent.DOM_DELTA_PAGE:
+                return delta * this._screen.fontHeightInPixels * this._screen.height
+            default:
+                return delta
+        }
     }
 
     private _convertEventToPosition(evt: MouseEvent): { line: number; column: number } {
@@ -70,5 +86,9 @@ export class Mouse extends EventEmitter {
             line: Math.floor(mouseX / this._screen.fontWidthInPixels),
             column: Math.floor(mouseY / this._screen.fontHeightInPixels),
         }
+    }
+
+    private _scrollDeltaIsSignificant(): boolean {
+        return Math.abs(this._scrollDelta) >= SCROLL_THRESHOLD_IN_PIXELS
     }
 }
