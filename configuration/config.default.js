b0411012bd06b6ab2d138a086d79ceb863db83f4
Himura Kazuto <glagol15@gmail.com>
2018-04-05 14:36:31 +0300

    Update config.default.js

--- configuration/config.default.js
+++ configuration/config.default.js
@@ -9,11 +9,11 @@ const activate = oni => {
     //
     // Add input bindings here:
     //
-    oni.input.bind("<f1>", "commands.show")
+    oni.input.bind("<f11>", "oni.debug.openDevTools")
     oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
 
     //
-    // Or remove the default bindings here by uncommenting the below line:
+    // Or remove the default bindings:
     //
     // oni.input.unbind("<c-p>")
 }
