onivim/oni

commit 18faff4b4c8777bdd44d375c5df61135a5a87d5c

Stop propagation of event in File Explorer to avoid multiple click events (#2078)

browser/src/Services/Explorer/ExplorerView.tsx
@@ -42,6 +42,12 @@ const scrollIntoViewIfNeeded = (elem: HTMLElement) => {
     // tslint:disable-next-line
     elem && elem["scrollIntoViewIfNeeded"] && elem["scrollIntoViewIfNeeded"]()
 }
+const stopPropagation = (fn: () => void) => {
+    return (e: React.MouseEvent<HTMLElement>) => {
+        e.stopPropagation()
+        fn()
+    }
+}
 
 const Types = {
     FILE: "FILE",
@@ -95,7 +101,7 @@ export class NodeView extends React.PureComponent<INodeViewProps, {}> {
                                     isOver={isOver && canDrop}
                                     didDrop={didDrop}
                                     canDrop={canDrop}
-                                    onClick={() => this.props.onClick()}
+                                    onClick={stopPropagation(() => this.props.onClick())}
                                     text={node.name}
                                     isFocused={this.props.isSelected}
                                     isContainer={false}
@@ -118,7 +124,7 @@ export class NodeView extends React.PureComponent<INodeViewProps, {}> {
                                     isOver={isOver}
                                     isContainer={true}
                                     isExpanded={node.expanded}
-                                    onClick={() => this.props.onClick()}
+                                    onClick={stopPropagation(() => this.props.onClick())}
                                     text={node.name}
                                     isFocused={this.props.isSelected}
                                 />
@@ -144,7 +150,7 @@ export class NodeView extends React.PureComponent<INodeViewProps, {}> {
                                     text={node.name}
                                     isFocused={this.props.isSelected}
                                     indentationLevel={node.indentationLevel}
-                                    onClick={() => this.props.onClick()}
+                                    onClick={stopPropagation(() => this.props.onClick())}
                                 />
                             )
                         }}

browser/src/UI/components/SidebarItemView.tsx
@@ -19,7 +19,7 @@ export interface ISidebarItemViewProps {
     isContainer?: boolean
     indentationLevel: number
     icon?: JSX.Element
-    onClick: () => void
+    onClick: (e: React.MouseEvent<HTMLElement>) => void
 }
 
 const px = (num: number): string => num.toString() + "px"
@@ -105,7 +105,7 @@ export interface ISidebarContainerViewProps extends IContainerProps {
     isFocused: boolean
     indentationLevel?: number
     isContainer?: boolean
-    onClick: () => void
+    onClick: (e: React.MouseEvent<HTMLElement>) => void
 }
 
 interface IContainerProps {

commit 07087c33fa3494007bc3fb37db920005eec9baad

Regression from #2078: Event can be null when Sneak navigation is used

browser/src/Services/Explorer/ExplorerView.tsx
@@ -43,8 +43,10 @@ const scrollIntoViewIfNeeded = (elem: HTMLElement) => {
     elem && elem["scrollIntoViewIfNeeded"] && elem["scrollIntoViewIfNeeded"]()
 }
 const stopPropagation = (fn: () => void) => {
-    return (e: React.MouseEvent<HTMLElement>) => {
-        e.stopPropagation()
+    return (e?: React.MouseEvent<HTMLElement>) => {
+        if (e) {
+            e.stopPropagation()
+        }
         fn()
     }
 }

browser/src/UI/components/SidebarItemView.tsx
@@ -19,7 +19,7 @@ export interface ISidebarItemViewProps {
     isContainer?: boolean
     indentationLevel: number
     icon?: JSX.Element
-    onClick: (e: React.MouseEvent<HTMLElement>) => void
+    onClick: (e?: React.MouseEvent<HTMLElement>) => void
 }
 
 const px = (num: number): string => num.toString() + "px"