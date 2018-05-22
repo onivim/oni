0af8a04143b6100e821b7fbd5acbd5ea51a74a08
Bryan Phelps <bryphe@outlook.com>
2018-05-21 19:09:15 -0700

    Stub out unit tests for key binding tree

--- browser/test/Services/InputManager/KeyBindingTreeTests.ts
+++ browser/test/Services/InputManager/KeyBindingTreeTests.ts
@@ -4,11 +4,45 @@
 
 import * as assert from "assert"
 
-import * as KeyBindingTree from "./../../../test/Services/InputManager/KeyBindingTree"
+import * as KeyBindingTree from "./../../../src/Services/InputManager/KeyBindingTree"
 
 describe("KeyBindingTreeTests", () => {
-    it("fails", async () => {
-        assert.ok(false, "fail")
-        KeyBindingTree.c
+    describe("create", () => {
+        it("has no bindings or children", () => {
+            const kbt = KeyBindingTree.create()
+
+            assert.deepEqual(kbt.children, {})
+            assert.deepEqual(kbt.bindings, [])
+        })
+    })
+
+    describe("isPotentialChord", () => {
+        it("returns false for unbound key", () => {
+            assert.ok(false)
+        })
+
+        it("returns true if part of chord, but false if complete", () => {
+            assert.ok(false)
+        })
+    })
+
+    describe("getKeyBindingsForChord", () => {
+        it("returns empty array if no bindings", () => {
+            assert.ok(false)
+        })
+
+        it("gives value for single item chord", () => {
+            assert.ok(false)
+        })
+    })
+
+    describe("isTerminal", () => {
+        it("returns true for single item with no chorded bindings", () => {
+            assert.ok(false)
+        })
+
+        it("returns false for key that has potential bindings", () => {
+            assert.ok(false)
+        })
     })
 })
