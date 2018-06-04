53ac043754f4bb8b84738f62274265e73227f879
Akin909 <akin.sowemimo@gmail.com>
2018-06-04 19:36:31 +0100

    lint fix

--- test/ci/PaintPerformanceTest.config.js
+++ test/ci/PaintPerformanceTest.config.js
@@ -1,10 +1,10 @@
 // For more information on customizing Oni,
 // check out our wiki page:
 // https://github.com/onivim/oni/wiki/Configuration
-
+// tslint:disable
 module.exports = {
+    "sidebar.enabled": false,
     "statusbar.enabled": false,
     "tabs.mode": "hidden",
     "ui.animations.enabled": false,
-    "sidebar.enabled": false,
 }
