ef6f7fa9763d58c306bcf7795d59e052ef448247
CrossR <sc13rjc@leeds.ac.uk>
2018-06-20 15:48:59 +0100

    Update tests for Markdown preview.

--- test/ci/MarkdownPreviewTest.tsx
+++ test/ci/MarkdownPreviewTest.tsx
@@ -3,7 +3,13 @@
  */
 
 import { Assertor } from "./Assert"
-import { awaitEditorMode, getTemporaryFilePath, insertText, navigateToFile } from "./Common"
+import {
+    awaitEditorMode,
+    getTemporaryFilePath,
+    insertText,
+    navigateToFile,
+    getElementByClassName,
+} from "./Common"
 
 import * as Oni from "oni-api"
 
@@ -11,6 +17,7 @@ interface IMarkdownPreviewPlugin {
     isPaneOpen(): boolean
     getUnrenderedContent(): string
     getRenderedContent(): string
+    toggle(): void
 }
 
 export const settings = {
@@ -24,25 +31,91 @@ export async function test(oni: Oni.Plugin.Api) {
 
     await oni.automation.waitForEditors()
 
+    // Wait for Plugin to be loaded
     await oni.automation.waitFor(() => oni.plugins.loaded)
     const markdownPlugin = oni.plugins.getPlugin(
         "oni-plugin-markdown-preview",
     ) as IMarkdownPreviewPlugin
     assert.defined(markdownPlugin, "plugin instance")
 
-    assert.assert(!markdownPlugin.isPaneOpen(), "Preview pane is not initially closed")
+    // Check its not open by default
+    assert.assert(!markdownPlugin.isPaneOpen(), "Preview pane is initially closed.")
 
-    await navigateToFile(getTemporaryFilePath("md"), oni)
+    // Check it opens when navigating into a markdown file.
+    const markdownFilePath = getTemporaryFilePath("md")
+    await navigateToFile(markdownFilePath, oni)
     await oni.automation.waitFor(() => markdownPlugin.isPaneOpen())
+
     assert.isEmpty(
         markdownPlugin.getUnrenderedContent().trim(),
-        "Preview pane for empty Markdown buffer",
+        "Preview pane shown for Markdown buffer.",
     )
 
+    // Check a Title is rendered correctly.
     await insertText(oni, "# Title 1")
     assert.contains(
         markdownPlugin.getRenderedContent(),
         ">Title 1</h1>",
-        "Preview pane with rendered header element",
+        "Preview pane with renders header element.",
+    )
+
+    // Check syntax highlights are applied.
+    oni.automation.sendKeys("o")
+    await oni.automation.waitFor(() => oni.editors.activeEditor.mode === "insert")
+
+    oni.automation.sendKeys("```<enter>")
+    oni.automation.sendKeys("const test = 'Oni'<enter>")
+    oni.automation.sendKeys("```")
+    oni.automation.sendKeys("<esc>")
+
+    await oni.automation.waitFor(() => oni.editors.activeEditor.mode === "normal")
+
+    assert.contains(
+        markdownPlugin.getRenderedContent(),
+        "code>",
+        "Code block present in preview HTML.",
+    )
+
+    assert.assert(
+        getElementByClassName("hljs-keyword").innerText === "const",
+        "Syntax highlights present in preview HTML.",
+    )
+
+    // Finally, test swapping between files.
+    // Swapping to a non-MD file should close it.
+    // Swapping to an MD file should open it.
+    // Closing the MD preview manually should cause it to remain closed.
+
+    const nonMDFilePath = getTemporaryFilePath("ts")
+
+    await navigateToFile(nonMDFilePath, oni)
+    await oni.automation.waitFor(
+        () => oni.editors.activeEditor.activeBuffer.language === "typescript",
+    )
+    assert.assert(!markdownPlugin.isPaneOpen(), "Preview pane is closed for non-MD file.")
+
+    await navigateToFile(markdownFilePath, oni)
+    await oni.automation.waitFor(
+        () => oni.editors.activeEditor.activeBuffer.language === "markdown",
+    )
+    assert.assert(markdownPlugin.isPaneOpen(), "Preview pane opens for MD file.")
+
+    // Manually closing preview, should remain closed now.
+    markdownPlugin.toggle()
+    assert.assert(!markdownPlugin.isPaneOpen(), "Preview pane closes on toggle.")
+
+    await navigateToFile(nonMDFilePath, oni)
+    await oni.automation.waitFor(
+        () => oni.editors.activeEditor.activeBuffer.language === "typescript",
+    )
+    await navigateToFile(markdownFilePath, oni)
+    await oni.automation.waitFor(
+        () => oni.editors.activeEditor.activeBuffer.language === "markdown",
+    )
+
+    // Should remain closed when swapping back in.
+    assert.assert(
+        !markdownPlugin.isPaneOpen(),
+        "Preview pane remains closed for MD file when manually closed.",
     )
 }
