7520a98f325ca16ec43dc4ed3251dc4dfa2ba276
CrossR <sc13rjc@leeds.ac.uk>
2018-06-30 14:36:37 +0100

    Clarify changes in cli_args.

--- cli/src/cli_args.ts
+++ cli/src/cli_args.ts
@@ -18,12 +18,10 @@ export function parseArgs(args: string[]): CLIArgs {
     return minimist(args, options) as CLIArgs
 }
 
+// Skip the first 2 arguments as that is the location of the Electron binary,
+// and the location of the cli.ts script.
 export function parseCLIProcessArgv(processArgv: string[]): CLIArgs {
     let [, , ...args] = processArgv
-
-    // if (process.env['VSCODE_DEV']) {
-    //     args = stripAppPath(args);
-    // }
 
     return parseArgs(args)
 }
