33b94e271498fb4cce5b4f1c3d7e76505398a4ba
Bryan Phelps <bryphe@outlook.com>
2018-06-30 10:32:24 -0700

    Gate tests to only run on Windows

---
+++ test/setup/WindowsInstallerTests.ts
@@ -0,0 +1,81 @@
+/**
+ * WindowsInstallerTests
+ */
+
+import * as assert from "assert"
+import * as cp from "child_process"
+import * as fs from "fs"
+import * as os from "os"
+import * as path from "path"
+
+const rootPath = path.join(__dirname, "..", "..", "..")
+const packageJsonPath = path.join(rootPath, "package.json")
+const oniVersion = require(packageJsonPath).version
+const installExecutablePath = path.join(rootPath, "dist", `Oni-${oniVersion}-win.exe`)
+
+const runInstaller = (setupExecutablePath: string, installDirectory: string) => {
+    return cp.spawnSync(installExecutablePath, [
+        "/silent",
+        "/norestart",
+        `/dir=${installDirectory}`,
+    ])
+}
+
+const runUninstaller = (uninstallerExecutablePath: string) => {
+    return cp.spawnSync(uninstallerExecutablePath, ["/silent", "/norestart"])
+}
+
+const getPathRegistryKey = () => {
+    // Use the `reg` command line tool to get information about the registry key
+    // Use `reg /?` or `reg query /?` at the command line ot get more info
+
+    const result = cp.spawnSync("reg", ["query", "HKCU\\Environment", "/e", "/f", "PATH"])
+
+    const output = result.output.toString()
+
+    // the output is multi-line, so we'll split it up
+    const [, , lastLine] = output.split("\r\n")
+
+    return lastLine.substring(29, lastLine.length)
+}
+
+if (os.platform() === "win32") {
+    describe("WindowsInstallerTests", () => {
+        it("installs / uninstalls to a test directory", () => {
+            const testDirectory = "oni-test-install-" + new Date().getTime()
+            const testPath = path.join("C:", testDirectory)
+
+            console.log("Installing to: " + testPath)
+            const result = runInstaller(installExecutablePath, testPath)
+
+            const doesInstallFolderExist = fs.existsSync(testPath)
+            assert.strictEqual(doesInstallFolderExist, true, "Validate install folder exists now")
+
+            const doesOniExeExists = fs.existsSync(path.join(testPath, "Oni.exe"))
+            assert.strictEqual(doesOniExeExists, true, "Validate Oni.exe exists")
+
+            const uninstallExecutablePath = path.join(testPath, "unins000.exe")
+            const doesUninstallExist = fs.existsSync(uninstallExecutablePath)
+            assert.strictEqual(doesUninstallExist, true, "Validate uninstaller exists")
+
+            const pathKey = getPathRegistryKey()
+            assert.ok(
+                pathKey.indexOf(testDirectory) >= 0,
+                "Validate path was added to registry key",
+            )
+
+            console.log("Running uninstaller...")
+            const uninstallResult = runUninstaller(uninstallExecutablePath)
+
+            assert.strictEqual(
+                doesInstallFolderExist,
+                true,
+                "Validate install folder doesn't exist after uninstall",
+            )
+
+            // TODO: This test fails today!
+            // const pathKey2 = getPathRegistryKey()
+            // assert.ok(pathKey2.indexOf(testDirectory) === -1, "Validate path was removed from registry after installation")
+        })
+    })
+}
