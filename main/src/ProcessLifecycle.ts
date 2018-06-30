a17908ecd929586d239e236261d286bd08ae148a
Bryan Phelps <bryphe@outlook.com>
2018-06-30 09:17:20 -0700

    Fix compilation error

--- main/src/ProcessLifecycle.ts
+++ main/src/ProcessLifecycle.ts
@@ -1,7 +1,5 @@
 import { app } from "electron"
 
-import * as Log from "./Log"
-
 export const makeSingleInstance = (options, callbackFunction) => {
     const isSecondInstance = app.makeSingleInstance((args, workingDirectory) => {
         callbackFunction({
