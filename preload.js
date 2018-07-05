c75a6ea10fe8fca9d862f323d8ec1e4dcb7700b1
Bryan Phelps <bryphe@outlook.com>
2018-07-05 10:13:30 -0700

    Add preload js

--- preload.js
+++ preload.js
@@ -1,6 +1,7 @@
 if (!process.env.ONI_WEBPACK_LOAD) {
     window.eval = global.eval = () => console.warn("eval is not available")
 }
+// test
 
 console.timeStamp("browser.domloaded")
 var path = "lib/browser/bundle.js"
