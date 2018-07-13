582da522f695f4f9504e3d32a211b98443650b39
Akin909 <akin.sowemimo@gmail.com>
2018-07-13 08:50:48 +0100

    remove jest coverage merging

--- browser/testCoverageReporter.js
+++ browser/testCoverageReporter.js
@@ -11,26 +11,11 @@ const mkdirp = require("mkdirp")
 const istanbulAPI = require("istanbul-api")
 const libCoverage = require("istanbul-lib-coverage")
 
-const addJestCoverage = coverageMap => {
-    const jestCoverageFile = path.join(__dirname, "..", "coverage", "jest", "coverage-final.json")
-    try {
-        const json = fs.readFileSync(jestCoverageFile)
-        console.log("coverage file", json)
-        if (json) {
-            coverageMap.merge(JSON.parse(json))
-            console.log("Adding jest coverage map")
-        }
-    } catch (e) {
-        console.log("Error merging jest coverage: ", e)
-    }
-}
-
 function Coverage(runner) {
     runner.on("end", () => {
         const mainReporter = istanbulAPI.createReporter()
         const coverageMap = libCoverage.createCoverageMap()
         console.log("Merging coverage map...")
-        addJestCoverage(coverageMap)
         coverageMap.merge(window.__coverage__ || {})
         console.log("Merging coverage map complete")
 
