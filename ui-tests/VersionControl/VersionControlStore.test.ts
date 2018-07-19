08ab3a719310a66c4fd35ad22ae3c03211236eda
Akin909 <akin.sowemimo@gmail.com>
2018-07-19 23:43:16 +0100

    add tests for new reducer branches

--- ui-tests/VersionControl/VersionControlStore.test.ts
+++ ui-tests/VersionControl/VersionControlStore.test.ts
@@ -1,10 +1,8 @@
 import { Store } from "redux"
 
-import store from "./../../browser/src/Services/VersionControl/VersionControlStore"
+import vcsStore from "./../../browser/src/Services/VersionControl/VersionControlStore"
 
 describe("Version control reducer test", () => {
-    const vcsStore = store
-
     it("Should correctly update the store with the vcs status", () => {
         const status = {
             currentBranch: "master",
@@ -27,13 +25,40 @@ describe("Version control reducer test", () => {
 
     it("should correctly update the focus state", () => {
         vcsStore.dispatch({ type: "ENTER" })
-        const state = store.getState()
+        const state = vcsStore.getState()
         expect(state.hasFocus).toBe(true)
     })
 
     it("Should correctly register an error", () => {
         vcsStore.dispatch({ type: "ERROR" })
-        const state = store.getState()
+        const state = vcsStore.getState()
         expect(state.hasError).toBe(true)
+    })
+
+    it("Should correctly set the selected item", () => {
+        vcsStore.dispatch({ type: "SELECT", payload: { selected: "/test.txt" } })
+        const state = vcsStore.getState()
+        expect(state.selected).toBe("/test.txt")
+    })
+    it("Should correctly set the commit active on commit start", () => {
+        vcsStore.dispatch({ type: "COMMIT_START" })
+        const state = vcsStore.getState()
+        expect(state.commit.active).toBe(true)
+    })
+    it("Should correctly set the commit active to false on commit cancel", () => {
+        vcsStore.dispatch({ type: "COMMIT_CANCEL" })
+        const state = vcsStore.getState()
+        expect(state.commit.active).toBe(false)
+    })
+    it("Should set commit active to false on commit success and add the commit", () => {
+        vcsStore.dispatch({ type: "COMMIT_START" })
+        vcsStore.dispatch({
+            type: "UPDATE_COMMIT_MESSAGE",
+            payload: { message: ["commit message"] },
+        })
+        vcsStore.dispatch({ type: "COMMIT_SUCCESS", payload: { commit: { branch: "test" } } })
+        const state = vcsStore.getState()
+        expect(state.commit.active).toBe(false)
+        expect(state.commit.previousCommits[0].message).toBe("commit message")
     })
 })
