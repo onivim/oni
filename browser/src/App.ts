e0f8225c8a6c9a1581076be882d17248fc72347e
Akin909 <akin.sowemimo@gmail.com>
2018-08-19 14:27:44 +0100

    fix lint error

--- browser/src/App.ts
+++ browser/src/App.ts
@@ -5,10 +5,10 @@
  */
 
 import { ipcRenderer, remote } from "electron"
+import { EventEmitter } from "events"
 import * as fs from "fs"
 import * as minimist from "minimist"
 import * as path from "path"
-import { EventEmitter } from "events"
 
 import { IDisposable } from "oni-types"
 
