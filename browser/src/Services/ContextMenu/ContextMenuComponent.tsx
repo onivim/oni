b9d81e21a754134103d701fd1ddd1feeb7ede5a8
Akin909 <akin.sowemimo@gmail.com>
2018-09-01 20:11:06 +0100

    fix moar lint errors

--- browser/src/Services/ContextMenu/ContextMenuComponent.tsx
+++ browser/src/Services/ContextMenu/ContextMenuComponent.tsx
@@ -12,13 +12,13 @@ import * as Oni from "oni-api"
 
 import { IMenus } from "./../Menu/MenuState"
 
+import styled, { enableMouse, layer } from "./../../UI/components/common"
 import { Arrow, ArrowDirection } from "./../../UI/components/Arrow"
 import { HighlightText } from "./../../UI/components/HighlightText"
 import { QuickInfoDocumentation } from "./../../UI/components/QuickInfo"
 import { Icon } from "./../../UI/Icon"
 
 import { ContextMenuState } from "./ContextMenu"
-import styled, { enableMouse, layer } from "../../UI/components/common"
 
 export interface IContextMenuItem {
     label: string
