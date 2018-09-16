a52f5db4e1d32cc155243116e26a4235c191a8e1
Cryza <manuel@hornung.in>
2018-09-16 20:52:07 +0200

    Update to the new version of oni-fontkit without security issues due to new Function()

--- @types/unicode-properties/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "unicode-properties" {
-    declare const unicodeProperties: UnicodeProperties
-
-    interface UnicodeProperties {
-        isMark(codePoint: number): boolean
-        // Leaving out the rest for now
-    }
-    export default unicodeProperties
-}
