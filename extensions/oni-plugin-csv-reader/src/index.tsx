c0836d56c69f248cce678e4c0329731cb3c5c711
Akin909 <akin.sowemimo@gmail.com>
2018-10-03 18:21:13 +0100

    add build to package json, use papaparse and react virtualised modules

--- extensions/oni-plugin-csv-reader/src/index.tsx
+++ extensions/oni-plugin-csv-reader/src/index.tsx
@@ -1,8 +1,10 @@
 import * as Oni from "oni-api"
 import * as React from "react"
 import * as path from "path"
+import { ParseResult } from "papaparse"
+import { Table, Column } from "react-virtualized"
 
-import { parseCsvToRowsAndColumn } from "./utils"
+import { parseCsvString } from "./utils"
 
 const isCompatible = (buf: Oni.EditorBufferEventArgs) => {
     const ext = path.extname(buf.filePath)
@@ -14,26 +16,52 @@ interface IProps {
 }
 
 interface IState {
-    csv: string[][]
+    csv: ParseResult["data"]
 }
 
 class CSVReader extends React.Component<IProps, IState> {
     state = {
-        csv: null,
+        csv: [],
     }
 
-    componentDidMount() {
-        this.convertLines()
+    async componentDidMount() {
+        await this.convertLines()
     }
 
-    convertLines() {
+    async convertLines() {
         const lines = this.props.context.visibleLines.join("\n")
-        const csv = parseCsvToRowsAndColumn(lines)
-        this.setState({ csv })
+        try {
+            const { data, errors } = await parseCsvString(lines)
+            console.log("data: ", data)
+            this.setState({ csv: data })
+        } catch (e) {
+            console.log("[Oni-CSV-Reader Error]: ", e)
+        }
     }
     render() {
-        console.log("csv: ", this.state.csv)
-        return <div>Reader</div>
+        return (
+            <Table
+                width={300}
+                height={300}
+                rowHeight={30}
+                headerHeight={20}
+                rowCount={this.state.csv.length}
+                rowGetter={this._getDatum}
+            >
+                <Column dataKey="name" width={90} />
+                <Column
+                    width={210}
+                    disableSort
+                    dataKey="random"
+                    cellRenderer={({ cellData }) => console.log({ cellData }) || cellData}
+                    flexGrow={1}
+                />
+            </Table>
+        )
+    }
+    _getDatum = ({ index }) => {
+        console.log("this.state.csv[index]: ", this.state.csv[index])
+        return this.state.csv[index]
     }
 }
 
@@ -48,12 +76,14 @@ class CSVReaderLayer implements Oni.BufferLayer {
 }
 
 export const activate = (oni: Oni.Plugin.Api) => {
-    oni.editors.activeEditor.onBufferEnter.subscribe(buf => {
+    oni.editors.activeEditor.onBufferEnter.subscribe(async buf => {
         const layer = new CSVReaderLayer()
         if (isCompatible(buf)) {
+            // const ext = path.extname(buf.filePath)
+            // const bufferName = buf.filePath.replace("ext", "")
+            // const preview = await oni.editors.activeEditor.openFile(`CSV PREVIEW`)
+            // preview.addLayer(layer)
             oni.editors.activeEditor.activeBuffer.addLayer(layer)
-        } else {
-            oni.editors.activeEditor.activeBuffer.removeLayer(layer)
         }
     })
 }
--- extensions/oni-plugin-csv-reader/src/utils.ts
+++ extensions/oni-plugin-csv-reader/src/utils.ts
@@ -1,3 +1,5 @@
+import * as Papa from "papaparse"
+
 /**
  * Converts CSV to HTML Table
  *
@@ -10,4 +12,19 @@ export function parseCsvToRowsAndColumn(csvText: string, csvColumnDelimiter = "\
     })
 
     return rowsWithColumns
+}
+
+export function parseCsvString(csvString: string) {
+    return new Promise<Papa.ParseResult>((resolve, reject) => {
+        const result = Papa.parse(csvString, {
+            header: true,
+            skipEmptyLines: true,
+            complete: results => {
+                resolve(results)
+            },
+            error(error) {
+                reject(error)
+            },
+        })
+    })
 }
