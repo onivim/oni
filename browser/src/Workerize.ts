baf331b2b3ce17aeb98949863e2a193503e8ffe5
Akin909 <akin.sowemimo@gmail.com>
2018-10-06 00:59:09 +0100

    remove additional global vars argument

--- browser/src/Workerize.ts
+++ browser/src/Workerize.ts
@@ -2,10 +2,6 @@ interface IArgs<T> {
     data: T
 }
 
-interface IContext {
-    [key: string]: any
-}
-
 type Callback<T> = (args?: T) => void
 
 /**
@@ -13,41 +9,27 @@ type Callback<T> = (args?: T) => void
  * passes it off to a dynamically generated WebWorker
  * and returns a promise to resolve the return value of the function
  *
+ * limitations: function has to be Pure! -> no external dependencies etc.
  * @name workerize
  * @function
  * @param Callback<T>
  * @param args?: T
- * @param context?: any
  * @returns Promise<R>
  */
-export function workerize<T, R>(work: Callback<T>, args?: T, context?: IContext) {
+export function workerize<T, R>(work: Callback<T>, args?: T) {
     const handleResult = ({ data }: IArgs<T>) => {
         const result = work(data)
         const worker: Worker = self as any
         return worker.postMessage(result)
     }
 
-    const globalVars = createContextVars(context)
     // courtesy of this gem -
     // https://stackoverflow.com/questions/42773714/is-async-await-truly-non-blocking-in-the-browser
     // writes the contents of the string passed in to a file and executes it.
-    const blob = new Blob(
-        [`var work = ${work};\n ${globalVars} onmessage = ${handleResult.toString()}`],
-        {
-            type: "text/javascript",
-        },
-    )
+    const blob = new Blob([`var work = ${work};\n onmessage = ${handleResult.toString()}`], {
+        type: "text/javascript",
+    })
     const worker = new Worker(URL.createObjectURL(blob))
     worker.postMessage(args)
     return new Promise<R>(resolve => (worker.onmessage = evt => resolve(evt.data)))
-}
-
-const createContextVars = (context: IContext) => {
-    if (!context) {
-        return ""
-    }
-    return Object.keys(context).reduce((acc, key) => {
-        acc += `var ${key} = ${context[key]};\n`
-        return acc
-    }, "")
 }
