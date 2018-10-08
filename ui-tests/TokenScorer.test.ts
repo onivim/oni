0dd54b1aef03b5042c2791e1cf49ca9ddc1be5c4
Akin909 <akin.sowemimo@gmail.com>
2018-10-08 10:17:41 +0100

    fix token scorer tests

--- ui-tests/TokenScorer.test.ts
+++ ui-tests/TokenScorer.test.ts
@@ -1,11 +1,18 @@
 import { TokenScorer } from "./../browser/src/Services/SyntaxHighlighting/TokenScorer"
 import { TokenColor } from "./../browser/src/Services/TokenColors"
+import TokenColorsTrie from "./../browser/src/Services/TokenColorsTrie"
+
+const getTokenTree = (tokens: TokenColor[]) => {
+    const tree = new TokenColorsTrie()
+    tree.setTokens(tokens)
+    return tree
+}
 
 describe("TokenScorer Tests", () => {
     const ranker = new TokenScorer()
     const theme: TokenColor[] = [
         {
-            scope: "entity",
+            scope: ["entity"],
             settings: {
                 foreground: "blue",
                 background: "red",
@@ -13,7 +20,7 @@ describe("TokenScorer Tests", () => {
             },
         },
         {
-            scope: "entity.name",
+            scope: ["entity.name"],
             settings: {
                 foreground: "blue",
                 background: "red",
@@ -21,7 +28,7 @@ describe("TokenScorer Tests", () => {
             },
         },
         {
-            scope: "entity.name.interface",
+            scope: ["entity.name.interface"],
             settings: {
                 foreground: "blue",
                 background: "red",
@@ -29,7 +36,7 @@ describe("TokenScorer Tests", () => {
             },
         },
         {
-            scope: "entity.name.interface.golang",
+            scope: ["entity.name.interface.golang"],
             settings: {
                 foreground: "blue",
                 background: "red",
@@ -37,11 +44,12 @@ describe("TokenScorer Tests", () => {
             },
         },
     ]
+    const tree = getTokenTree(theme)
     it("should correctly rank tokens based on how deep in the syntax tree they are", () => {
         const scopes = ["entity.name", "entity.name.interface", "entity.name.interface.golang"]
-        const highest = ranker.rankTokenScopes(scopes, theme)
+        const highest = ranker.rankTokenScopes(scopes, tree)
         expect(highest).toEqual({
-            scope: "entity.name.interface.golang",
+            scope: ["entity.name.interface.golang"],
             settings: {
                 foreground: "blue",
                 background: "red",
@@ -57,8 +65,8 @@ describe("TokenScorer Tests", () => {
             "entity.name.interface.golang",
             "item.class.component.element.object",
         ]
-        const highest = ranker.rankTokenScopes(scopes, theme)
-        expect(highest.scope).toBe("entity.name.interface.golang")
+        const highest = ranker.rankTokenScopes(scopes, tree)
+        expect(highest.scope).toEqual(["entity.name.interface.golang"])
     })
 
     it('should correctly prioritise fields with higher scope priorities like "support" ', () => {
@@ -68,21 +76,21 @@ describe("TokenScorer Tests", () => {
             "entity.name.interface.golang",
             "support.class.component.element",
         ]
-        const supportTheme: TokenColor[] = [
+        const supportTree = getTokenTree([
             ...theme,
             {
-                scope: "support.class.component.element",
+                scope: ["support.class.component.element"],
                 settings: {
                     foreground: "blue",
                     background: "red",
                     fontStyle: "italic",
                 },
             },
-        ]
+        ])
 
-        const highest = ranker.rankTokenScopes(scopes, supportTheme)
+        const highest = ranker.rankTokenScopes(scopes, supportTree)
         expect(highest).toEqual({
-            scope: "support.class.component.element",
+            scope: ["support.class.component.element"],
             settings: {
                 foreground: "blue",
                 background: "red",
@@ -92,14 +100,14 @@ describe("TokenScorer Tests", () => {
     })
 
     it("should return null if none of the scopes it is passed have any matches", () => {
-        const highest = ranker.rankTokenScopes(["testing.failure"], theme)
+        const highest = ranker.rankTokenScopes(["testing.failure"], tree)
         expect(highest).toBeFalsy()
     })
 
     it("should ignore banned scopes like meta", () => {
         const highest = ranker.rankTokenScopes(
             ["meta.long.token.name", "source.other.long.name"],
-            theme,
+            tree,
         )
         expect(highest).toBeFalsy()
     })
