0b4007fb5ab3d312e31cf80b6e40ffacd57c46b9
Akin <akin.sowemimo@gmail.com>
2018-11-02 09:39:09 +0000

    Feature/fix buffer opening closing (#2644)
    * add a quickOpen default mode command and set to edit and fix check against zero in getDefaultOpenMode
    
    * fix typo in comment
    
    * further typo fixes in comment

--- browser/src/Services/Configuration/DefaultConfiguration.ts
+++ browser/src/Services/Configuration/DefaultConfiguration.ts
@@ -149,6 +149,8 @@ const BaseConfiguration: IConfigurationValues = {
     "editor.quickOpen.alternativeOpenMode": Oni.FileOpenMode.ExistingTab,
     "editor.quickOpen.showHidden": true,
 
+    "quickOpen.defaultOpenMode": Oni.FileOpenMode.Edit,
+
     "editor.split.mode": "native",
 
     "editor.typingPrediction": true,
